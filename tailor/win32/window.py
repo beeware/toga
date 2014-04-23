e6ec1505d49c56e2424c0aa15f44bff0c7713e91
Russell Keith-Magee <russell@keith-magee.com>
2014-04-23 14:27:44 +0800

    Clean up win32 implementation, removing need for delegate class.

--- tailor/win32/window.py
+++ tailor/win32/window.py
@@ -1,42 +1,15 @@
 from tailor.win32.libs import *
 
 
-class WindowDelegate(object):
-    def __init__(self, interface):
-        self.interface = interface
-
-        self._event_handlers = {
-            WM_CLOSE: self.wm_close
-        }
-
-    def _wnd_proc(self, hwnd, msg, wParam, lParam):
-        try:
-            result = self._event_handlers[msg](msg, wParam, lParam)
-        except KeyError:
-            # print "no handler for", msg
-            result = 0
-
-        if not result and msg != WM_CLOSE:
-            result = user32.DefWindowProcW(hwnd, msg, wParam, lParam)
-
-        return result
-
-    def wm_close(self, msg, wParam, lParam):
-        self.interface.on_close()
-        return 0
-
-
 class Window(object):
     def __init__(self, position=(100, 100), size=(640, 480)):
-
-        self._delegate = WindowDelegate(self)
 
         module = kernel32.GetModuleHandleW(None)
         # white = gdi32.GetStockObject(WHITE_BRUSH)
         black = gdi32.GetStockObject(BLACK_BRUSH)
         self._window_class = WNDCLASS()
         self._window_class.lpszClassName = u'GenericAppClass%d' % id(self)
-        self._window_class.lpfnWndProc = WNDPROC(self._delegate._wnd_proc)
+        self._window_class.lpfnWndProc = WNDPROC(self._wnd_proc)
         self._window_class.style = CS_VREDRAW | CS_HREDRAW
         self._window_class.hInstance = 0
         self._window_class.hIcon = user32.LoadIconW(module, MAKEINTRESOURCE(1))
@@ -75,6 +48,24 @@ class Window(object):
 
     def show(self):
         user32.ShowWindow(self._impl, SW_SHOWDEFAULT)
+
+    def _wnd_proc(self, hwnd, msg, wParam, lParam):
+        try:
+            result = {
+                WM_CLOSE: self._wm_close
+            }[msg](msg, wParam, lParam)
+        except KeyError:
+            # print "no handler for", msg
+            result = 0
+
+        if not result and msg != WM_CLOSE:
+            result = user32.DefWindowProcW(hwnd, msg, wParam, lParam)
+
+        return result
+
+    def _wm_close(self, msg, wParam, lParam):
+        self.on_close()
+        return 0
 
     def on_close(self):
         pass
