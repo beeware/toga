28d2971ce0b6a54cb7f17248e3fbc14742c802dc
Russell Keith-Magee <russell@keith-magee.com>
2014-04-25 10:35:37 +0800

    Refactored common Cassowary integration logic into base class.

--- tailor/platform/gtk/widgets/base.py
+++ tailor/platform/gtk/widgets/base.py
@@ -1,6 +1,4 @@
-from tailor.cassowary import Expression, BoundingBox
-from tailor.constraint import Attribute
-from tailor.widget import WidgetBase
+from tailor.cassowary.widget import Widget as CassowaryWidget
 
 
 def wrapped_handler(widget, handler):
@@ -10,36 +8,7 @@ def wrapped_handler(widget, handler):
     return _handler
 
 
-class Widget(WidgetBase):
-    def __init__(self):
-        super(Widget, self).__init__()
-        self._bounding_box = BoundingBox()
-        self._expand_horizontal = True
-        self._expand_vertical = True
-
-    def _expression(self, identifier):
-        if identifier == Attribute.LEFT:
-            return Expression(variable=self._bounding_box.x)
-        elif identifier == Attribute.RIGHT:
-            return Expression(variable=self._bounding_box.x) + Expression(variable=self._bounding_box.width)
-        elif identifier == Attribute.TOP:
-            return Expression(variable=self._bounding_box.y)
-        elif identifier == Attribute.BOTTOM:
-            return Expression(variable=self._bounding_box.y) + Expression(variable=self._bounding_box.height)
-        elif identifier == Attribute.LEADING:
-            return Expression(variable=self._bounding_box.x)
-        elif identifier == Attribute.TRAILING:
-            return Expression(variable=self._bounding_box.x) + Expression(variable=self._bounding_box.width)
-        elif identifier == Attribute.WIDTH:
-            return Expression(variable=self._bounding_box.width)
-        elif identifier == Attribute.HEIGHT:
-            return Expression(variable=self._bounding_box.height)
-        elif identifier == Attribute.CENTER_X:
-            return Expression(variable=self._bounding_box.x) + (Expression(variable=self._bounding_box.width) / 2)
-        elif identifier == Attribute.CENTER_Y:
-            return Expression(variable=self._bounding_box.y) + (Expression(variable=self._bounding_box.height) / 2)
-        # elif identifier == self.BASELINE:
-        #     return ...
+class Widget(CassowaryWidget):
 
     @property
     def _width_hint(self):
--- tailor/platform/win32/widgets/container.py
+++ tailor/platform/win32/widgets/container.py
@@ -1,59 +1,15 @@
-from tailor.cassowary import LayoutManager, Equation, Inequality, approx_equal
-from tailor.constraint import Constraint
-
-from .base import Widget
+from tailor.cassowary.widget import Container as CassowaryContainer
 
 
-class Container(Widget):
-    _RELATION = {
-        Constraint.LTE: Inequality.LEQ,
-        Constraint.GTE: Inequality.GEQ
-    }
-
+class Container(CassowaryContainer):
     def __init__(self):
         super(Container, self).__init__()
-        self._layout_manager = LayoutManager(self._bounding_box)
         self._window = None
 
     def add(self, widget):
         self._layout_manager.add_widget(widget)
         if self._window:
             widget._create(self._window)
-
-    def constrain(self, constraint):
-        "Add the given constraint to the widget."
-        widget = constraint.attr.widget
-        identifier = constraint.attr.identifier
-
-        if constraint.related_attr:
-            related_widget = constraint.related_attr.widget
-            related_identifier = constraint.related_attr.identifier
-
-            expr1 = widget._expression(identifier)
-            if not approx_equal(constraint.attr.multiplier, 1.0):
-                expr1 = expr1 * constraint.attr.multiplier
-            if not approx_equal(constraint.attr.constant, 0.0):
-                expr1 = expr1 + constraint.attr.constant
-
-            expr2 = related_widget._expression(related_identifier)
-            if not approx_equal(constraint.related_attr.multiplier, 1.0):
-                expr2 = expr2 * constraint.related_attr.multiplier
-            if not approx_equal(constraint.related_attr.constant, 0.0):
-                expr2 = expr2 + constraint.related_attr.constant
-
-            if constraint.relation == Constraint.EQUAL:
-                self._layout_manager.add_constraint(Equation(expr1, expr2))
-            else:
-                self._layout_manager.add_constraint(Inequality(expr1, self._RELATION[constraint.relation], expr2))
-        else:
-            expr = widget._expression(identifier)
-            if not approx_equal(constraint.attr.multiplier, 1.0):
-                expr = expr * constraint.attr.multiplier
-
-            if constraint.relation == Constraint.EQUAL:
-                self._layout_manager.add_constraint(Equation(expr, constraint.attr.constant))
-            else:
-                self._layout_manager.add_constraint(Equation(expr, self._RELATION[constraint.relation], constraint.attr.constant))
 
     def _create(self, window, x, y, width, height):
         self._window = window
