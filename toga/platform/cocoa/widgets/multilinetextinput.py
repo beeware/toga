2b5ea42360db08224ad254de73d3d8b219120515
Russell Keith-Magee <russell@keith-magee.com>
2014-07-06 13:29:04 +0800

    Corrected text handling for MultilineTextInput.

--- toga/platform/cocoa/widgets/multilinetextinput.py
+++ toga/platform/cocoa/widgets/multilinetextinput.py
@@ -1,6 +1,6 @@
 from __future__ import print_function, absolute_import, division
 
-from ..libs import get_NSString, cfstring_to_string, NSTextView, NSScrollView, NSBezelBorder
+from ..libs import get_NSString, NSTextView, NSScrollView, NSBezelBorder
 from .base import Widget
 
 
@@ -17,19 +17,18 @@ class MultilineTextInput(Widget):
         # The scroll view is the _impl, because it's the outer container.
         self._impl = NSScrollView.alloc().init()
         self._impl.setHasVerticalScroller_(True)
-        self._impl.setHasHorizontalScroller_(True)
+        self._impl.setHasHorizontalScroller_(False)
         self._impl.setAutohidesScrollers_(False)
         self._impl.setBorderType_(NSBezelBorder)
         self._impl.setTranslatesAutoresizingMaskIntoConstraints_(False)
 
-        self._text = NSTextView.new()
+        self._text = NSTextView.alloc().init()
+
         if self.initial:
-            self._text.setStringValue_(get_NSString(self.initial))
+            self._text.insertText_(get_NSString(self.initial))
 
         self._text.setEditable_(True)
-        # self._text.setBezeled_(True)
-        # self._text.setBezelStyle_(NSTextFieldSquareBezel)
-        self._text.setTranslatesAutoresizingMaskIntoConstraints_(False)
+        self._text.setVerticallyResizable_(True)
+        self._text.setHorizontallyResizable_(True)
 
-    def value(self):
-        return cfstring_to_string(self._text.stringValue)
+        self._impl.setDocumentView_(self._text)
