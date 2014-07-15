c90d3e03dd6ac191385b658c42613eb7e5b79f53
Russell Keith-Magee <russell@keith-magee.com>
2014-07-15 20:51:04 +0800

    Added readonly and value properties to textinput.

--- toga/platform/cocoa/widgets/textinput.py
+++ toga/platform/cocoa/widgets/textinput.py
@@ -7,10 +7,11 @@ from .base import Widget
 class TextInput(Widget):
     _IMPL_CLASS = NSTextField
 
-    def __init__(self, initial=None, placeholder=None):
+    def __init__(self, initial=None, placeholder=None, readonly=False):
         super(TextInput, self).__init__()
         self.initial = initial
         self.placeholder = placeholder
+        self._readonly = readonly
 
     def _startup(self):
         self._impl = self._IMPL_CLASS.new()
@@ -19,10 +20,25 @@ class TextInput(Widget):
         if self.placeholder:
             self._impl.cell.setPlaceholderString_(get_NSString(self.placeholder))
 
-        self._impl.setEditable_(True)
+        self._impl.setEditable_(not self._readonly)
         self._impl.setBezeled_(True)
         self._impl.setBezelStyle_(NSTextFieldSquareBezel)
         self._impl.setTranslatesAutoresizingMaskIntoConstraints_(False)
 
+    @property
+    def readonly(self):
+        return self._readonly
+
+    @readonly.setter
+    def readonly(self, value):
+        self._readonly = value
+        if self._impl:
+            self._impl.setEditable_(not self._readonly)
+
+    @property
     def value(self):
         return cfstring_to_string(self._impl.stringValue)
+
+    @value.setter
+    def value(self, value):
+        self._impl.setStringValue_(get_NSString(unicode(value)))
