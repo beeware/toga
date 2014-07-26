8f5046da8a362da3f7673461cecf53b1841bd41d
Russell Keith-Magee <russell@keith-magee.com>
2014-07-26 11:11:39 +0800

    Added GTK OptionContainer.

--- toga/platform/cocoa/widgets/optioncontainer.py
+++ toga/platform/cocoa/widgets/optioncontainer.py
@@ -7,7 +7,6 @@ from .base import Widget
 class OptionContainer(Widget):
     def __init__(self):
         super(OptionContainer, self).__init__()
-        self._impl = None
         self._content = []
 
     def _startup(self):
