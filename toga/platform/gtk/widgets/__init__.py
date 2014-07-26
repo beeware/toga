4861aebc37d9e191d6053afd70b1705a7a547c4b
Russell Keith-Magee <russell@keith-magee.com>
2014-07-26 10:07:41 +0800

    Added Gtk implementation of Dialogs

--- toga/platform/gtk/widgets/__init__.py
+++ toga/platform/gtk/widgets/__init__.py
@@ -2,6 +2,7 @@ from __future__ import print_function, absolute_import, division
 
 from .button import Button
 from .container import Container
+from .dialog import Dialog
 from .icon import *
 from .label import Label
 from .passwordinput import PasswordInput
