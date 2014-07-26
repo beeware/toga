c25a7947a9a4c9fad5e4626255ebaa957b607c3c
Russell Keith-Magee <russell@keith-magee.com>
2014-07-26 09:39:06 +0800

    Modified GTK backend to load icons for toolbars.

--- toga/platform/gtk/widgets/image.py
+++ toga/platform/gtk/widgets/image.py
@@ -1,5 +1,7 @@
 import os
 
+from gi.repository import Gtk, GdkPixbuf
+
 
 class Image(object):
     app_icon = None
@@ -9,22 +11,39 @@ class Image(object):
         self.system = system
 
         if self.system:
-            filename = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))), 'resources', self.path)
+            self._filename = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))), 'resources', self.path)
         else:
-            filename = self.path
+            self._filename = self.path
 
-        # self._impl = NSImage.alloc().initWithContentsOfFile_(get_NSString(filename))
+        # GTK can load ICNS and image files, but doesn't natively scale to the
+        # appropriate size as required. So, we help it out. But to avoid loading
+        # all the possible icon sizes at once, we lazy load them on first use.
+        self._impl_cache = {}
+
+    def __impl(self, size):
+        try:
+            return self._impl_cache[size]
+        except KeyError:
+            self._impl_cache[size] = Gtk.Image.new_from_pixbuf(
+                GdkPixbuf.Pixbuf.new_from_file(self._filename).scale_simple(size, size, GdkPixbuf.InterpType.BILINEAR)
+            )
+        return self._impl_cache[size]
+
+    _impl_16 = property(lambda self: self.__impl(16))
+    _impl_32 = property(lambda self: self.__impl(32))
+    _impl_48 = property(lambda self: self.__impl(48))
+    _impl_72 = property(lambda self: self.__impl(72))
 
     @staticmethod
     def load(path_or_icon, default=None):
         if path_or_icon:
             if isinstance(path_or_icon, Image):
-                impl = path_or_icon
+                obj = path_or_icon
             else:
-                impl = Image(path_or_icon)
+                obj = Image(path_or_icon)
         elif default:
-            impl = default
-        return impl
+            obj = default
+        return obj
 
 
 TIBERIUS_ICON = Image('tiberius.icns', system=True)
