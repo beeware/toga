527c3d96a8ddd831338324246c79d4c20e972420
Russell Keith-Magee <russell@keith-magee.com>
2014-08-16 18:22:19 +0800

    Initial commit of code after repository split.

---
+++ toga_android/__init__.py
@@ -0,0 +1,36 @@
+# Core capabilities
+# from .app import *
+# from .window import *
+# from .command import *
+
+# Widgets
+# from .widgets.button import *
+# from .widgets.container import *
+# from .widgets.icon import *
+# from .widgets.label import *
+# from .widgets.dialog import *
+# from .widgets.multilinetextinput import *
+# from .widgets.optioncontainer import *
+# from .widgets.passwordinput import *
+# from .widgets.progressbar import *
+# from .widgets.scrollcontainer import *
+# from .widgets.splitcontainer import *
+# from .widgets.table import *
+# from .widgets.textinput import *
+# from .widgets.tree import *
+# from .widgets.webview import *
+
+__all__ = [
+    '__version__',
+
+]
+
+# Examples of valid version strings
+# __version__ = '1.2.3.dev1'  # Development release 1
+# __version__ = '1.2.3a1'     # Alpha Release 1
+# __version__ = '1.2.3b1'     # Beta Release 1
+# __version__ = '1.2.3rc1'    # RC Release 1
+# __version__ = '1.2.3'       # Final Release
+# __version__ = '1.2.3.post1' # Post Release 1
+
+__version__ = '0.0.0'
