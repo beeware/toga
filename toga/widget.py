31cb8af30460915e047a1dc5a07e564ce7aeec61
Russell Keith-Magee <russell@keith-magee.com>
2015-04-09 14:28:35 +0800

    Turned Widget into a CSSNode.

--- toga/widget.py
+++ toga/widget.py
@@ -2,15 +2,12 @@ from __future__ import print_function, absolute_import, division
 
 from colosseum import CSSNode
 
-from rubicon.objc import NSSize, NSRect, NSPoint
-
-
-class Widget(object):
-    def __init__(self):
+class Widget(CSSNode):
+    def __init__(self, **style):
+        super(Widget, self).__init__(**style)
         self._window = None
         self._app = None
         self._impl = None
-        self._css = CSSNode()
 
     @property
     def app(self):
@@ -40,48 +37,3 @@ class Widget(object):
 
     def __repr__(self):
         return "<%s:%s>" % (self.__class__.__name__, id(self))
-
-    def style(self, **styles):
-        for style, value in styles.items():
-            if style in ('margin', 'borderWidth', 'padding'):
-                if style == 'borderWidth':
-                    pre = 'border'
-                    post = 'Width'
-                else:
-                    pre = style
-                    post = ''
-
-                try:
-                    if len(value) == 4:
-                        setattr(self._css, pre + 'Top' + post, value)
-                        setattr(self._css, pre + 'Right' + post, value)
-                        setattr(self._css, pre + 'Bottom' + post, value)
-                        setattr(self._css, pre + 'Left' + post, value)
-                    elif len(value) == 3:
-                        setattr(self._css, pre + 'Top' + post, value)
-                        setattr(self._css, pre + 'Right' + post, value)
-                        setattr(self._css, pre + 'Bottom' + post, value)
-                        setattr(self._css, pre + 'Left' + post, value)
-                    elif len(value) == 2:
-                        setattr(self._css, pre + 'Top' + post, value)
-                        setattr(self._css, pre + 'Right' + post, value)
-                        setattr(self._css, pre + 'Bottom' + post, value)
-                        setattr(self._css, pre + 'Left' + post, value)
-                    elif len(value) == 1:
-                        setattr(self._css, pre + 'Top' + post, value)
-                        setattr(self._css, pre + 'Right' + post, value)
-                        setattr(self._css, pre + 'Bottom' + post, value)
-                        setattr(self._css, pre + 'Left' + post, value)
-                    else:
-                        raise Exception('Invalid %s definition' % style)
-                except TypeError:
-                    setattr(self._css, pre + 'Top' + post, value)
-                    setattr(self._css, pre + 'Right' + post, value)
-                    setattr(self._css, pre + 'Bottom' + post, value)
-                    setattr(self._css, pre + 'Left' + post, value)
-            else:
-                setattr(self._css, style, value)
-
-    def _update_layout(self):
-        layout = self._css.layout
-        self._impl.setFrame_(NSRect(NSPoint(layout.left, layout.top), NSSize(layout.width, layout.height)))
