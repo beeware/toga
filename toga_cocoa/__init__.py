b192a22ee142eb13a89b555ffa5a3eae64ee563d
Russell Keith-Magee <russell@keith-magee.com>
2016-09-04 10:45:58 +0800

    Corrected exposure of App elements.

--- toga_cocoa/__init__.py
+++ toga_cocoa/__init__.py
@@ -24,7 +24,7 @@ from .widgets.webview import *
 
 __all__ = [
     '__version__',
-    'App',
+    'App', 'MainWindow',
     'Window',
     'Command', 'SEPARATOR', 'SPACER', 'EXPANDING_SPACER',
     'Button',
