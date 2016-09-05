0b14708c4cbc6dab8a4500d0e7ce20befb2f9b98
Russell Keith-Magee <russell@keith-magee.com>
2016-09-05 15:33:52 +0800

    Corrected prototype of tree constructor.

--- toga/interface/widgets/tree.py
+++ toga/interface/widgets/tree.py
@@ -2,5 +2,5 @@ from .base import Widget
 
 
 class Tree(Widget):
-    def __init__(self, id=None, style=None, initial=None):
+    def __init__(self, headings, id=None, style=None):
         super().__init__(id=id, style=style)
