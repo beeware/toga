6d34d3e7a5d353c2b8c4f3559329a9c5727ad90e
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2016-10-03 16:30:48 +0800

    review comments

--- toga/interface/dialogs.py
+++ toga/interface/dialogs.py
@@ -1,22 +1,22 @@
-def info_dialog(window, title, message):
+def info(window, title, message):
     raise NotImplementedError()
 
 
-def question_dialog(window, title, message):
+def question(window, title, message):
     raise NotImplementedError()
 
 
-def confirm_dialog(window, title, message):
+def confirm(window, title, message):
     raise NotImplementedError()
 
 
-def error_dialog(window, title, message):
+def error(window, title, message):
     raise NotImplementedError()
 
 
-def stack_trace_dialog(window, title, message, content, retry=False):
+def stack_trace(window, title, message, content, retry=False):
     raise NotImplementedError()
 
 
-def save_file_dialog(window, title, suggested_filename, file_types):
+def save_file(window, title, suggested_filename, file_types):
     raise NotImplementedError()
