13db938a90843070174fd2d57efe6c4f83333808
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2016-10-08 21:28:03 +1100

    put it in a box

--- toga_gtk/widgets/selection.py
+++ toga_gtk/widgets/selection.py
@@ -15,44 +15,26 @@ class Selection(SelectionInterface, WidgetMixin):
 
     def create(self):
 
-        self._impl = Gtk.ComboBoxText.new()
+        self._impl = Gtk.Box()
         self._impl._interface = self
+
+        self._comboimpl = Gtk.ComboBoxText.new()
+        self._comboimpl._interface = self
+        self._impl.pack_start(name_combo, False, False, 0)
 
         for item in self._items:
             self._add_item(item)
 
-
-
     def _remove_all_items(self):
         self._text.clear()
-        self._impl.remove_all()
+        self._comboimpl.remove_all()
 
     def _add_item(self, item):
         self._text.append(item)
-        self._impl.append_text(item)
+        self._comboimpl.append_text(item)
 
     def _select_item(self, item):
-        self._impl.set_active(self._text.index(item))
+        self._comboimpl.set_active(self._text.index(item))
 
     def _get_selected_item(self):
-        return self._impl.get_active_text()
-
-
-    def rehint(self):
-        # print("REHINT", self, self._impl.get_preferred_width(), self._impl.get_preferred_height(), getattr(self, '_fixed_height', False), getattr(self, '_fixed_width', False))
-        hints = {}
-        width = self._impl.get_preferred_width()
-        minimum_width = width[0]
-        natural_width = width[1]
-
-        height = self._impl.get_preferred_height()
-        minimum_height = height[0]
-        natural_height = height[1]
-
-        if minimum_width > 0:
-            hints['min_width'] = minimum_width
-        if minimum_height > 0:
-            hints['min_height'] = minimum_height
-
-        if hints:
-            self.style.hint(**hints)
+        return self._comboimpl.get_active_text()
