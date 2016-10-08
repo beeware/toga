9a3a7d69161443fe797ec0570297666b9d49e56d
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2016-10-08 19:36:57 +1100

    try

--- toga_gtk/widgets/numberinput.py
+++ toga_gtk/widgets/numberinput.py
@@ -6,4 +6,15 @@ from .base import WidgetMixin
 
 class NumberInput(WidgetMixin, NumberInputInterface):
 
-    pass
+    def __init__(self, id=None, style=None, min=0, max=100, step=1, **ex):
+        super().__init__(id=id, style=style, min=min, max=max, step=step, **ex)
+
+    def create(self):
+
+        self._impl = Gtk.SpinButton.new_with_range(self._config["min_value"], self._config["max_value"], self._config["step"])
+
+    def _get_value(self):
+        return 0
+
+    def _set_value(self, value):
+        pass
