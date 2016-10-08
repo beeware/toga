945b3fa8e1b520c86b77184f1a5610b98c21f543
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2016-10-08 22:01:46 +1100

    try

--- toga/interface/widgets/numberinput.py
+++ toga/interface/widgets/numberinput.py
@@ -3,12 +3,13 @@ from .base import Widget
 
 class NumberInput(Widget):
     def __init__(self, id=None, style=None, min_value=0, max_value=100, step=1, **ex):
+        self._min_value = min_value
+        self._max_value = max_value
+        self._step = step
         super().__init__(id=id, style=style, min_value=min_value, max_value=max_value, step=step, **ex)
 
-    def _configure(self, min, max, step):
-        self.min = min
-        self.max = max
-        self.step = step
+    def _configure(self, **kw):
+        pass
 
     @property
     def value(self):
