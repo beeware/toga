b3580760b135f8818c5f087b2f5f480520a28eb8
Russell Keith-Magee <russell@keith-magee.com>
2016-10-24 18:49:05 +0800

    Corrected error in manual merge.
    Thanks to @marcmolla for the report.

--- src/cocoa/toga_cocoa/widgets/imageview.py
+++ src/cocoa/toga_cocoa/widgets/imageview.py
@@ -56,7 +56,6 @@ class ImageView(ImageViewInterface, WidgetMixin):
         if image:
             self._impl.image = image._impl
         else:
-            self._impl.image = NSImage.alloc().initWithSize_(NSSize(self.width, self.height))
             width = 0
             height = 0
             if self.style and self.style.width and self.style.height:
