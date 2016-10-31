36962236de36d367551bd2b57481be3e13e2b5bd
Jonas Schell <jonasschell@ocupr.org>
2016-10-31 02:48:17 +0100

    Enabled placeholder functionality for PasswordInput
    Signed-off-by: Jonas Schell <jonasschell@ocupr.org>

--- src/cocoa/toga_cocoa/widgets/passwordinput.py
+++ src/cocoa/toga_cocoa/widgets/passwordinput.py
@@ -5,5 +5,5 @@ from .textinput import TextInput
 class PasswordInput(TextInput):
     _IMPL_CLASS = NSSecureTextField
 
-    def __init__(self, id=None, style=None):
-        super(PasswordInput, self).__init__(id=id, style=style)
+    def __init__(self, id=None, style=None, placeholder=None):
+        super(PasswordInput, self).__init__(id=id, style=style, placeholder=placeholder)
