650dab03d541690e97b73f1cbb8bf58b0bb1c161
Shai Berger <shai@platonix.com>
2016-11-22 15:17:57 +0200

    Select the right Toga platform under Django
    Added it in the ready() call -- I can somehow imagine one actually
    wanting the server platform even when this app is imported, but
    not if it is initialized by Django.

--- src/django/toga_django/django/__init__.py
+++ src/django/toga_django/django/__init__.py
@@ -3,9 +3,14 @@ import os
 from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
+import toga
+
 
 class TogaApp(AppConfig):
     name = 'toga'
     label = 'toga'
     verbose_name = _("Toga")
     path = os.path.dirname(__file__)
+
+    def ready(self):
+        toga.set_platform('toga_django')
