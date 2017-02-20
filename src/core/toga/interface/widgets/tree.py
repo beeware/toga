85271fd5bf34b4f05295c76881f246783f23748b
Anthony Shaw <anthonyshaw@apache.org>
2017-02-20 06:24:51 +0300

    docstrings for web view and other remaining components

--- src/core/toga/interface/widgets/tree.py
+++ src/core/toga/interface/widgets/tree.py
@@ -2,7 +2,23 @@ from .base import Widget
 
 
 class Tree(Widget):
+    '''
+    Tree widget
+    '''
     def __init__(self, headings, id=None, style=None):
+        '''
+        Instantiate a new instance of the tree widget
+
+        :param headings: The list of headings for the tree
+        :type  headings: ``list`` of ``str``
+
+        :param id:          An identifier for this widget.
+        :type  id:          ``str``
+
+        :param style:       an optional style object. If no style is provided then a
+                            new one will be created for the widget.
+        :type style:        :class:`colosseum.CSSNode`
+        '''
         super().__init__(id=id, style=style)
         self.headings = headings
 
