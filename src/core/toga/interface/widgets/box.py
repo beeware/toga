e8572ebd2305ce66037614146ff514b9b49d3f55
Anthony Shaw <anthonyshaw@apache.org>
2017-02-20 11:28:28 +1100

    docstrings for the box class

--- src/core/toga/interface/widgets/box.py
+++ src/core/toga/interface/widgets/box.py
@@ -2,16 +2,20 @@ from .base import Widget
 
 
 class Box(Widget):
-    '''This is a Widget that contains other widgets, but has no rendering or
+    '''
+    This is a Widget that contains other widgets, but has no rendering or
     interaction of its own.
 
     :param id:          An identifier for this widget.
+    :type  id:          ``str``
+
     :param style:       an optional style object. If no style is provided then a
                         new one will be created for the widget.
-    :type style:        colosseum.CSS
+    :type style:        :class:`colosseum.CSSNode`
+
     :param children:    An optional list of child Widgets that will be in this
                         box.
-    :type children:     list
+    :type children:     ``list``
     '''
     def __init__(self, id=None, style=None, children=None):
         super().__init__(id=id, style=style, children=children)
