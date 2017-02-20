41060325b8d669ac845b30633f4ebb1d1ad6b1af
Anthony Shaw <anthonyshaw@apache.org>
2017-02-20 11:33:57 +1100

    docstrings for the image

--- src/core/toga/interface/widgets/imageview.py
+++ src/core/toga/interface/widgets/imageview.py
@@ -2,7 +2,22 @@ from .base import Widget
 
 
 class ImageView(Widget):
+    '''
+    An image viewer
+    '''
     def __init__(self, id=None, style=None, image=None):
+        '''
+        Instantiate a new image viewer widget
+        
+        :param id:          An identifier for this widget.
+        :type  id:          ``str``
+    
+        :param style:       an optional style object. If no style is provided then a
+                            new one will be created for the widget.
+        :type style:        :class:`colosseum.CSSNode`
+    
+        :param image:    Image to display
+        '''
         super().__init__(id=id, style=style, image=image)
 
     def _configure(self, image):
