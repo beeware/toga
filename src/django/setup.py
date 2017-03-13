dcf0cc095f411d41a9a462741a3059cdd12ff481
Russell Keith-Magee <russell@keith-magee.com>
2017-03-13 11:31:44 +0800

    Tweaked dependencies for django backend.

--- src/django/setup.py
+++ src/django/setup.py
@@ -31,7 +31,8 @@ setup(
     url='http://pybee.org/toga',
     packages=find_packages(exclude='tests'),
     install_requires=[
-        'django>=1.8',
+        'django==1.10.6',
+        'django-environ==0.4.1'
     ],
     license='New BSD',
     classifiers=[
