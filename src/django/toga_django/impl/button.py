15ebffd5d24c6313ed45cc8f756ce30ee7a11ef6
anna <annarschneider@gmail.com>
2017-04-06 07:49:00 -0700

    Django button widgets are Bootstrap styled and span the width of the parent.

--- src/django/toga_django/impl/button.py
+++ src/django/toga_django/impl/button.py
@@ -12,7 +12,7 @@ class Button:
         self.style = style
 
     def __html__(self):
-        return '<button id="toga:%s" class="toga Button" style="%s" data-toga-class="toga.Button" data-toga-parent="%s" data-toga-ports="%s" data-toga-on-press="%s">%s</button>' % (
+        return '<button id="toga:%s" class="toga Button btn btn-default btn-block" style="%s" data-toga-class="toga.Button" data-toga-parent="%s" data-toga-ports="%s" data-toga-on-press="%s">%s</button>' % (
             self.id,
             self.style,
             self.parent.id,
