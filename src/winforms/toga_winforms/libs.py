32a6f6334cdf5954a85e749861f0b2802b8941da
Russell Keith-Magee <russell@keith-magee.com>
2017-04-22 23:29:55 +0800

    Added color to default symbols on Windows.

--- src/winforms/toga_winforms/libs.py
+++ src/winforms/toga_winforms/libs.py
@@ -2,5 +2,5 @@ import clr
 clr.AddReference("System.Windows.Forms")
 
 from System import Uri, Threading
-from System.Drawing import Size, Point
+from System.Drawing import Size, Point, Color
 import System.Windows.Forms as WinForms
