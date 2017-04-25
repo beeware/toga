d213012917557f7d8bbf53555d62329ee80fe5b8
Adam Baxter <voltagex@voltagex.org>
2017-04-26 00:33:36 +1000

    Start work on NumberInput

---
+++ src/winforms/toga_winforms/widgets/numberinput.py
@@ -0,0 +1,29 @@
+from ..libs import WinForms
+
+from toga.interface import NumberInput as NumberInputInterface
+
+from .base import WidgetMixin
+
+class NumberInput(WidgetMixin, NumberInputInterface):
+    _IMPL_CLASS = WinForms.NumericUpDown
+
+    def __init__(self, id=None, style=None, min_value=0, max_value=100, step=1,
+                 **ex):
+        super().__init__(id=id, style=style, min_value=min_value,
+                         max_value=max_value, step=step, **ex)
+        self._create()
+
+    def create(self):
+        self._impl = self._IMPL_CLASS()
+        self._impl._interface = self
+        self.rehint()
+
+    def _get_value(self):
+        return self._impl.get_value()
+
+    def _set_value(self, value):
+        self._impl.set_value(value)
+
+    def rehint(self):
+        self.style.min_width = 120
+        self.style.height = 32
