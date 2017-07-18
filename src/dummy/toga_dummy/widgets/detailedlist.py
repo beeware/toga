2fd5f3608fdb51b4cc8d2bdea51ccfaf6e15bad0
Jonas Schell <jonasschell@ocupe.org>
2017-07-18 09:43:24 +0200

    added implementation tests to toga-iOS
    Signed-off-by: Jonas Schell <jonasschell@ocupe.org>

---
+++ src/dummy/toga_dummy/widgets/detailedlist.py
@@ -0,0 +1,9 @@
+class DetailedList():
+    def __init__(self, id=None, data=None, on_delete=None, on_refresh=None, style=None):
+        pass
+
+    def create(self):
+        pass
+
+    def _add(self, item):
+        pass
---
+++ src/dummy/toga_dummy/widgets/navigationview.py
@@ -0,0 +1,12 @@
+class NavigationView():
+    def __init__(self, title, content, on_action=None, style=None):
+        pass
+
+    def create(self):
+        pass
+
+    def _push(self, content):
+        pass
+
+    def _pop(self, content):
+        pass
