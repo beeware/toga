dd7de33ac66e0e248471b246f560ffdedb545971
Russell Keith-Magee <russell@keith-magee.com>
2017-07-23 20:28:07 +0800

    Move to fully constraint-based layout of widgets in scroll panels.
    This fixes the problem with scrollview displays.

--- src/cocoa/toga_cocoa/widgets/multilinetextinput.py
+++ src/cocoa/toga_cocoa/widgets/multilinetextinput.py
@@ -1,10 +1,6 @@
 from toga.interface import MultilineTextInput as MultilineTextInputInterface
 
-from ..libs import (
-    NSTextView, NSScrollView,
-    NSBezelBorder, NSViewWidthSizable, NSViewHeightSizable,
-    NSRect, NSPoint, NSSize
-)
+from ..libs import *
 from .base import WidgetMixin
 
 
@@ -19,15 +15,16 @@ class MultilineTextInput(MultilineTextInputInterface, WidgetMixin):
         # Create a multiline view, and put it in a scroll view.
         # The scroll view is the _impl, because it's the outer container.
         self._impl = NSScrollView.alloc().init()
-        self._impl.setHasVerticalScroller_(True)
-        self._impl.setHasHorizontalScroller_(False)
-        self._impl.setAutohidesScrollers_(False)
-        self._impl.setBorderType_(NSBezelBorder)
+        self._impl.hasVerticalScroller = True
+        self._impl.hasHorizontalScroller = False
+        self._impl.autohidesScrollers = False
+        self._impl.borderType = NSBezelBorder
 
         # Disable all autolayout functionality on the outer widget
-        self._impl.setTranslatesAutoresizingMaskIntoConstraints_(False)
-        self._impl.setAutoresizesSubviews_(True)
+        self._impl.translatesAutoresizingMaskIntoConstraints = False
+        self._impl.autoresizesSubviews = True
 
+        # Create the actual text widget
         self._text = NSTextView.alloc().init()
         self._text.editable = True
         self._text.enabled = True
@@ -35,7 +32,27 @@ class MultilineTextInput(MultilineTextInputInterface, WidgetMixin):
         self._text.verticallyResizable = True
         self._text.horizontallyResizable = False
 
-        self._impl.setDocumentView_(self._text)
+        # Disable the autolayout functionality, and replace with
+        # constraints controlled by the layout.
+        self._text.translatesAutoresizingMaskIntoConstraints = False
+        self._width_constraint = NSLayoutConstraint.constraintWithItem_attribute_relatedBy_toItem_attribute_multiplier_constant_(
+            self._text, NSLayoutAttributeRight,
+            NSLayoutRelationEqual,
+            self._text, NSLayoutAttributeLeft,
+            1.0, 0
+        )
+        self._text.addConstraint(self._width_constraint)
+
+        self._height_constraint = NSLayoutConstraint.constraintWithItem_attribute_relatedBy_toItem_attribute_multiplier_constant_(
+            self._text, NSLayoutAttributeBottom,
+            NSLayoutRelationEqual,
+            self._text, NSLayoutAttributeTop,
+            1.0, 0
+        )
+        self._text.addConstraint(self._height_constraint)
+
+        # Put the text view in the scroll window.
+        self._impl.documentView = self._text
 
         # Add the layout constraints
         self._add_constraints()
@@ -49,14 +66,9 @@ class MultilineTextInput(MultilineTextInputInterface, WidgetMixin):
         if value:
             self._text.insertText(value)
 
-    def _apply_layout(self):
-        super()._apply_layout()
-        self._text.setFrame(
-            NSRect(
-                NSPoint(0, 0),
-                NSSize(self.layout.width, self.layout.height)
-            )
-        )
+    def _update_child_layout(self):
+        self._width_constraint.constant = self.layout.width
+        self._height_constraint.constant = self.layout.height
 
     def rehint(self):
         self.style.hint(
