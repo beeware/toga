bf7455673d7688bc2e6c72b201ddf2f6bd896539
Jonas Schell <jonasschell@ocupe.org>
2017-07-26 16:02:09 +0200

    added set_background_color in cocoa.Widget
    Signed-off-by: Jonas Schell <jonasschell@ocupe.org>

--- src/cocoa/toga_cocoa/widgets/base.py
+++ src/cocoa/toga_cocoa/widgets/base.py
@@ -1,3 +1,5 @@
+from toga_cocoa.libs import NSColor, NSColorUsingColorName
+
 from ..container import Constraints
 
 
@@ -48,6 +50,25 @@ class Widget:
 
     def set_enabled(self, value):
         self.native.setEnabled_(value)
+
+    def set_background_color(self, background_color):
+        if background_color:
+            if isinstance(background_color, tuple):
+                background_color = NSColor.colorWithRed_green_blue_alpha_(background_color[0] / 255,
+                                                                          background_color[1] / 255,
+                                                                          background_color[2] / 255, 1.0)
+            elif isinstance(background_color, str):
+                try:
+                    background_color = NSColorUsingColorName(background_color.upper())
+                except:
+                    raise ValueError(
+                        'Background color %s does not exist, try a RGB number (red, green, blue).' % background_color)
+            else:
+                raise ValueError('_set_background_color on button widget must receive a tuple or a string')
+
+            self.native.setBordered_(False)
+            self.native.setWantsLayer_(True)
+            self.native.setBackgroundColor_(background_color)
 
     def rehint(self):
         pass
