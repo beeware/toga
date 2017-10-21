74509093a2df4aaf8afb4e24f017f0816f95a5f3
Dan Yeaw <dan@yeaw.me>
2017-10-21 17:27:44 -0400

    Add initial dummy Context2D and Matrix Widgets
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- src/dummy/toga_dummy/widgets/canvas.py
+++ src/dummy/toga_dummy/widgets/canvas.py
@@ -4,3 +4,11 @@ from .base import Widget
 class Canvas(Widget):
     def create(self):
         pass
+
+class Context2D(Widget):
+    def create(self):
+        pass
+
+class Matrix(Widget):
+    def create(self):
+        pass
