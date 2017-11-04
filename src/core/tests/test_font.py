06d87873a6540c408c74b028d729849f5de72514
Drew Meier <darius.montez@gmail.com>
2017-11-03 20:29:16 -0400

    Create dummy factory for Font
    In order to test the core API Font class, create
    dummy factory for it.
    
    Signed-off-by: Drew Meier <darius.montez@gmail.com>

--- src/core/tests/test_font.py
+++ src/core/tests/test_font.py
@@ -6,12 +6,18 @@ import toga_dummy
 
 class TestFont(unittest.TestCase):
     def setUp(self):
+        mock_font = MagicMock(spec=toga_dummy.factory.Font)
+        
+        self.factory = MagicMock()
+        self.factory.Font = MagicMock(return_value=mock_font)
+
         self.family = "sans-serif"
         self.size = 14
 
         self.font = toga.Font(
             self.family,
-            self.size
+            self.size,
+            factory=self.factory
         )
 
     def test_family(self):
