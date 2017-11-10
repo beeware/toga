c6da1040e0d6043af99b4e422e4ca62e79bef866
Dan Yeaw <dan@yeaw.me>
2017-11-10 14:49:13 -0500

    Simplify interface to remove context handling
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- docs/tutorial/source/tutorial-4.py
+++ docs/tutorial/source/tutorial-4.py
@@ -11,9 +11,8 @@ class StartApp(toga.App):
         self.main_window = toga.MainWindow(self.name, size=(500, 500))
         self.main_window.app = self
 
-        canvas = toga.Canvas()
-        canvas.draw(self.draw_steps)
-        box = toga.Box(children=[canvas])
+        self.canvas = toga.Canvas(on_draw=self.draw_steps)
+        box = toga.Box(children=[self.canvas])
 
         # Add the content on the main window
         self.main_window.content = box
@@ -21,73 +20,73 @@ class StartApp(toga.App):
         # Show the main window
         self.main_window.show()
 
-    def triangle(self, context):
-        with context.closed_path(self.SIZE, 0):
-            context.line_to(self.SIZE, 2 * self.SIZE)
-            context.line_to(-2 * self.SIZE, 0)
+    def triangle(self):
+        with self.canvas.closed_path(self.SIZE, 0):
+            self.canvas.line_to(self.SIZE, 2 * self.SIZE)
+            self.canvas.line_to(-2 * self.SIZE, 0)
 
-    def square(self, context):
-        with context.closed_path(0, 0):
-            context.line_to(2 * self.SIZE, 0)
-            context.line_to(0, 2 * self.SIZE)
-            context.line_to(-2 * self.SIZE, 0)
+    def square(self):
+        with self.canvas.closed_path(0, 0):
+            self.canvas.line_to(2 * self.SIZE, 0)
+            self.canvas.line_to(0, 2 * self.SIZE)
+            self.canvas.line_to(-2 * self.SIZE, 0)
 
-    def bowtie(self, context):
-        with context.closed_path(0, 0):
-            context.line_to(2 * self.SIZE, 2 * self.SIZE)
-            context.line_to(-2 * self.SIZE, 0)
-            context.line_to(2 * self.SIZE, -2 * self.SIZE)
+    def bowtie(self):
+        with self.canvas.closed_path(0, 0):
+            self.canvas.line_to(2 * self.SIZE, 2 * self.SIZE)
+            self.canvas.line_to(-2 * self.SIZE, 0)
+            self.canvas.line_to(2 * self.SIZE, -2 * self.SIZE)
 
-    def inf(self, context):
-        with context.closed_path(0, self.SIZE):
-            context.bezier_curve_to(0, self.SIZE, self.SIZE, self.SIZE, 2 * self.SIZE, 0)
-            context.bezier_curve_to(self.SIZE, -self.SIZE, 2 * self.SIZE, -self.SIZE, 2 * self.SIZE, 0)
-            context.bezier_curve_to(0, self.SIZE, -self.SIZE, self.SIZE, -2 * self.SIZE, 0)
-            context.bezier_curve_to(-self.SIZE, -self.SIZE, -2 * self.SIZE, -self.SIZE, -2 * self.SIZE, 0)
+    def inf(self):
+        with self.canvas.closed_path(0, self.SIZE):
+            self.canvas.bezier_curve_to(0, self.SIZE, self.SIZE, self.SIZE, 2 * self.SIZE, 0)
+            self.canvas.bezier_curve_to(self.SIZE, -self.SIZE, 2 * self.SIZE, -self.SIZE, 2 * self.SIZE, 0)
+            self.canvas.bezier_curve_to(0, self.SIZE, -self.SIZE, self.SIZE, -2 * self.SIZE, 0)
+            self.canvas.bezier_curve_to(-self.SIZE, -self.SIZE, -2 * self.SIZE, -self.SIZE, -2 * self.SIZE, 0)
 
-    def fill_shapes(self, canvas, context, x, y):
+    def fill_shapes(self, x, y):
         for shape in range(4):
-            with context.fill():
+            with self.canvas.fill():
                 if shape == 0:
-                    context.translate(x + self.SIZE, y + self.SIZE)
-                    self.bowtie(context)
+                    self.canvas.translate(x + self.SIZE, y + self.SIZE)
+                    self.bowtie()
                 elif shape == 1:
-                    context.translate(3 * self.SIZE, 0)
-                    self.square(context)
+                    self.canvas.translate(3 * self.SIZE, 0)
+                    self.square()
                 elif shape == 2:
-                    context.translate(3 * self.SIZE, 0)
-                    self.triangle(context)
+                    self.canvas.translate(3 * self.SIZE, 0)
+                    self.triangle()
                 else:
-                    context.translate(3 * self.SIZE, 0)
-                    self.inf(context)
+                    self.canvas.translate(3 * self.SIZE, 0)
+                    self.inf()
 
-    def stroke_shapes(self, canvas, context, x, y):
+    def stroke_shapes(self, x, y):
         for shape in range(4):
-            with context.stroke():
+            with self.canvas.stroke():
                 if shape == 0:
-                    context.translate(x + self.SIZE, y + self.SIZE)
-                    self.bowtie(context)
+                    self.canvas.translate(x + self.SIZE, y + self.SIZE)
+                    self.bowtie()
                 elif shape == 1:
-                    context.translate(3 * self.SIZE, 0)
-                    self.square(context)
+                    self.canvas.translate(3 * self.SIZE, 0)
+                    self.square()
                 elif shape == 2:
-                    context.translate(3 * self.SIZE, 0)
-                    self.triangle(context)
+                    self.canvas.translate(3 * self.SIZE, 0)
+                    self.triangle()
                 else:
-                    context.translate(3 * self.SIZE, 0)
-                    self.inf(context)
+                    self.canvas.translate(3 * self.SIZE, 0)
+                    self.inf()
 
-    def draw_steps(self, canvas, context):
-        context.line_style('rbga(0, 0, 0, 1)')
-        canvas.line_width(context, self.SIZE / 4)
-        canvas.draw(self.stroke_shapes(x=0, y=0))
-        canvas.draw(self.stroke_shapes(x=0, y=3 * self.SIZE))
-        canvas.draw(self.stroke_shapes(x=0, y=6 * self.SIZE))
-        canvas.draw(self.stroke_shapes(x=0, y=9 * self.SIZE))
-        canvas.draw(self.fill_shapes(x=0, y=12 * self.SIZE))
-        canvas.draw(self.fill_shapes(x=0, y=15 * self.SIZE))
-        context.line_style('rgba(1, 0, 0, 1)')
-        canvas.draw(self.stroke_shapes(x=0, y=15 * self.SIZE))
+    def draw_steps(self):
+        self.canvas.stroke_style('rbga(0, 0, 0, 1)')
+        self.canvas.line_width(self.SIZE / 4)
+        self.stroke_shapes(0, 0)
+        self.stroke_shapes(0, 3 * self.SIZE)
+        self.stroke_shapes(0, 6 * self.SIZE)
+        self.stroke_shapes(0, 9 * self.SIZE)
+        self.fill_shapes(0, 12 * self.SIZE)
+        self.fill_shapes(0, 15 * self.SIZE)
+        self.canvas.stroke_style('rgba(1, 0, 0, 1)')
+        self.stroke_shapes(0, 15 * self.SIZE)
 
 
 if __name__ == '__main__':
