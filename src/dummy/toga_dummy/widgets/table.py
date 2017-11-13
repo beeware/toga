f77dcc9ba66ee20c7ecf0d7ef31558bed46503f1
Russell Keith-Magee <russell@keith-magee.com>
2017-11-13 20:04:57 +0800

    Updated dummy interface to reflect new Table and Tree APIs.

--- src/dummy/toga_dummy/widgets/table.py
+++ src/dummy/toga_dummy/widgets/table.py
@@ -5,8 +5,8 @@ class Table(Widget):
     def create(self):
         self._action('create Table')
 
-    def refresh(self):
-        self._action('refresh')
+    def data_changed(self):
+        self._action('data changed')
 
     def set_on_select(self, handler):
         self._set_value('on_select', handler)
--- src/dummy/toga_dummy/widgets/tree.py
+++ src/dummy/toga_dummy/widgets/tree.py
@@ -5,17 +5,8 @@ class Tree(Widget):
     def create(self):
         self._action('create Tree')
 
-    def insert_node(self, node):
-        self._action('insert node', node=node)
-
-    def refresh_node(self, node):
-        self._action('refresh node', node=node)
-
-    def remove_node(self, node):
-        self._action('remove node', node=node)
-
-    def refresh(self):
-        self._action('refresh')
+    def data_changed(self):
+        self._action('data changed')
 
     def set_on_select(self, handler):
         self._set_value('on_select', handler)
