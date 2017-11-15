ff55e6681b3e0547f513364c1cd206d5e5979455
Dan Yeaw <dan@yeaw.me>
2017-11-14 22:40:10 -0500

    Fix AssertionError when calling assertActionPerformedWith
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- src/core/tests/widgets/test_canvas.py
+++ src/core/tests/widgets/test_canvas.py
@@ -34,6 +34,7 @@ class CanvasTests(TestCase):
             self.testing_canvas.stroke_style('rgba(0.25, 0.25, 0.25, 0.6)')
             self.testing_canvas.line_width(1)
             self.testing_canvas.stroke()
+
         self.testing_canvas.on_draw = drawing
 
     def test_self_oval_path(self):
@@ -97,33 +98,49 @@ class CanvasTests(TestCase):
         with self.testing_canvas.closed_path(32, 0):
             self.testing_canvas.line_to(32, 64)
             self.testing_canvas.line_to(-64, 0)
+            # TODO add multi-action testing: for example 'close path', 'create Canvas', 'line to', 'move to', 'rehint Canvas'
+            # self.assertActionPerformedWith(self.testing_canvas, 'closed path', x=32, y=0)
 
     def test_move_to(self):
         self.testing_canvas.move_to(5, 7)
+        self.assertActionPerformedWith(self.testing_canvas, 'move to', x=5, y=7)
+        self.testing_canvas.move_to(-5, 20.0)
+        self.assertActionPerformedWith(self.testing_canvas, 'move to', x=-5, y=20.0)
 
     def test_line_to(self):
         self.testing_canvas.line_to(2, 3)
+        self.assertActionPerformedWith(self.testing_canvas, 'line to', x=2, y=3)
 
     def test_bezier_curve_to(self):
         self.testing_canvas.bezier_curve_to(1, 1, 2, 2, 5, 5)
+        self.assertActionPerformedWith(self.testing_canvas, 'bezier curve to', cp1x=1, cp1y=1, cp2x=2, cp2y=2, x=5, y=5)
 
     def test_quadratic_curve_to(self):
         self.testing_canvas.quadratic_curve_to(1, 1, 5, 5)
+        self.assertActionPerformedWith(self.testing_canvas, 'quadratic curve to', cpx=1, cpy=1, x=5, y=5)
 
     def test_arc(self):
         self.testing_canvas.arc(-10, -10, 10, math.pi / 2, 0, True)
+        self.assertActionPerformedWith(self.testing_canvas, 'arc', x=-10, y=-10, radius=10, startangle=math.pi / 2,
+                                       endangle=0, anticlockwise=True)
 
     def test_ellipse(self):
         self.testing_canvas.ellipse(1, 1, 50, 20, 0, math.pi, 2 * math.pi, False)
+        self.assertActionPerformedWith(self.testing_canvas, 'ellipse', x=1, y=1, radiusx=50, radiusy=20, rotation=0,
+                                       startangle=math.pi, endangle=2 * math.pi, anticlockwise=False)
 
     def test_rotate(self):
         self.testing_canvas.rotate(math.pi)
+        self.assertActionPerformedWith(self.testing_canvas, 'rotate', radians=math.pi)
 
     def test_scale(self):
         self.testing_canvas.scale(2, 1.5)
+        self.assertActionPerformedWith(self.testing_canvas, 'scale', sx=2, sy=1.5)
 
     def test_translate(self):
         self.testing_canvas.translate(5, 3.5)
+        self.assertActionPerformedWith(self.testing_canvas, 'translate', tx=5, ty=3.5)
 
     def test_reset_transform(self):
         self.testing_canvas.reset_transform()
+        self.assertActionPerformedWith(self.testing_canvas, 'reset transform')
--- src/dummy/toga_dummy/widgets/canvas.py
+++ src/dummy/toga_dummy/widgets/canvas.py
@@ -42,25 +42,25 @@ class Canvas(Widget):
         self._action('close path')
 
     def move_to(self, x, y):
-        self._action('move to')
+        self._action('move to', x=x, y=y)
 
     def line_to(self, x, y):
-        self._action('line to')
+        self._action('line to', x=x, y=y)
 
     def bezier_curve_to(self, cp1x, cp1y, cp2x, cp2y, x, y):
-        self._action('bezier curve to')
+        self._action('bezier curve to', cp1x=cp1x, cp1y=cp1y, cp2x=cp2x, cp2y=cp2y, x=x, y=y)
 
     def quadratic_curve_to(self, cpx, cpy, x, y):
-        self._action('quadratic curve to')
+        self._action('quadratic curve to', cpx=cpx, cpy=cpy, x=x, y=y)
 
     def arc(self, x, y, radius, startangle, endangle, anticlockwise):
-        self._action('arc')
+        self._action('arc', x=x, y=y, radius=radius, startangle=startangle, endangle=endangle, anticlockwise=anticlockwise)
 
     def ellipse(self, x, y, radiusx, radiusy, rotation, startangle, endangle, anticlockwise):
-        self._action('ellipse')
+        self._action('ellipse', x=x, y=y, radiusx=radiusx, radiusy=radiusy, rotation=rotation, startangle=startangle, endangle=endangle, anticlockwise=anticlockwise)
 
     def rect(self, x, y, width, height):
-        self._action('rect')
+        self._action('rect', x=x, y=y, width=width, height=height)
 
     # Drawing Paths
 
@@ -77,13 +77,13 @@ class Canvas(Widget):
     # Transformations
 
     def rotate(self, radians):
-        self._action('rotate')
+        self._action('rotate', radians=radians)
 
     def scale(self, sx, sy):
-        self._action('scale')
+        self._action('scale', sx=sx, sy=sy)
 
     def translate(self, tx, ty):
-        self._action('translate')
+        self._action('translate', tx=tx, ty=ty)
 
     def reset_transform(self):
         self._action('reset transform')
