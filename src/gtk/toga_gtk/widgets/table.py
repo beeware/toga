38cc5822e62cce6b20bfc1aa4f6a2b128857ac65
Drew Meier <darius.montez@gmail.com>
2017-11-17 20:32:07 -0500

    Integrate data sources with GTK Table + Tree
    Implement insert/remove/change/change_source for GTK+ Table and Tree
    implementations.
    
    Signed-off-by: Drew Meier <darius.montez@gmail.com>

--- src/gtk/toga_gtk/widgets/table.py
+++ src/gtk/toga_gtk/widgets/table.py
@@ -1,65 +1,80 @@
 from gi.repository import Gtk
 from .base import Widget
+from toga.sources.base import Row
 
 
 class Table(Widget):
     def create(self):
-        self._connections = []
-
-        self.data = Gtk.ListStore(*[str for h in self.interface.headings])
+        self.store = Gtk.ListStore(*[str for h in self.interface.headings])
         # Create a table view, and put it in a scroll view.
         # The scroll view is the native, because it's the outer container.
-        self.table = Gtk.TreeView(self.data)
-        self.selection = self.table.get_selection()
+        self.treeview = Gtk.TreeView(self.store)
+        self.selection = self.treeview.get_selection()
         self.selection.set_mode(Gtk.SelectionMode.SINGLE)
+        self.selection.connect("changed", self._on_select)
 
-        self.columns = []
         for i, heading in enumerate(self.interface.headings):
             renderer = Gtk.CellRendererText()
             column = Gtk.TreeViewColumn(heading, renderer, text=i)
-            self.table.append_column(column)
+            self.treeview.append_column(column)
 
         self.native = Gtk.ScrolledWindow()
         self.native.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
-        self.native.add(self.table)
+        self.native.add(self.treeview)
         self.native.set_min_content_width(200)
         self.native.set_min_content_height(200)
         self.native.interface = self.interface
 
-    def change_source(self, source):
-        for row in self.interface.data:
-            self.data.append(*[getattr(row, attr) for attr in self.interface._accessors])
-
-    def insert(self, item):
-        raise NotImplementedError()
-
-    def change(self, item):
-        raise NotImplementedError()
-
-    def remove(self, item):
-        raise NotImplementedError()
-
-    def clear(self):
-        self.data.clear()
-
-    def set_on_select(self, handler):
-        for conn_id in self._connections:
-            # Disconnect all other on_select handlers, so that if you reassign
-            # the on_select, it doesn't trigger the old ones too.
-            self.selection.disconnect(conn_id)
-            self._connections.remove(conn_id)
-
-        if handler is None:
-            return
-
-        def _handler(selection):
+    def _on_select(self, selection):
+        if hasattr(self.interface, "_on_select") and self.interface.on_select:
             tree_model, tree_iter = selection.get_selected()
 
             if tree_iter:
                 tree_path = tree_model.get_path(tree_iter)
-                index = tree_path.get_indices()[0]
-                handler(self.table, row=index)
+                row = tree_path.get_indices()[0]
             else:
-                handler(self.table, row=None)
+                row = None
+            self.interface.on_select(None, row=row)
 
-        self._connections.append(self.selection.connect("changed", _handler))
+    def _refresh(self):
+        """
+        Synchronize self.data with self.interface.data
+        """
+
+        # Temporarily disconnecting the TreeStore improves performance for large
+        # updates by deferring row rendering until the update is complete.
+        self.treeview.set_model(None) # temporarily disconnect the view
+
+        self.store.clear()
+        for row in self.interface.data:
+            self.store.append(self._row_items(row))
+
+        self.treeview.set_model(self.store)
+
+    # TODO: The interface should provide the row index instead of using this
+    # method to access a private property
+    def _row_index(self, row: Row):
+        return self.interface.data._data.index(row)
+
+    # TODO: The interface should provide the row list items instead of using
+    # this method to access a private property
+    def _row_items(self, row: Row):
+        return [getattr(row, attr) for attr in self.interface._accessors]
+
+    def change_source(self, source):
+        self._refresh()
+
+    def insert(self, index, item: Row):
+        self.store.insert(index, self._row_items(row))
+
+    def change(self, item: Row):
+        self.store[self._row_index(row)] = self._row_items(row)
+
+    def remove(self, index, item: Row):
+        self.store.remove(self.store.get_iter((index,)))
+
+    def clear(self):
+        self.store.clear()
+
+    def set_on_select(self, handler):
+        pass
--- src/gtk/toga_gtk/widgets/tree.py
+++ src/gtk/toga_gtk/widgets/tree.py
@@ -1,44 +1,133 @@
 from gi.repository import Gtk
 from .base import Widget
+from itertools import chain
 
 
 class Tree(Widget):
     def create(self):
-        self.tree = None
-        self.columns = None
-
-        self.data = Gtk.TreeStore(*[str for h in self.interface.headings])
+        self.store = Gtk.TreeStore(*[str for h in self.interface.headings])
         # Create a tree view, and put it in a scroll view.
         # The scroll view is the _impl, because it's the outer container.
-        self.table = Gtk.TreeView(self.data)
+        self.treeview = Gtk.TreeView(self.store)
+        self.selection = self.treeview.get_selection()
+        self.selection.set_mode(Gtk.SelectionMode.SINGLE)
+        self.selection.connect("changed", self._on_select)
 
-        self.columns = []
         for heading in self.interface.headings:
             renderer = Gtk.CellRendererText()
             column = Gtk.TreeViewColumn(heading, renderer, text=0)
-            self.table.append_column(column)
+            self.treeview.append_column(column)
 
         self.native = Gtk.ScrolledWindow()
         self.native.interface = self.interface
         self.native.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
-        self.native.add(self.table)
+        self.native.add(self.treeview)
         self.native.set_min_content_width(200)
         self.native.set_min_content_height(200)
 
+    # TODO: The interface (or data source?) should provide the row list items instead of using
+    # this method to access a private property.
+
+    # toga.sources.base.Row -> list (row's items, converted to string)
+    def _row_items(self, row):
+        return [str(getattr(row, attr)) for attr in self.interface._accessors]
+
+    # sources.base.Node -> int (node's index)
+    def _node_index(self, node):
+        def _parent(node):
+            return getattr(node, "_parent", None) or getattr(node, "_source", None)
+
+        for i, sibling in enumerate(_parent(node)):
+            if sibling is node:
+                return i
+
+    # sources.base.Node -> tuple (node's path)
+    def _node_path(self, node):
+        def f(node, path=[]):
+            if hasattr(node, "_parent"):
+                path.insert(0, self._node_index(node))
+                return f(node._parent, path)
+            else:
+                return path
+
+        return tuple(f(node))
+
+    # sources.base.Node -> Gtk.TreeIter
+    def _node_iter(self, node):
+        if hasattr(node, "_parent"):
+            return self.store.get_iter(self._node_path(node))
+        else:
+            return None
+
+    # tuple (node's path) -> sources.base.Node
+    def _get_node(self, path: tuple):
+        lpath = list(path)
+        lpath.reverse() # [level2, level1, level0]
+
+        def _children(node):
+            return getattr(node, "_children", None) or getattr(node, "_roots", None)
+
+        def f(node, path):
+            if _children(node) and lpath:
+                index = lpath.pop()
+                return f(node[index], lpath)
+            else:
+                return node
+
+        return f(self.interface.data, lpath)
+
+    def _on_select(self, selection):
+        if hasattr(self.interface, "_on_select") and self.interface.on_select:
+            tree_model, tree_iter = selection.get_selected()
+
+            if tree_iter:
+                tree_path = tree_model.get_path(tree_iter)
+                path = tree_path.get_indices()
+            else:
+                path = None
+
+            self.interface.on_select(None, node=self._get_node(path))
+
+    def _refresh(self):
+
+        # Temporarily disconnecting the TreeStore improves performance for large
+        # updates by deferring row rendering until the update is complete.
+        self.treeview.set_model(None)
+
+        self.store.clear()
+
+        def append_node(parent_node, it):
+            for i, child_node in enumerate(parent_node):
+                self.store.append(it, self._row_items(child_node))
+                it = self.store.iter_nth_child(it, i)
+                append_node(child_node, it)
+
+        append_node(self.interface.data, None)
+
+        self.treeview.set_model(self.store)
+
     def change_source(self, source):
-        raise NotImplementedError()
+        self._refresh()
 
     def insert(self, item):
-        raise NotImplementedError()
+        self.store.insert(
+            self._node_iter(item._parent),
+            index,
+            self._row_items(item)
+        )
 
     def change(self, item):
-        raise NotImplementedError()
+        self.store.set(
+            self._node_iter(item._parent),
+            # `TreeModel.set()` expects *flat* column-value pairs
+            *chain.from_iterable(enumerate(self._row_items(node))
+        )
 
     def remove(self, item):
-        raise NotImplementedError()
+        self.store.remove(self._node_iter(item))
 
     def clear(self):
-        self.native.Clear()
+        self.store.clear()
 
     def set_on_select(self, handler):
         pass
