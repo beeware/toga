7f0d2450f2f51a3565f7142d712da8accb6d8b4e
Dan Yeaw <dan@yeaw.me>
2017-11-22 06:21:09 -0500

    Undo change on error
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- examples/button/button/app.py
+++ examples/button/button/app.py
@@ -70,7 +70,7 @@ class StartApp(toga.App):
     def callbackResize(self, button):
         # Some action when you hit the button
         #   In this case the window size will change
-        self.main_window.size(100,100)
+        self.main_window.size = (100,100)
 
 
 def main():
