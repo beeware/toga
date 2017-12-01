3f864d18777e3a1cf6c59e48b4c38b7d57d0819b
星川かえで <futursolo@icloud.com>
2017-11-30 23:35:59 -0500

    Implement WebKit2WebView.

--- src/cocoa/toga_cocoa/widgets/webview.py
+++ src/cocoa/toga_cocoa/widgets/webview.py
@@ -4,6 +4,8 @@ from toga_cocoa.libs import *
 
 from .base import Widget
 
+from concurrent.futures import Future
+
 
 class TogaWebView(WebView):
     @objc_method
@@ -27,7 +29,7 @@ class TogaWebView(WebView):
         return None
 
 
-class WebView(Widget):
+class WebKit1WebView(Widget):
     def create(self):
         self.native = TogaWebView.alloc().init()
         self.native.interface = self.interface
@@ -66,3 +68,78 @@ class WebView(Widget):
         :type  javascript: ``str``
         """
         return self.native.stringByEvaluatingJavaScriptFromString(javascript)
+
+
+if "WKWebView" in vars():
+    class TogaWKWebView(WKWebView):
+        @objc_method
+        def webView_didFinish_(self, navi):
+            if self.interface.on_webview_load:
+            self.interface.on_webview_load(self.interface)
+
+        @objc_method
+        def acceptsFirstResponder(self) -> bool:
+            return True
+
+        @objc_method
+        def keyDown_(self, event) -> None:
+            print('in keyDown', event.keyCode)
+            if self.interface.on_key_down:
+                self.interface.on_key_down(event.keyCode, event.modifierFlags)
+
+        @objc_method
+        def touchBar(self):
+            # Disable the touchbar.
+            return None
+
+    class WebKit2WebView(Widget):
+        def create(self):
+            self.native = TogaWKWebView.alloc().init()
+            self.native.interface = self.interface
+
+            self.native.uiDelegate = self.native
+            self.native.navigationDelegate = self.native
+
+            self.add_constraints()
+
+        def get_dom(self):
+            return self.evaluate(
+                'document.getElementsByTagName("html")[0].outerHTML')
+
+        def set_url(self, value):
+            if value:
+                self.interface.url = value
+
+            request = NSURLRequest.requestWithURL(NSURL.URLWithString(
+                self.interface.url))
+            self.native.load(request)
+
+        def set_content(self, root_url, content):
+            self.native.loadHTMLString_baseURL_(
+                content, NSURL.URLWithString(root_url))
+
+        def set_user_agent(self, value):
+            self.native.customUserAgent = value
+
+        def evaluate(self, js_str):
+            fur = Future()  # type: concurrent.futures.Future
+
+            def when_finish(result, err):
+                if fur.done():
+                    return
+
+                if err:
+                    fur.set_exception(err)
+
+                else:
+                    fur.set_result(result)
+
+            self.native.evaluateJavaScript_completionHandler_(
+                js_str, when_finish)
+
+            return fur
+
+    WebView = WebKit2WebView
+
+else:
+    WebView = WebKit1WebView
--- src/iOS/toga_iOS/widgets/webview.py
+++ src/iOS/toga_iOS/widgets/webview.py
@@ -4,8 +4,10 @@ from toga_iOS.libs import *
 
 from .base import Widget
 
+from concurrent.futures import Future
 
-class TogaWebView(UIWebView):
+
+class TogaUIWebView(UIWebView):
     @objc_method
     def webView_didFinishLoadForFrame_(self, sender, frame) -> None:
         if self.interface.on_webview_load:
@@ -21,9 +23,9 @@ class TogaWebView(UIWebView):
             self.interface.on_key_down(event.keyCode, event.modifierFlags)
 
 
-class WebView(Widget):
+class WebKit1WebView(Widget):
     def create(self):
-        self.native = TogaWebView.alloc().init()
+        self.native = TogaUIWebView.alloc().init()
         self.native.interface = self.interface
         self.native.delegate = self.native
 
@@ -49,3 +51,77 @@ class WebView(Widget):
     def evaluate(self, javascript):
         return self.native.stringByEvaluatingJavaScriptFromString_(javascript)
 
+
+if "WKWebView" in vars():
+    class TogaWKWebView(WKWebView):
+        @objc_method
+        def webView_didFinish_(self, navi):
+            if self.interface.on_webview_load:
+            self.interface.on_webview_load(self.interface)
+
+        @objc_method
+        def acceptsFirstResponder(self) -> bool:
+            return True
+
+        @objc_method
+        def keyDown_(self, event) -> None:
+            print('in keyDown', event.keyCode)
+            if self.interface.on_key_down:
+                self.interface.on_key_down(event.keyCode, event.modifierFlags)
+
+        @objc_method
+        def touchBar(self):
+            # Disable the touchbar.
+            return None
+
+    class WebKit2WebView(Widget):
+        def create(self):
+            self.native = TogaWKWebView.alloc().init()
+            self.native.interface = self.interface
+
+            self.native.uiDelegate = self.native
+            self.native.navigationDelegate = self.native
+
+            self.add_constraints()
+
+        def get_dom(self):
+            return self.evaluate(
+                'document.getElementsByTagName("html")[0].outerHTML')
+
+        def set_url(self, value):
+            if value:
+                self.interface.url = value
+
+            request = NSURLRequest.requestWithURL(NSURL.URLWithString(
+                self.interface.url))
+            self.native.load(request)
+
+        def set_content(self, root_url, content):
+            self.native.loadHTMLString_baseURL_(
+                content, NSURL.URLWithString(root_url))
+
+        def set_user_agent(self, value):
+            self.native.customUserAgent = value
+
+        def evaluate(self, js_str):
+            fur = Future()  # type: concurrent.futures.Future
+
+            def when_finish(result, err):
+                if fur.done():
+                    return
+
+                if err:
+                    fur.set_exception(err)
+
+                else:
+                    fur.set_result(result)
+
+            self.native.evaluateJavaScript_completionHandler_(
+                js_str, when_finish)
+
+            return fur
+
+    WebView = WebKit2WebView
+
+else:
+    WebView = WebKit1WebView
