d655c5d4af6d87edd84cfeddc550c8122fa5a6cc
Dan Yeaw <dan@yeaw.me>
2017-12-14 18:32:03 -0500

    Update __init__ factories also to make consistent across platforms
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- src/django/toga_django/__init__.py
+++ src/django/toga_django/__init__.py
@@ -1,23 +1,61 @@
 from . import django
-from .app import *
+from .app import App, MainWindow
+# from .command import Command
+# from .font import Font
 from .widgets.box import *
 from .widgets.button import *
+# from .widgets.canvas import Canvas
+# from .widgets.detailedlist import DetailedList
+# from .widgets.icon import *
+# from .widgets.image import *
+# from .widgets.imageview import *
 from .widgets.label import *
+# from .widgets.multilinetextinput import *
+# from .widgets.numberinput import NumberInput
+# from .widgets.optioncontainer import *
+# from .widgets.passwordinput import *
+# from .widgets.progressbar import *
+# from .widgets.scrollcontainer import *
+# from .widgets.selection import Selection
+# from .widgets.slider import *
+# from .widgets.splitcontainer import *
+# from .widgets.switch import *
+# from .widgets.table import *
 from .widgets.textinput import *
+# from .widgets.tree import *
 from .widgets.webview import *
-from .window import *
+from .window import Window
 
 
 __all__ = [
     '__version__',
     'django',
-    'App',
-    'Window', 'MainWindow',
+    'App', 'MainWindow',
+    # 'Command',
+    # 'Font',
     'Box',
     'Button',
+    # 'Canvas',
+    # 'DetailedList',
+    # 'Icon',
+    # 'Image',
+    # 'ImageView',
     'Label',
+    # 'MultilineTextInput',
+    # 'NumberInput',
+    # 'OptionContainer',
+    # 'PasswordInput',
+    # 'ProgressBar',
+    # 'ScrollContainer',
+    # 'Selection',
+    # 'Slider',
+    # 'SplitContainer',
+    # 'Switch',
+    # 'Table',
     'TextInput',
+    # 'Tree',
     'WebView',
+    'Window',
 ]
 
 # Examples of valid version strings
