cfd63842e4e5b706a8f0e4df8a2dc4ddc76da32a
Dan Yeaw <dan@yeaw.me>
2017-12-13 21:59:53 -0500

    Revert accidental change
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- docs/conf.py
+++ docs/conf.py
@@ -11,12 +11,11 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 import sys
-
+import os
 sys.path.insert(0, os.path.abspath('../src/core/'))
 
 # -- General configuration -----------------------------------------------------
