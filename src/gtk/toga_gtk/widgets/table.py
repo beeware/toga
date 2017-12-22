2372d65825c87aaa897310efcb16523e2f0ca24d
Russell Keith-Magee <russell@keith-magee.com>
2017-12-22 11:04:33 +0800

    Remove need for recursive search calls to find selected nodes.

--- src/gtk/toga_gtk/widgets/table.py
+++ src/gtk/toga_gtk/widgets/table.py
@@ -5,18 +5,18 @@ from .base import Widget
 
 class Table(Widget):
     def create(self):
-        self.store = Gtk.ListStore(*[str for h in self.interface.headings])
+        self.store = Gtk.ListStore(*[object] + [str for h in self.interface.headings])
 
         # Create a table view, and put it in a scroll view.
         # The scroll view is the native, because it's the outer container.
         self.treeview = Gtk.TreeView(self.store)
         self.selection = self.treeview.get_selection()
         self.selection.set_mode(Gtk.SelectionMode.SINGLE)
-        self.selection.connect("changed", self._on_select)
+        self.selection.connect("changed", self.on_select)
 
         for i, heading in enumerate(self.interface.headings):
             renderer = Gtk.CellRendererText()
-            column = Gtk.TreeViewColumn(heading, renderer, text=i)
+            column = Gtk.TreeViewColumn(heading, renderer, text=i + 1)
             self.treeview.append_column(column)
 
         self.native = Gtk.ScrolledWindow()
@@ -26,39 +26,17 @@ class Table(Widget):
         self.native.set_min_content_height(200)
         self.native.interface = self.interface
 
-    def _on_select(self, selection):
-        if hasattr(self.interface, "_on_select") and self.interface.on_select:
+    def on_select(self, selection):
+        if self.interface.on_select:
             tree_model, tree_iter = selection.get_selected()
-            row = self.get_row(tree_iter)
+            row = tree_model.get(tree_iter, 0)[0]
             self.interface.on_select(None, row=row)
 
     def row_data(self, row):
-        return [
+        return [row] + [
             str(getattr(row, attr))
             for attr in self.interface._accessors
         ]
-
-    # Gtk.TreeIter cannot be compared with __eq__ !!!
-    def compare_tree_iters(self, one, two):
-        return self.store.get_path(one) == self.store.get_path(two)
-
-    def get_row(self, impl):
-        for row in self.interface.data:
-            if impl and self.get_impl(row):
-                if self.compare_tree_iters(impl, self.get_impl(row)):
-                    return row
-
-    def set_impl(self, node, impl):
-        node._impl = getattr(node, '_impl', {})
-        node._impl[self] = impl
-
-    def del_impl(self, node):
-        if hasattr(node, '_impl'):
-            del node._impl[self]
-
-    def get_impl(self, node):
-        if hasattr(node, '_impl'):
-            return node._impl.get(self, None)
 
     def change_source(self, source):
         """
@@ -77,14 +55,17 @@ class Table(Widget):
 
     def insert(self, index, item):
         impl = self.store.insert(index, self.row_data(item))
-        self.set_impl(item, impl)
+        try:
+            item._impl[self] = impl
+        except AttributeError:
+            item._impl = {self: impl}
 
     def change(self, item):
-        self.store[self.get_impl(item)] = self.row_data(item)
+        self.store[item._impl[self]] = self.row_data(item)
 
     def remove(self, item):
         del self.store[self.get_impl(item)]
-        self.del_impl(item)
+        del node._impl[self]
 
     def clear(self):
         self.store.clear()
--- src/gtk/toga_gtk/widgets/tree.py
+++ src/gtk/toga_gtk/widgets/tree.py
@@ -5,18 +5,18 @@ from .base import Widget
 
 class Tree(Widget):
     def create(self):
-        self.store = Gtk.TreeStore(*[str for h in self.interface.headings])
+        self.store = Gtk.TreeStore(*[object] + [str for h in self.interface.headings])
 
         # Create a tree view, and put it in a scroll view.
         # The scroll view is the _impl, because it's the outer container.
         self.treeview = Gtk.TreeView(self.store)
         self.selection = self.treeview.get_selection()
         self.selection.set_mode(Gtk.SelectionMode.SINGLE)
-        self.selection.connect("changed", self._on_select)
+        self.selection.connect("changed", self.on_select)
 
         for i, heading in enumerate(self.interface.headings):
             renderer = Gtk.CellRendererText()
-            column = Gtk.TreeViewColumn(heading, renderer, text=i)
+            column = Gtk.TreeViewColumn(heading, renderer, text=i + 1)
             self.treeview.append_column(column)
 
         self.native = Gtk.ScrolledWindow()
@@ -27,14 +27,10 @@ class Tree(Widget):
         self.native.set_min_content_height(200)
 
     def row_data(self, item):
-        return [
+        return [item] + [
             str(getattr(item, attr))
             for attr in self.interface._accessors
         ]
-
-    # Gtk.TreeIter cannot be compared with __eq__ !!!
-    def compare_tree_iters(self, one, two):
-        return self.store.get_path(one) == self.store.get_path(two)
 
     # TODO: Remove this function once a consistent API exists for
     # checking if a TreeSource OR Node has children.
@@ -43,34 +39,10 @@ class Tree(Widget):
             hasattr(parent, '_children') and parent._children) or (
             hasattr(parent, '_roots') and parent._roots)
 
-    def get_node(self, tree_iter):
-        def iterate_tree(node, root=False):
-            yield node
-            if self.node_has_children(node):
-                for child in node:
-                    yield from iterate_tree(child)
-
-        for node in iterate_tree(self.interface.data, root=True):
-            if self.get_impl(node) and tree_iter:
-                if self.compare_tree_iters(tree_iter, self.get_impl(node)):
-                    return node
-
-    def set_impl(self, node, impl):
-        node._impl = getattr(node, '_impl', {})
-        node._impl[self] = impl
-
-    def del_impl(self, node):
-        if hasattr(node, '_impl'):
-            del node._impl[self]
-
-    def get_impl(self, node):
-        if hasattr(node, '_impl'):
-            return node._impl.get(self, None)
-
-    def _on_select(self, selection):
-        if hasattr(self.interface, "_on_select") and self.interface.on_select:
+    def on_select(self, selection):
+        if hasattr(self.interface, "on_select") and self.interface.on_select:
             tree_model, tree_iter = selection.get_selected()
-            row = self.get_node(tree_iter)
+            row = tree_model.get(tree_iter, 0)[0]
             self.interface.on_select(None, row=row)
 
     def change_source(self, source):
@@ -80,30 +52,32 @@ class Tree(Widget):
 
         self.store.clear()
 
-        def append_node(parent, root=False):
-            if self.node_has_children(parent):
-                for i, child_node in enumerate(parent):
-                    self.insert(parent, i, child_node)
-                    append_node(child_node)
+        def append_node(parent):
+            for i, child_node in enumerate(parent):
+                self.insert(parent, i, child_node)
+                append_node(child_node)
 
-        append_node(self.interface.data, root=True)
+        append_node(self.interface.data)
 
         self.treeview.set_model(self.store)
 
     def insert(self, parent, index, item, **kwargs):
         impl = self.store.insert(
-            self.get_impl(parent),
+            parent._impl[self] if parent else None,
             index,
             self.row_data(item)
         )
-        self.set_impl(item, impl)
+        try:
+            item._impl[self] = impl
+        except AttributeError:
+            item._impl = {self: impl}
 
     def change(self, item):
-        self.store[self.get_impl(item)] = self.row_data(item)
+        self.store[item._impl[self]] = self.row_data(item)
 
     def remove(self, item):
-        del self.store[self.get_impl(item)]
-        self.del_impl(item)
+        del self.store[item._impl[self]]
+        del item._impl[self]
 
     def clear(self):
         self.store.clear()
