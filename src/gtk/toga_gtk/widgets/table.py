c0a04167bc5a9d0a8c3b98337e93a4736640bbb7
Drew Meier <darius.montez@gmail.com>
2017-12-28 22:04:45 -0500

    Remove unnecessary hasattr() for on_select, rename 'row' to 'node'
    Signed-off-by: Drew Meier <darius.montez@gmail.com>

--- src/gtk/toga_gtk/widgets/table.py
+++ src/gtk/toga_gtk/widgets/table.py
@@ -6,7 +6,7 @@ from .tree import Tree
 class Table(Tree):
 
     def on_select(self, selection):
-        if hasattr(self.interface, "on_select") and self.interface.on_select:
+        if self.interface.on_select:
             tree_model, tree_iter = selection.get_selected()
             if tree_iter:
                 row = tree_model.get(tree_iter, 0)[0]
@@ -29,6 +29,9 @@ class Table(Tree):
     def insert(self, index, item, **kwargs):
         super().insert(None, index, item, **kwargs)
 
+    def scroll_to_row(self, row):
+        return NotImplementedError
+
     # =================================
     # UNCHANGED METHODS (inherited from Tree)
     # They are included here only to satisfy the implementation tests, which
@@ -36,9 +39,6 @@ class Table(Tree):
 
     def create(self):
         super().create()
-
-    def scroll_to_row(self, row):
-        super().scroll_to_row(row)
 
     def change(self, item):
         super().change(item)
