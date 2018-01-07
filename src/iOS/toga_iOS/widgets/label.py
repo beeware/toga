750346062d61639a7bdf7108c27f8bb3b4926975
sidhhant <sidhhant.t20@gmail.com>
2018-01-07 16:45:50 +0530

    Ref#299 Only for widgets/label   changed set_text in src/core/toga/widgets/label.py to pass down literal argument   changed each implementation (Cocoa, Gtk+, etc) to use self.interface._text that they use values from interface

--- src/iOS/toga_iOS/widgets/label.py
+++ src/iOS/toga_iOS/widgets/label.py
@@ -31,7 +31,7 @@ class Label(Widget):
             self.native.font = value._impl.native
 
     def set_text(self, value):
-        self.native.setText_(value)
+        self.native.setText_(self.interface._text)
 
     def rehint(self):
         # Width & height of a label is known and fixed.
