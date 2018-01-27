63aea09e319182a516bd7ff4182404e705484b80
Drew Meier <darius.montez@gmail.com>
2018-01-26 21:22:43 -0500

    Update example app for ProgressBar
    - add a ProgressBar for each of the four states:
      1. disabled
      2. indeterminate (continuous pulsing/spinning animation)
      3. inactive determinate (display %, no animation)
      4. working determinate (display %, animate)
    - re-order items
    
    Signed-off-by: Drew Meier <darius.montez@gmail.com>

--- examples/progressbar/progressbar/app.py
+++ examples/progressbar/progressbar/app.py
@@ -6,10 +6,10 @@ class ProgressBarApp(toga.App):
 
     def startup(self):
         # Main window of the application with title and size
-        self.main_window = toga.MainWindow(self.name, size=(400, 400))
+        self.main_window = toga.MainWindow(self.name, size=(500, 500))
         self.main_window.app = self
 
-        self.progress2 = toga.ProgressBar(value=0)
+        self.progress_demo = toga.ProgressBar()
 
         # set up common styls
         label_style = CSS(flex=1, padding_right=24)
@@ -19,27 +19,51 @@ class ProgressBarApp(toga.App):
         self.main_window.content = toga.Box(
             children=[
 
+                toga.Box(style=CSS(padding=24), children=[
+                    toga.Label("Use the +/- buttons to change the progress",
+                               style=label_style),
+
+                    self.progress_demo,
+
+                    toga.Box(
+                        children=[
+                            toga.Button("+", on_press=self.increase_progress,
+                                        style=CSS(margin=8, flex=1)),
+                            toga.Button("-", on_press=self.decrease_progress,
+                                        style=CSS(margin=8, flex=1)),
+                        ],
+                        style=CSS(flex=1, flex_direction="row")
+                    ),
+                ]),
+
                 toga.Box(style=box_style, children=[
                     toga.Label("default ProgressBar", style=label_style),
 
                     toga.ProgressBar(),
                 ]),
 
-                toga.Box(style=CSS(padding=24), children=[
-                    toga.Label("Use the +/- buttons to change the progress",
-                               style=label_style),
+                toga.Box(style=box_style, children=[
+                    toga.Label("disabled ProgressBar", style=label_style),
 
-                    self.progress2,
+                    toga.ProgressBar(max=None,  running=False),
+                ]),
 
-                    toga.Box(
-                        children=[
-                            toga.Button("+", on_press=self.increase_progress2,
-                                        style=CSS(margin=8, flex=1)),
-                            toga.Button("-", on_press=self.decrease_progress2,
-                                        style=CSS(margin=8, flex=1)),
-                        ],
-                        style=CSS(flex=1, flex_direction="row")
-                    ),
+                toga.Box(style=box_style, children=[
+                    toga.Label("indeterminate ProgressBar", style=label_style),
+
+                    toga.ProgressBar(max=None,  running=True),
+                ]),
+
+                toga.Box(style=box_style, children=[
+                    toga.Label("inactive determinate ProgressBar", style=label_style),
+
+                    toga.ProgressBar(max=1, running=False, value=0.5),
+                ]),
+
+                toga.Box(style=box_style, children=[
+                    toga.Label("working determinate ProgressBar", style=label_style),
+
+                    toga.ProgressBar(max=1, running=True, value=0.5),
                 ]),
             ],
             style=CSS(padding=24)
@@ -47,11 +71,11 @@ class ProgressBarApp(toga.App):
 
         self.main_window.show()
 
-    def increase_progress2(self, button, **kw):
-        self.progress2.value += 0.1 * self.progress2.max
+    def increase_progress(self, button, **kw):
+        self.progress_demo.value += 0.1 * self.progress_demo.max
 
-    def decrease_progress2(self, button, **kw):
-        self.progress2.value -= 0.1 * self.progress2.max
+    def decrease_progress(self, button, **kw):
+        self.progress_demo.value -= 0.1 * self.progress_demo.max
 
 
 def main():
