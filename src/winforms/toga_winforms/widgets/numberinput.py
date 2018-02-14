6dcb0a05421a3ed9cb0f3884168cee055a4360b0
obulat <obulat@gmail.com>
2018-02-14 10:43:11 +0300

    Added Numberinput implementation for Winforms

--- src/winforms/toga_winforms/widgets/numberinput.py
+++ src/winforms/toga_winforms/widgets/numberinput.py
@@ -1,4 +1,5 @@
-from toga_winforms.libs import WinForms, ClrDecimal
+from toga_winforms.libs import LEFT, RIGHT, CENTER
+from toga_winforms.libs import WinForms, Convert
 
 from .base import Widget
 
@@ -8,23 +9,34 @@ class NumberInput(Widget):
         self.native = WinForms.NumericUpDown()
 
     def set_readonly(self, value):
-        self.interface.factory.not_implemented('NumberInput.set_readonly()')
+        self.native.ReadOnly = self.interface.readonly
 
     def set_step(self, step):
-        self.interface.factory.not_implemented('NumberInput.set_step()')
+        self.native.Increment = Convert.ToDecimal(self.interface.step)
 
     def set_min_value(self, value):
-        self.interface.factory.not_implemented('NumberInput.set_min_value()')
+        if self.interface.min_value is not None:
+            self.native.Minimum = Convert.ToDecimal(self.interface.min_value)
 
     def set_max_value(self, value):
-        self.interface.factory.not_implemented('NumberInput.set_max_value()')
+        if self.interface.max_value is not None:
+            self.native.Maximum = Convert.ToDecimal(self.interface.max_value)
 
     def set_value(self, value):
         if value is not None and value is not "":
-            self.native.Value = ClrDecimal.Parse(value)
+            self.native.Value = Convert.ToDecimal(self.interface.value)
 
     def set_alignment(self, value):
-        self.interface.factory.not_implemented('NumberInput.set_alignment()')
+        if value is not None:
+            if value == CENTER:
+                self.native.TextAlign = WinForms.HorizontalAlignment.Center
+            elif value == LEFT:
+                self.native.TextAlign = WinForms.HorizontalAlignment.Left
+            elif value == RIGHT:
+                self.native.TextAlign = WinForms.HorizontalAlignment.Right
+            else:
+                self.interface.factory.not_implemented(
+                    'NumberInput.set_alignment() with anything else but center, left and right')
 
     def set_font(self, value):
         self.interface.factory.not_implemented('NumberInput.set_font()')
