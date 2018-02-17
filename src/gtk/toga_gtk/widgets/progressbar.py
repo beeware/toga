4436a52ed7b2687d4e6b2c85aae79b671efee752
Drew Meier <darius.montez@gmail.com>
2018-02-16 23:07:48 -0500

    gtk/ProgressBar: revert back to GObject.timeout_add
    So it turns out GTK methods can only be called on the main thread, so the
    `interval()` function I wrote was causing intermittent crashing. Reverting
    to GObject.timeout_add is the way to go.
    
    Signed-off-by: Drew Meier <darius.montez@gmail.com>

--- src/gtk/toga_gtk/widgets/progressbar.py
+++ src/gtk/toga_gtk/widgets/progressbar.py
@@ -1,34 +1,14 @@
-import threading
-from gi.repository import Gtk
+from gi.repository import Gtk, GObject
 
 from .base import Widget
 
 
-def interval(period, f, *a, **kw):
-    stop_event = threading.Event()
-
-    def stop():
-        stop_event.set()
-
-    def run():
-        f(*a, **kw)
-        if not stop_event.is_set():
-            t = threading.Timer(period, run)
-            t.daemon = True
-            t.start()
-
-    run()
-    return stop
-
-PROGRESSBAR_TICK_INTERVAL = 0.1 # seconds per tick
+PROGRESSBAR_TICK_INTERVAL = 100 # ms per tick
 
 class ProgressBar(Widget):
     def create(self):
         self.native = Gtk.ProgressBar()
         self.native.interface = self.interface
-
-    def _tick(self):
-        self.native.pulse()
 
     def _render_disabled(self):
         self.native.set_fraction(0)
@@ -41,15 +21,14 @@ class ProgressBar(Widget):
             self._render_disabled()
 
     def start(self):
-        def tick():
+        def tick(*a, **kw):
             self.native.pulse()
+            return not self.interface.max or self.interface.is_running
 
-        self.stop_animation = interval(PROGRESSBAR_TICK_INTERVAL, tick)
+        GObject.timeout_add(PROGRESSBAR_TICK_INTERVAL, tick, None)
 
     def stop(self):
-        self.stop_animation()
-
-        def reset():
+        def restore_fraction():
             if self.interface.enabled:
                 # set_value uses self.interface.value, not the parameter.
                 # Therefore, passing None does NOT change the value to None, but it
@@ -58,9 +37,10 @@ class ProgressBar(Widget):
             else:
                 # handle disabled state manually
                 self._render_disabled()
+            return False
 
-        # If `reset()` should be scheduled for two tick intervals in the
+        # If `restore_fraction()` is scheduled for two tick intervals in the
         # future to guarantee that it will execute after the last tick,
         # otherwise the last tick will put the native progress bar back in pulsing
         # mode.
-        threading.Timer(PROGRESSBAR_TICK_INTERVAL * 2, reset).start()
+        GObject.timeout_add(PROGRESSBAR_TICK_INTERVAL * 2, restore_fraction)
