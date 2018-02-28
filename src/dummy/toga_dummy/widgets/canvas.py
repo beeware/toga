07cb34d42b2393fc190d08fdca28872205032ace
Dan Yeaw <dan@yeaw.me>
2018-02-27 22:27:07 -0500

    Cleanup dummy implementation
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- src/dummy/toga_dummy/widgets/canvas.py
+++ src/dummy/toga_dummy/widgets/canvas.py
@@ -7,8 +7,8 @@ class Canvas(Widget):
     def create(self):
         self._action('create Canvas')
 
-    def set_context(self, context, remove=False):
-        self._set_value('context', context, remove)
+    def set_context(self, context):
+        self._set_value('context', context)
 
     def draw_contexts(self, canvas, context):
         self._action('draw contexts', canvas=canvas, context=context)
@@ -19,10 +19,10 @@ class Canvas(Widget):
     def delete(self, draw_command):
         self._action('delete', draw_command=draw_command)
 
-    def line_width(self, width=2.0, remove=False):
-        self._set_value('line_width', width, remove)
+    def line_width(self, width=2.0):
+        self._set_value('line_width', width)
 
-    def fill_style(self, color=None, remove=False):
+    def fill_style(self, color=None):
         if color is not None:
             num = re.search('^rgba\((\d*\.?\d*), (\d*\.?\d*), (\d*\.?\d*), (\d*\.?\d*)\)$', color)
             if num is not None:
@@ -31,7 +31,7 @@ class Canvas(Widget):
                 b = num.group(3)
                 a = num.group(4)
                 rgba = str(r + ', ' + g + ', ' + b + ', ' + a)
-                self._set_value('fill_style', rgba, remove)
+                self._set_value('fill_style', rgba)
             else:
                 pass
                 # Support future colosseum versions
@@ -40,68 +40,68 @@ class Canvas(Widget):
                 #         exec('self._set_value('fill_style', color)
         else:
             # set color to black
-            self._set_value('fill_style', '0, 0, 0, 1', remove)
+            self._set_value('fill_style', '0, 0, 0, 1')
 
-    def stroke_style(self, color=None, remove=False):
-        self.fill_style(color, remove)
+    def stroke_style(self, color=None):
+        self.fill_style(color)
 
-    def close_path(self, remove=False):
-        self._action('close path', remove=remove)
+    def close_path(self):
+        self._action('close path')
 
-    def closed_path(self, x, y, remove=False):
-        self._action('closed path', x=x, y=y, remove=remove)
+    def closed_path(self, x, y):
+        self._action('closed path', x=x, y=y)
 
-    def move_to(self, x, y, remove=False):
-        self._action('move to', x=x, y=y, remove=remove)
+    def move_to(self, x, y):
+        self._action('move to', x=x, y=y)
 
-    def line_to(self, x, y, remove=False):
-        self._action('line to', x=x, y=y, remove=remove)
+    def line_to(self, x, y):
+        self._action('line to', x=x, y=y)
 
-    def bezier_curve_to(self, cp1x, cp1y, cp2x, cp2y, x, y, remove=False):
-        self._action('bezier curve to', cp1x=cp1x, cp1y=cp1y, cp2x=cp2x, cp2y=cp2y, x=x, y=y, remove=remove)
+    def bezier_curve_to(self, cp1x, cp1y, cp2x, cp2y, x, y):
+        self._action('bezier curve to', cp1x=cp1x, cp1y=cp1y, cp2x=cp2x, cp2y=cp2y, x=x, y=y)
 
-    def quadratic_curve_to(self, cpx, cpy, x, y, remove=False):
-        self._action('quadratic curve to', cpx=cpx, cpy=cpy, x=x, y=y, remove=remove)
+    def quadratic_curve_to(self, cpx, cpy, x, y):
+        self._action('quadratic curve to', cpx=cpx, cpy=cpy, x=x, y=y)
 
-    def arc(self, x, y, radius, startangle, endangle, anticlockwise, remove=False):
+    def arc(self, x, y, radius, startangle, endangle, anticlockwise):
         self._action('arc', x=x, y=y, radius=radius, startangle=startangle, endangle=endangle,
-                     anticlockwise=anticlockwise, remove=remove)
+                     anticlockwise=anticlockwise)
 
-    def ellipse(self, x, y, radiusx, radiusy, rotation, startangle, endangle, anticlockwise, remove=False):
+    def ellipse(self, x, y, radiusx, radiusy, rotation, startangle, endangle, anticlockwise):
         self._action('ellipse', x=x, y=y, radiusx=radiusx, radiusy=radiusy, rotation=rotation, startangle=startangle,
-                     endangle=endangle, anticlockwise=anticlockwise, remove=remove)
+                     endangle=endangle, anticlockwise=anticlockwise)
 
-    def rect(self, x, y, width, height, remove=False):
-        self._action('rect', x=x, y=y, width=width, height=height, remove=remove)
+    def rect(self, x, y, width, height):
+        self._action('rect', x=x, y=y, width=width, height=height)
 
     # Drawing Paths
 
-    def fill(self, fill_rule, preserve, remove=False):
-        self._set_value('fill rule', fill_rule, remove)
+    def fill(self, fill_rule, preserve):
+        self._set_value('fill rule', fill_rule)
         if preserve:
-            self._action('fill preserve', remove=remove)
+            self._action('fill preserve')
         else:
-            self._action('fill', remove=remove)
+            self._action('fill')
 
-    def stroke(self, remove=False):
-        self._action('stroke', remove=remove)
+    def stroke(self):
+        self._action('stroke')
 
     # Transformations
 
-    def rotate(self, radians, remove=False):
-        self._action('rotate', radians=radians, remove=remove)
+    def rotate(self, radians):
+        self._action('rotate', radians=radians)
 
-    def scale(self, sx, sy, remove=False):
-        self._action('scale', sx=sx, sy=sy, remove=remove)
+    def scale(self, sx, sy):
+        self._action('scale', sx=sx, sy=sy)
 
-    def translate(self, tx, ty, remove=False):
-        self._action('translate', tx=tx, ty=ty, remove=remove)
+    def translate(self, tx, ty):
+        self._action('translate', tx=tx, ty=ty)
 
-    def reset_transform(self, remove=False):
+    def reset_transform(self):
         self._action('reset transform')
 
-    def write_text(self, text, x, y, font, remove=False):
-        self._action('write text', text=text, x=x, y=y, font=font, remove=remove)
+    def write_text(self, text, x, y, font):
+        self._action('write text', text=text, x=x, y=y, font=font)
 
     def rehint(self):
         self._action('rehint Canvas')
