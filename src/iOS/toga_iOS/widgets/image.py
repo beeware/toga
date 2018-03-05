a59cd7f0246de1c8a3e6083edda7e7ea239f1b7b
Moises Aranas <moises.aranas@gmail.com>
2018-03-05 22:19:53 +0100

    toga-iOS: Updated image.py for some iOS specific behavior when loading images.
    Signed-off-by: Moises Aranas <moises.aranas@gmail.com>

--- src/iOS/toga_iOS/widgets/image.py
+++ src/iOS/toga_iOS/widgets/image.py
@@ -11,7 +11,14 @@ class Image(object):
         self.interface = interface
 
     def load_image(self, path):
+        # If a remote URL is provided, use the download from NSData (similar to toga-cocoa)
         if path.startswith('http://') or path.startswith('https://'):
             self.native = UIImage.imageWithData_(NSData.dataWithContentsOfURL_(NSURL.URLWithString_(path)))
         else:
-            self.native = UIImage.imageNamed_(os.path.basename(path))
+            basename = os.path.basename(path)
+            if basename == path:
+                # if a path isn't provided, assume that the image is bundled into the app
+                self.native = UIImage.imageNamed_(basename)
+            else:
+                # load with the full path
+                self.native = UIImage.alloc().initWithContentsOfFile_(path)
