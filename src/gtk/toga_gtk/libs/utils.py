29e259a4a9eade948a3c05322222b4de75c2fca4
ArchKudo <vgd.vyas17@gmail.com>
2018-03-08 19:10:55 +0530

    Added CSS creation, applicaton helper functions.

--- src/gtk/toga_gtk/libs/utils.py
+++ src/gtk/toga_gtk/libs/utils.py
@@ -1,5 +1,7 @@
-from toga.constants import *
-
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+from toga.constants import LEFT, RIGHT, CENTER, JUSTIFY
 
 try:
     text = unicode
@@ -15,3 +17,41 @@ def gtk_alignment(alignment):
         CENTER: (0.5, 0.5),
         JUSTIFY: (0.0, 0.0),
     }[alignment]
+
+
+def gtk_apply_css(style_context, css_rule: str):
+    """
+    Set custom css styles for Gtk widgets
+
+    Args:
+    style_context (Gtk.StyleContext): CSS is applied th the style context of this widget
+    css_rule (str): Valid Gtk CSS rule
+    """
+
+    # Create style and convert to binary data
+    css = bytes(css_rule, 'utf-8')
+
+    # Load CSS
+    style_provider = Gtk.CssProvider()
+    style_provider.load_from_data(css)
+
+    # Apply CSS to widget
+    Gtk.StyleContext.add_provider(style_context, style_provider,
+                                  Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+
+
+def css_rule_factory(selector, decls):
+    """
+    Generate Gtk CSS from property-value pairs for a single selector
+    Args:
+    selector(str): Properties are applied to this CSS selector
+    decl(dict): CSS property-value pairs
+    Returns:
+    str: Gtk CSS rule
+    """
+    res = selector + ' {\n'
+    for attr, val in decls.items():
+        res += f'\t{attr}: {val};\n'
+    res += '}'
+
+    return res
