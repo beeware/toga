22af9586b2c175276617b98555466b11fb0774d6
obulat <obulat@gmail.com>
2018-03-10 13:47:00 +0300

    Icons implementation in the backend for cocoa, tests adjusted

--- src/cocoa/toga_cocoa/widgets/icon.py
+++ src/cocoa/toga_cocoa/widgets/icon.py
@@ -1,8 +1,15 @@
 from toga_cocoa.libs import NSImage
-
+import os
 
 class Icon:
     def __init__(self, interface):
         self.interface = interface
-        interface.__impl = self
-        self.native = NSImage.alloc().initWithContentsOfFile(interface.filename)
+        interface._impl = self
+        valid_icon_extensions = ('.png', '.bmp', '.icns')
+        file_path, file_extension = os.path.splitext(self.interface.filename)
+
+        if file_extension in valid_icon_extensions:
+            self.native = NSImage.alloc().initWithContentsOfFile(interface.filename)
+        else:
+            # Return tiberius?
+            raise AttributeError("No valid icon format for cocoa")
