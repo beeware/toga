561c98b4fc585723fa478d8660245f325af0f049
Moises Aranas <moises.aranas@gmail.com>
2018-03-25 22:42:11 +0200

    toga-iOS: Changes based on code review - Converted some variables to be instance-only - Removed private methods, since the implementation layer won't be   exposed - Changed the use of visual autolayout constraints to more explicit   constraints creation
    Signed-off-by: Moises Aranas <moises.aranas@gmail.com>

--- src/iOS/toga_iOS/widgets/scrollcontainer.py
+++ src/iOS/toga_iOS/widgets/scrollcontainer.py
@@ -1,11 +1,9 @@
 from rubicon.objc import CGSizeMake
 from toga_iOS.libs import (
     NSLayoutAttributeBottom,
-    NSLayoutAttributeHeight,
     NSLayoutAttributeLeading,
     NSLayoutAttributeTop,
     NSLayoutAttributeTrailing,
-    NSLayoutAttributeWidth,
     NSLayoutConstraint,
     NSLayoutRelationEqual,
     UIScrollView
@@ -19,86 +17,110 @@ from .base import Widget
 
 
 class ScrollContainer(Widget):
-    __current_content = None
-    __scroll_height_constraint = None
-    __scroll_width_constraint = None
-    __vertical_enabled = False
-    __horizontal_enabled = False
-    
-    def __update_content_size(self):
+    def update_content_size(self):
         # We need a layout pass to figure out how big the scrollable area should be
-        self.__current_content.interface.refresh()
+        self.current_content.interface.refresh()
         
         content_width = 0
         padding_horizontal = 0
         content_height = 0
         padding_vertical = 0
         
-        if self.__horizontal_enabled:
-            content_width = self.__current_content.interface.layout.width
-            padding_horizontal = self.__current_content.interface.style.padding_left + self.__current_content.interface.style.padding_right
+        if self.horizontal_enabled:
+            content_width = self.current_content.interface.layout.width
+            padding_horizontal = self.current_content.interface.style.padding_left + self.current_content.interface.style.padding_right
         else:
             content_width = self.native.frame.size.width
         
-        if self.__vertical_enabled:
-            content_height = self.__current_content.interface.layout.height
-            padding_vertical = self.__current_content.interface.style.padding_top + self.__current_content.interface.style.padding_bottom
+        if self.vertical_enabled:
+            content_height = self.current_content.interface.layout.height
+            padding_vertical = self.current_content.interface.style.padding_top + self.current_content.interface.style.padding_bottom
             # pad the scrollview for the statusbar offset
-            padding_vertical = padding_vertical + self.__current_content.viewport.statusbar_height
+            padding_vertical = padding_vertical + self.current_content.viewport.statusbar_height
         else:
             content_height = self.native.frame.size.height
 
         self.native.setContentSize_(CGSizeMake(content_width + padding_horizontal,
                                                content_height + padding_vertical))
 
-    def __constrain_to_scrollview(self, widget):
+    def constrain_to_scrollview(self, widget):
         # The scrollview should know the content size as long as the
         # view contained has an intrinsic size and the constraints are
         # not ambiguous in any axis.
         view = widget.native
-        horizontal = NSLayoutConstraint.constraintsWithVisualFormat_options_metrics_views_(
-            'H:|[view]|',
-            0,
-            None,
-            {'view': view}
+        leading_constraint = NSLayoutConstraint.constraintWithItem_attribute_relatedBy_toItem_attribute_multiplier_constant_(
+            view,
+            NSLayoutAttributeLeading,
+            NSLayoutRelationEqual,
+            self.native,
+            NSLayoutAttributeLeading,
+            1.0,
+            0
         )
-        self.native.addConstraints_(horizontal)
-
-        vertical = NSLayoutConstraint.constraintsWithVisualFormat_options_metrics_views_(
-            'V:|[view]|',
-            0,
-            None,
-            {'view': view}
+        trailing_constraint = NSLayoutConstraint.constraintWithItem_attribute_relatedBy_toItem_attribute_multiplier_constant_(
+            self.native,
+            NSLayoutAttributeTrailing,
+            NSLayoutRelationEqual,
+            view,
+            NSLayoutAttributeTrailing,
+            1.0,
+            0
         )
-        self.native.addConstraints_(vertical)
+        top_constraint = NSLayoutConstraint.constraintWithItem_attribute_relatedBy_toItem_attribute_multiplier_constant_(
+            view,
+            NSLayoutAttributeTop,
+            NSLayoutRelationEqual,
+            self.native,
+            NSLayoutAttributeTop,
+            1.0,
+            0
+        )
+        bottom_constraint = NSLayoutConstraint.constraintWithItem_attribute_relatedBy_toItem_attribute_multiplier_constant_(
+            self.native,
+            NSLayoutAttributeBottom,
+            NSLayoutRelationEqual,
+            view,
+            NSLayoutAttributeBottom,
+            1.0,
+            0
+        )
+        self.native.addConstraints_([
+            leading_constraint,
+            trailing_constraint,
+            top_constraint,
+            bottom_constraint
+        ])
 
     def create(self):
+        self.current_content = None
+        self.vertical_enabled = True
+        self.horizontal_enabled = True
         self.native = UIScrollView.alloc().init()
         self.native.translatesAutoresizingMaskIntoConstraints = False
         self.native.backgroundColor = UIColor.whiteColor
         self.add_constraints()
 
     def set_content(self, widget):
-        if self.__current_content != None:
-            self.__current_content.removeFromSuperview()
-        self.__current_content = widget
+        if self.current_content != None:
+            self.current_content.removeFromSuperview()
+        self.current_content = widget
         self.native.addSubview(widget.native)
         widget.viewport = iOSViewport(self.native)
 
         for child in widget.interface.children:
             child._impl.container = widget
 
-        self.__constrain_to_scrollview(widget)
+        self.constrain_to_scrollview(widget)
 
     def set_vertical(self, value):
-        self.__vertical_enabled = value
-        if self.__current_content:
-            self.__update_content_size()
+        self.vertical_enabled = value
+        if self.current_content:
+            self.update_content_size()
 
     def set_horizontal(self, value):
-        self.__horizontal_enabled = value
-        if self.__current_content:
-            self.__update_content_size()
+        self.horizontal_enabled = value
+        if self.current_content:
+            self.update_content_size()
 
     def rehint(self):
-        self.__update_content_size()
+        self.update_content_size()
