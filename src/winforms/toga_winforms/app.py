78b70244126932d9bac356ffa912df12718ef220
Olga Bulat <obulat@gmail.com>
2018-04-07 16:27:30 +0300

    Move create_menus to app

--- src/winforms/toga_winforms/app.py
+++ src/winforms/toga_winforms/app.py
@@ -21,21 +21,48 @@ class App:
 
         self.interface.commands.add(
             toga.Command(None, 'About ' + self.interface.name, group=toga.Group.HELP),
-
             toga.Command(None, 'Preferences', group=toga.Group.FILE),
             # Quit should always be the last item, in a section on it's own
             toga.Command(lambda s: self.exit(), 'Exit ' + self.interface.name, shortcut='q', group=toga.Group.FILE,
                          section=sys.maxsize),
-
             toga.Command(None, 'Visit homepage', group=toga.Group.HELP)
         )
 
         # Call user code to populate the main window
         self.interface.startup()
-
+        self._menu_items = {}
+        self.create_menus()
         self.interface.main_window._impl.native.Icon = \
             self.interface.icon.bind(self.interface.factory).native
 
+    def create_menus(self):
+        toga.Group.FILE.order = 0
+        # Only create the menu if the menu item index has been created.
+        if hasattr(self, '_menu_items'):
+            menubar = WinForms.MenuStrip()
+            submenu = None
+            for cmd in self.interface.commands:
+                if cmd == toga.GROUP_BREAK:
+                    menubar.Items.Add(submenu)
+                    submenu = None
+                elif cmd == toga.SECTION_BREAK:
+                    submenu.DropDownItems.Add('-')
+                else:
+                    if submenu is None:
+                        submenu = WinForms.ToolStripMenuItem(cmd.group.label)
+                    item = WinForms.ToolStripMenuItem(cmd.label)
+                    item.Click += add_handler(cmd)
+                    cmd._widgets.append(item)
+                    self._menu_items[item] = cmd
+                    # This line may appear redundant, but it triggers the logic
+                    # to force the enabled status on the underlying widgets.
+                    cmd.enabled = cmd.enabled
+                    submenu.DropDownItems.Add(item)
+            if submenu:
+                menubar.Items.Add(submenu)
+
+            self.interface.main_window._impl.native.Controls.Add(menubar)
+            
 
 
     def open_document(self, fileURL):
