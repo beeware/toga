a6482188b346962725cb180ee2e2a21ab53c3f7b
Olga Bulat <obulat@gmail.com>
2018-05-08 19:18:06 +0300

    Add self.interface

--- src/dummy/toga_dummy/command.py
+++ src/dummy/toga_dummy/command.py
@@ -4,4 +4,4 @@ from toga_dummy.utils import LoggedObject
 class Command(LoggedObject):
     def __init__(self, interface):
         super().__init__()
-        
+        self.interface = interface        
