6a1f9aa18e98e6d5edc02dd1424ecc80612720be
Olga Bulat <obulat@gmail.com>
2018-05-09 14:46:47 +0300

    Update on_change for TextInput in Winforms
    Signed-off-by: obulat <obulat@gmail.com>

--- src/winforms/toga_winforms/widgets/textinput.py
+++ src/winforms/toga_winforms/widgets/textinput.py
@@ -1,21 +1,29 @@
-from travertino.size import at_least
-
 from toga_winforms.libs import WinForms
+from travertino.size import at_least
 
 from .base import Widget
 
 
+class TogaTextInput(WinForms.TextBox):
+    def __init__(self, interface):
+        super().__init__()
+        self.Multiline = False
+        self.interface = interface
+
+    def on_text_change(self, sender, event):
+        if self.interface._on_change:
+            self.interface.on_change(self.interface)
+
+
 class TextInput(Widget):
     def create(self):
-        self.native = WinForms.TextBox()
-        self.native.Multiline = False
+        self.native = TogaTextInput(self.interface)
 
     def set_readonly(self, value):
         self.native.ReadOnly = value
 
     def set_placeholder(self, value):
-        # self.native.cell.placeholderString = self._placeholder
-        self.interface.factory.not_implemented('TextInput.set_placeholder()')
+        self.native.Text = self.interface.placeholder
 
     def get_value(self):
         return self.native.Text
@@ -24,7 +32,17 @@ class TextInput(Widget):
         self.native.Text = value
 
     def set_alignment(self, value):
-        self.interface.factory.not_implemented('TextInput.set_alignment()')
+        print('Alignment: ', value)
+        if value == 'center':
+            win_value = WinForms.HorizontalAlignment.Center
+        elif value == 'left':
+            win_value = WinForms.HorizontalAlignment.Left
+        elif value == 'right':
+            win_value = WinForms.HorizontalAlignment.Right
+        else:
+            raise ValueError("Justify alignment is not supported in Windows; "
+                             "choose left, right or center")
+        self.native.TextAlign = win_value
 
     def set_font(self, value):
         self.interface.factory.not_implemented('TextInput.set_font()')
@@ -37,4 +55,4 @@ class TextInput(Widget):
         self.interface.intrinsic.height = self.native.PreferredSize.Height
 
     def set_on_change(self, handler):
-        pass
+        self.native.TextChanged += self.native.on_text_change
