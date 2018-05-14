20017ddc1ac0bdb4028ed0c139d6cfc9d9d206a6
Adam Yala <adamkyala@gmail.com>
2018-05-14 12:01:52 -0400

    fix spelling. conform to pep8
    Signed-off-by: Adam Yala <adamkyala@gmail.com>

--- src/gtk/toga_gtk/widgets/detailedlist.py
+++ src/gtk/toga_gtk/widgets/detailedlist.py
@@ -2,54 +2,54 @@ from gi.repository import Gtk
 
 from .base import Widget
 
+
 class DetailedList(Widget):
-	def create(self):
-		self.native = Gtk.ScrolledWindow()
-		self.native.interface = self.interface
+    def create(self):
+        self.native = Gtk.ScrolledWindow()
+        self.native.interface = self.interface
 
-		#Craete DetailedList
-		self.vbox = Gtk.VBox()
+        # Create DetailedList
+        self.vbox = Gtk.VBox()
 
-		self.native.add_with_viewport(self.vbox)
+        self.native.add_with_viewport(self.vbox)
 
+    def change_source(self, source):
+        # TODO
+        raise NotImplementedError()
 
-	def change_source(self, source):
-		# TODO
-		raise NotImplementedError()
+    def insert(self, index, item):
+        # TODO
+        raise NotImplementedError()
 
-	def insert(self, index, item):
-		# TODO
-		raise NotImplementedError()
+    def change(self, item):
+        # TODO
+        raise NotImplementedError()
 
-	def change(self, item):
-		# TODO
-		raise NotImplementedError()
+    def remove(self, item):
+        # TODO
+        raise NotImplementedError()
 
-	def remove(self, item):
-		#TODO
-		raise NotImplementedError()
+    def clear(self):
+        # TODO
+        raise NotImplementedError()
 
-	def clear(self):
-		#TODO
-		raise NotImplementedError()
+    def set_on_refresh(self, handler):
+        pass
 
-	def set_on_refresh(self, handler):
-		pass
+    def after_on_refresh(self):
+        # TODO
+        raise NotImplementedError()
 
-	def after_on_refresh(self):
-		#TODO
-		raise NotImplementedError()
+    def set_on_select(self, handler):
+        pass
 
-	def set_on_select(self, handler):
-		pass
+    def set_on_delete(self, handler):
+        pass
 
-	def set_on_delete(self, handler):
-		pass
+    def scroll_to_row(self, row):
+        # TODO
+        raise NotImplementedError()
 
-	def scroll_to_row(self, row):
-		# TODO
-		raise NotImplementedError()
-
-	def rehint(self):
-		# TODO
-		raise NotImplementedError()
+    def rehint(self):
+        # TODO
+        raise NotImplementedError()
