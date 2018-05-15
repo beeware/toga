c9c8a1dda9b5e44cd7b78e48a7382340e5526c53
William Clemens <wesclemens@gmail.com>
2018-05-15 08:46:47 -0500

    ImageView example provides rel path not working
    The ImageView example provides a relative path but Image.load_image
    requires absolute path in Cocoa. This patch allows load_image to accept
    a relative path.
    
    Signed-off-by: William Clemens <wesclemens@gmail.com>

--- src/cocoa/toga_cocoa/widgets/image.py
+++ src/cocoa/toga_cocoa/widgets/image.py
@@ -1,3 +1,6 @@
+import os
+
+import toga
 from toga_cocoa.libs import *
 
 
@@ -6,8 +9,14 @@ class Image(object):
         self.interface = interface
         self.interface.impl = self
 
+    def __get_full_path(self, path):
+        if path.startswith(('http://', 'https://')) or os.path.isabs(path):
+            return path
+        else:
+            return os.path.join(toga.App.app_dir, path)
+
     def load_image(self, path):
-        if path.startswith('http://') or path.startswith('https://'):
+        if path.startswith(('http://', 'https://')):
             self.native = NSImage.alloc().initByReferencingURL(NSURL.URLWithString_(path))
         else:
-            self.native = NSImage.alloc().initWithContentsOfFile(path)
+            self.native = NSImage.alloc().initWithContentsOfFile(self.__get_full_path(path))
--- src/iOS/toga_iOS/widgets/image.py
+++ src/iOS/toga_iOS/widgets/image.py
@@ -1,21 +1,19 @@
 import os
+
 import toga
-from toga_iOS.libs import(
-    NSData,
-    NSURL,
-    UIImage
-)
+from toga_iOS.libs import NSURL, NSData, UIImage
+
 
 class Image(object):
     def __init__(self, interface):
         self.interface = interface
 
     def __get_full_path(self, path):
-        if (os.path.isabs(path)):
+        if path.startswith(('http://', 'https://')) or os.path.isabs(path):
             return path
         else:
             return os.path.join(toga.App.app_dir, path)
-    
+
     def load_image(self, path):
         # If a remote URL is provided, use the download from NSData (similar to toga-cocoa)
         if path.startswith('http://') or path.startswith('https://'):
