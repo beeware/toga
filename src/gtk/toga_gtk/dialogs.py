2e621866dc51cab726d717844e80ca4544eb5e3f
davidroeca <david.roeca@gmail.com>
2018-05-22 14:32:44 -0400

    handle suggested name for no file types

--- src/gtk/toga_gtk/dialogs.py
+++ src/gtk/toga_gtk/dialogs.py
@@ -70,7 +70,8 @@ def save_file(window, title, suggested_filename, file_types):
     for x in file_types or []:
         _set_filetype_filter(dialog, x)
 
-    dialog.set_current_name(suggested_filename + "." + file_types[0])
+    dialog.set_current_name(suggested_filename if not file_types else
+                            suggested_filename + "." + file_types[0])
 
     response = dialog.run()
 
