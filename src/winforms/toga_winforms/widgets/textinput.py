8ef157c039b451cdf549f2050cd7fe52adde8fc2
Olga Bulat <obulat@gmail.com>
2018-05-27 20:07:15 +0300

    Added textinput placeholder in winforms
    Changed winforms specific handler name to winforms_ prefixed name.

--- src/winforms/toga_winforms/widgets/textinput.py
+++ src/winforms/toga_winforms/widgets/textinput.py
@@ -1,4 +1,4 @@
-from toga_winforms.libs import WinForms, HorizontalTextAlignment
+from toga_winforms.libs import WinForms, HorizontalTextAlignment, Color
 from travertino.size import at_least
 
 from .base import Widget
@@ -13,7 +13,11 @@ class TextInput(Widget):
         self.native.ReadOnly = value
 
     def set_placeholder(self, value):
-        self.native.Text = self.interface.placeholder
+        if self.interface.placeholder:
+            self.native.Click += self.winforms_Click
+            self.native.KeyDown += self.winforms_KeyDown
+            self.native.Text = self.interface.placeholder
+            self.native.ForeColor = Color.FromName('GRAY')
 
     def get_value(self):
         return self.native.Text
@@ -35,8 +39,17 @@ class TextInput(Widget):
         self.interface.intrinsic.height = self.native.PreferredSize.Height
 
     def set_on_change(self, handler):
-        self.native.TextChanged += self.on_text_change
+        self.native.TextChanged += self.winforms_onTextChanged
 
-    def on_text_change(self, sender, event):
-        if self.interface._on_change:
-            self.interface.on_change(self.interface)
+    def winforms_onTextChanged(self, sender, event):
+         if self.interface._on_change:
+             self.interface.on_change(self.interface)
+
+    def winforms_KeyDown(self, sender, event):
+        self.native.Text = ""
+        self.native.ForeColor = Color.FromName('BLACK')
+        self.native.KeyDown -= self.winforms_KeyDown
+
+    def winforms_Click(self, sender, event):
+        self.native.SelectAll()
+        self.native.Click -= self.winforms_Click
