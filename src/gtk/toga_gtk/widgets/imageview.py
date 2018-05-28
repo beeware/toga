2e2cee93d1ee77d7d3ee023959e772401edeb9dd
Olga Bulat <obulat@gmail.com>
2018-05-28 18:42:35 +0300

    GTK to raise error if image path is not valid

--- src/gtk/toga_gtk/widgets/imageview.py
+++ src/gtk/toga_gtk/widgets/imageview.py
@@ -27,11 +27,11 @@ class ImageView(Widget):
             with urlopen(request) as result:
                 input_stream = Gio.MemoryInputStream.new_from_data(result.read(), None)
                 self._original_pixbuf = GdkPixbuf.Pixbuf.new_from_stream(input_stream, None)
-        elif os.path.isabs(self.image.path):
-            self._original_pixbuf = GdkPixbuf.Pixbuf.new_from_file(self.image.path)
+        full_image_path = self.image.path if os.path.isabs(self.image.path) else os.path.join(toga.App.app_dir, self.image.path)
+        if os.path.isfile(full_image_path):
+            self._original_pixbuf = GdkPixbuf.Pixbuf.new_from_file)
         else:
-            self._original_pixbuf = GdkPixbuf.Pixbuf.new_from_file(os.path.join(toga.App.app_dir, self.image.path))
-
+            raise ValueError("No image file available at ", path)
         self.rehint()
 
     def rehint(self):
