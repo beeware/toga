945c1386b26e89fd69955535c64bb749ba842b91
Dan Yeaw <dan@yeaw.me>
2018-06-04 01:02:25 -0400

    Remove Gtk+ static methods and restore traversal
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- src/gtk/toga_gtk/widgets/canvas.py
+++ src/gtk/toga_gtk/widgets/canvas.py
@@ -40,7 +40,7 @@ class Canvas(Widget):
         """
 
         def draw_callback(canvas, context):
-            for drawing_object in root_context.drawing_objects:
+            for drawing_object in traverse(root_context.drawing_objects):
                 drawing_object(self, native_context=context)
 
         self.native.connect("draw", draw_callback)
@@ -50,42 +50,34 @@ class Canvas(Widget):
 
     # Basic paths
 
-    @staticmethod
-    def new_path(native_context):
+    def new_path(self, native_context):
         native_context.new_path()
 
-    @staticmethod
-    def closed_path(native_context, x, y):
+    def closed_path(self, x, y, native_context):
         native_context.close_path()
 
-    @staticmethod
-    def move_to(native_context, x, y):
+    def move_to(self, x, y, native_context):
         native_context.move_to(x, y)
 
-    @staticmethod
-    def line_to(native_context, x, y):
+    def line_to(self, x, y, native_context):
         native_context.line_to(x, y)
 
     # Basic shapes
 
-    @staticmethod
-    def bezier_curve_to(native_context, cp1x, cp1y, cp2x, cp2y, x, y):
+    def bezier_curve_to(self, cp1x, cp1y, cp2x, cp2y, x, y, native_context):
         native_context.curve_to(cp1x, cp1y, cp2x, cp2y, x, y)
 
-    @staticmethod
-    def quadratic_curve_to(native_context, cpx, cpy, x, y):
+    def quadratic_curve_to(self, cpx, cpy, x, y, native_context):
         native_context.curve_to(cpx, cpy, cpx, cpy, x, y)
 
-    @staticmethod
-    def arc(native_context, x, y, radius, startangle, endangle, anticlockwise):
+    def arc(self, x, y, radius, startangle, endangle, anticlockwise, native_context):
         if anticlockwise:
             native_context.arc_negative(x, y, radius, startangle, endangle)
         else:
             native_context.arc(x, y, radius, startangle, endangle)
 
-    @staticmethod
     def ellipse(
-        native_context,
+        self,
         x,
         y,
         radiusx,
@@ -94,36 +86,34 @@ class Canvas(Widget):
         startangle,
         endangle,
         anticlockwise,
+        native_context,
     ):
         native_context.save()
         native_context.translate(x, y)
         if radiusx >= radiusy:
             native_context.scale(1, radiusy / radiusx)
-            native_context.arc(0, 0, radiusx, startangle, endangle, anticlockwise, native_context)
-        else:
+            self.arc(0, 0, radiusx, startangle, endangle, anticlockwise, native_context)
+        elif radiusy > radiusx:
             native_context.scale(radiusx / radiusy, 1)
-            native_context.arc(0, 0, radiusy, startangle, endangle, anticlockwise, native_context)
+            self.arc(0, 0, radiusy, startangle, endangle, anticlockwise, native_context)
         native_context.rotate(rotation)
         native_context.identity_matrix()
         native_context.restore()
 
-    @staticmethod
-    def rect(native_context, x, y, width, height):
+    def rect(self, x, y, width, height, native_context):
         native_context.rectangle(x, y, width, height)
 
     # Drawing Paths
 
-    @staticmethod
-    def apply_color(native_context, color):
+    def apply_color(self, color, native_context):
         if color is not None:
             native_context.set_source_rgba(*native_color(color))
         else:
             # set color to black
             native_context.set_source_rgba(0, 0, 0, 1.0)
 
-    @staticmethod
-    def fill(native_context, color, fill_rule, preserve):
-        native_context.apply_color(color, native_context)
+    def fill(self, color, fill_rule, preserve, native_context):
+        self.apply_color(color, native_context)
         if fill_rule is "evenodd":
             native_context.set_fill_rule(cairo.FILL_RULE_EVEN_ODD)
         else:
@@ -132,43 +122,37 @@ class Canvas(Widget):
             native_context.fill_preserve()
         else:
             native_context.fill()
-            
-    @staticmethod   
-    def stroke(native_context, color, line_width):
-        native_context.apply_color(color, native_context)
+
+    def stroke(self, color, line_width, native_context):
+        self.apply_color(color, native_context)
         native_context.set_line_width(line_width)
         native_context.stroke()
 
     # Transformations
 
-
-    @staticmethod
-    def rotate(native_context, radians):
+    def rotate(self, radians, native_context):
+        print('Rotate(', radians, ')')
         native_context.rotate(radians)
 
-    @staticmethod
-    def scale(native_context, sx, sy):
+    def scale(self, sx, sy, native_context):
         native_context.scale(sx, sy)
 
-    @staticmethod
-    def translate(native_context, tx, ty):
+    def translate(self, tx, ty, native_context):
         native_context.translate(tx, ty)
 
-    @staticmethod
-    def reset_transform(native_context):
+    def reset_transform(self, native_context):
         native_context.identity_matrix()
 
     # Text
 
-    @staticmethod
-    def write_text(native_context, text, x, y, font):
+    def write_text(self, text, x, y, font, native_context):
         # Set font family and size
         if font:
             write_font = font
-        elif native_context.native.font:
-            write_font = native_context.native.font
-            write_font.family = native_context.native.font.get_family()
-            write_font.size = native_context.native.font.get_size() / SCALE
+        elif self.native.font:
+            write_font = self.native.font
+            write_font.family = self.native.font.get_family()
+            write_font.size = self.native.font.get_size() / SCALE
         native_context.select_font_face(write_font.family)
         native_context.set_font_size(write_font.size)
 
@@ -179,15 +163,14 @@ class Canvas(Widget):
             native_context.text_path(line)
             y += height
 
-    @staticmethod
-    def measure_text(native_context, text, font):
+    def measure_text(self, text, font, native_context):
         # Set font family and size
         if font:
             native_context.select_font_face(font.family)
             native_context.set_font_size(font.size)
-        elif native_context.native.font:
-            native_context.select_font_face(native_context.native.font.get_family())
-            native_context.set_font_size(native_context.native.font.get_size() / SCALE)
+        elif self.native.font:
+            native_context.select_font_face(self.native.font.get_family())
+            native_context.set_font_size(self.native.font.get_size() / SCALE)
 
         x_bearing, y_bearing, width, height, x_advance, y_advance = native_context.text_extents(
             text
@@ -200,3 +183,12 @@ class Canvas(Widget):
         # print("REHINT", self, self.native.get_preferred_width(), self.native.get_preferred_height())
         width = self.native.get_preferred_width()
         height = self.native.get_preferred_height()
+
+
+def traverse(nested_list):
+    if isinstance(nested_list, list):
+        for drawing_object in nested_list:
+            for subdrawing_object in traverse(drawing_object):
+                yield subdrawing_object
+    else:
+        yield nested_list
