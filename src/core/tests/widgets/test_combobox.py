1d4740995c1e183ab1e6597c1bb3ec59b7fdc95b
davidroeca <david.roeca@gmail.com>
2018-06-15 14:46:20 -0400

    pass tests

--- src/core/tests/widgets/test_combobox.py
+++ src/core/tests/widgets/test_combobox.py
@@ -7,27 +7,27 @@ from toga_dummy.utils import TestCase
 class ComboBoxTests(TestCase):
     def setUp(self):
         super().setUp()
-        self.data = ['item_{}'.format(x) for x in range(0, 3)]
+        self.items = ['item_{}'.format(x) for x in range(0, 3)]
         self.combobox = toga.ComboBox(factory=toga_dummy.factory)
-        self.combobox = toga.ComboBox(data=self.data, factory=toga_dummy.factory)
+        self.combobox = toga.ComboBox(items=self.items, factory=toga_dummy.factory)
 
     def test_widget_created(self):
         self.assertEqual(self.combobox._impl.interface, self.combobox)
         self.assertActionPerformed(self.combobox, 'create ComboBox')
 
-    def test_data_were_set(self):
-        actual_fields = [i.field for i in self.combobox.data]
-        self.assertEqual(self.data, actual_fields)
+    def test_items_were_set(self):
+        actual_fields = [i.field for i in self.combobox.items]
+        self.assertEqual(self.items, actual_fields)
         self.assertValueSet(
             self.combobox,
             'source',
-            self.combobox.data
+            self.combobox.items
         )
 
-    def test_set_data(self):
+    def test_set_items(self):
         expected_fields = ['new_item_{}'.format(x) for x in range(0, 3)]
-        self.combobox.data = expected_fields
-        actual_fields = [i.field for i in self.combobox.data]
+        self.combobox.items = expected_fields
+        actual_fields = [i.field for i in self.combobox.items]
         self.assertEqual(expected_fields, actual_fields)
 
     def test_get_selected_item_invokes_impl_method(self):
@@ -35,10 +35,10 @@ class ComboBoxTests(TestCase):
         self.assertValueGet(self.combobox, 'value')
 
     def test_value_set_invokes_impl_methods(self):
-        self.combobox.value = self.data[0]
-        self.assertValueSet(self.combobox, 'value', self.data[0])
-        self.combobox.value = 'not in data'
-        self.assertValueSet(self.combobox, 'value', 'not in data')
+        self.combobox.value = self.items[0]
+        self.assertValueSet(self.combobox, 'value', self.items[0])
+        self.combobox.value = 'not in items'
+        self.assertValueSet(self.combobox, 'value', 'not in items')
 
     def test_on_change(self):
         self.assertIsNone(self.combobox.on_change)
