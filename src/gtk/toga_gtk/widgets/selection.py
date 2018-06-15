fdc73bedbe402627bfa61a2cbc096c82457cbd6a
davidroeca <david.roeca@gmail.com>
2018-06-15 15:43:31 -0400

    add gtk implementation for listsource selection

--- src/gtk/toga_gtk/widgets/selection.py
+++ src/gtk/toga_gtk/widgets/selection.py
@@ -9,28 +9,32 @@ class Selection(Widget):
     def create(self):
         self.native = Gtk.ComboBoxText.new()
         self.native.interface = self.interface
-        self.native.connect("changed", self._on_select)
+        self.native.connect("changed", self.gtk_on_select)
 
         self.rehint()
 
-    def _on_select(self, widget):
+    def gtk_on_select(self, widget):
         if self.interface.on_select:
             self.interface.on_select(widget)
 
-    def remove_all_items(self):
-        # self._text.clear()
+    def change_source(self, source):
         self.native.remove_all()
-
-    def add_item(self, item):
-        # self._text.append(item)
-        self.native.append_text(item)
-
-        # Gtk.ComboBox does not select the first item, so it's done here.
-        if not self.get_selected_item():
-            self.select_item(item)
+        for item in source:
+            self.native.append_text(item.field)
+            # Gtk.ComboBox does not select the first item, so it's done here.
+            if not self.get_selected_item():
+                self.select_item(item.field)
 
     def select_item(self, item):
-        self.native.set_active(self.interface.items.index(item))
+        try:
+            index = next(
+                idx
+                for idx, row in enumerate(self.interface.items)
+                if row.field == item
+            )
+        except StopIteration:
+            raise ValueError('Item {} not found'.format(item))
+        self.native.set_active(index)
 
     def get_selected_item(self):
         return self.native.get_active_text()
@@ -43,5 +47,4 @@ class Selection(Widget):
         self.interface.intrinsic.height = height[1]
 
     def set_on_select(self, handler):
-        # No special handling required
-        pass
+        '''No special handling required'''
