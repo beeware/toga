6499ddf6913b081a77fb1deec616e2a313faf35e
davidroeca <david.roeca@gmail.com>
2018-06-15 23:20:03 -0400

    add listener methods

--- src/gtk/toga_gtk/widgets/selection.py
+++ src/gtk/toga_gtk/widgets/selection.py
@@ -17,15 +17,8 @@ class Selection(Widget):
         if self.interface.on_select:
             self.interface.on_select(widget)
 
-    def change_source(self, source):
-        self.native.remove_all()
-        for item in source:
-            self.native.append_text(item.field)
-            # Gtk.ComboBox does not select the first item, so it's done here.
-            if not self.get_selected_item():
-                self.select_item(item.field)
-
-    def select_item(self, item):
+    def _get_index(self, item):
+        '''Gets the index of the ListSource Row'''
         try:
             index = next(
                 idx
@@ -33,8 +26,32 @@ class Selection(Widget):
                 if row.field == item
             )
         except StopIteration:
-            raise ValueError('Item {} not found'.format(item))
-        self.native.set_active(index)
+            raise ValueError('Item {} not found'.format(item.field))
+        return index
+
+    def insert(self, *, index, item):
+        '''Listener method for ListSource'''
+        self.native.insert_text(index, item.field)
+
+    def remove(self, *, item):
+        '''Listener method for ListSource'''
+        # TODO
+        self.interface.factory.not_implemented('Selection.remove()')
+
+    def clear(self, *, item):
+        '''Listener method for ListSource'''
+        self.native.remove_all()
+
+    def change_source(self, source):
+        self.native.remove_all()
+        for index, row in enumerate(source):
+            self.insert(index=index, item=row)
+            # Gtk.ComboBox does not select the first item, so it's done here.
+            if not self.get_selected_item():
+                self.select_item(row.field)
+
+    def select_item(self, item):
+        self.native.set_active(self._get_index(item))
 
     def get_selected_item(self):
         return self.native.get_active_text()
