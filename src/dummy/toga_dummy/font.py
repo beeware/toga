d42211c0c2e79b729c50bb2f2393155cb9fccd76
Dan Yeaw <dan@yeaw.me>
2018-06-29 23:01:49 -0400

    Remove create string methods from core and dummy
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- src/dummy/toga_dummy/font.py
+++ src/dummy/toga_dummy/font.py
@@ -6,8 +6,5 @@ class Font(LoggedObject):
         super().__init__()
         self.interface = interface
 
-    def create_string(self, text):
-        self._action('create string', text=text)
-
     def measure(self, text, tight=False):
         self._action('measure', text=text, tight=tight)
