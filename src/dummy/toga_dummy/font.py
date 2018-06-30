4a69845645832ece82899730d12b5dd0286d699c
Dan Yeaw <dan@yeaw.me>
2018-06-29 20:38:21 -0400

    Change back example text size, add new create string dummy method
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- src/dummy/toga_dummy/font.py
+++ src/dummy/toga_dummy/font.py
@@ -6,5 +6,8 @@ class Font(LoggedObject):
         super().__init__()
         self.interface = interface
 
+    def create_string(self, text):
+        self._action('create string', text=text)
+
     def measure(self, text, tight=False):
         self._action('measure', text=text, tight=tight)
