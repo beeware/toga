63570bc306e73a7905f557c489a52b703d4f03da
Olga Bulat <obulat@gmail.com>
2018-09-04 12:35:39 +0300

    Update factory.py

--- src/winforms/toga_winforms/factory.py
+++ src/winforms/toga_winforms/factory.py
@@ -6,22 +6,22 @@ from .widgets.button import Button
 # from .widgets.canvas import Canvas
 # from .widgets.detailedlist import DetailedList
 from .widgets.icon import Icon
-from .widgets.image import *
-from .widgets.imageview import *
-from .widgets.label import *
-from .widgets.multilinetextinput import *
+from .widgets.image import Image
+from .widgets.imageview import ImageView
+from .widgets.label import Label
+from .widgets.multilinetextinput import MultilineTextInput
 from .widgets.numberinput import NumberInput
-from .widgets.optioncontainer import *
-from .widgets.passwordinput import *
-from .widgets.progressbar import *
-from .widgets.scrollcontainer import *
+from .widgets.optioncontainer import OptionContainer
+from .widgets.passwordinput import PasswordInput
+from .widgets.progressbar import ProgressBar
+from .widgets.scrollcontainer import ScrollContainer
 from .widgets.selection import Selection
-from .widgets.slider import *
+from .widgets.slider import Slider
 from .widgets.splitcontainer import SplitContainer
 from .widgets.switch import Switch
-from .widgets.table import *
+from .widgets.table import Table
 from .widgets.tree import Tree
-from .widgets.webview import *
+from .widgets.webview import WebView
 from .window import Window
 
 
