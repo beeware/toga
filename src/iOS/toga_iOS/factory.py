e45203b9591c7164b157cd1ab9e1ca9bf34ff269
Russell Keith-Magee <russell@keith-magee.com>
2018-09-23 11:53:27 +0800

    Corrected the use of fonts, colors and images in iOS.

--- src/iOS/toga_iOS/factory.py
+++ src/iOS/toga_iOS/factory.py
@@ -1,10 +1,9 @@
 from .app import App, MainWindow
 # from .command import Command
 
-from .resources.colors import native_color
-from .resources.fonts import Font
-from .resources.icons import Icon
-from .resources.images import Image
+from .fonts import Font
+from .icons import Icon
+from .images import Image
 
 from .widgets.box import Box
 from .widgets.button import Button
