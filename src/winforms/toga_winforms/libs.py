253e57e1b6bbfaf3f6fb7f683bb43f1ee5135659
PythonicAccountant <monkeybowler58@gmail.com>
2018-10-17 18:27:38 -0700

    fixes brutusthebee comments round 3

--- src/winforms/toga_winforms/libs.py
+++ src/winforms/toga_winforms/libs.py
@@ -1,16 +1,16 @@
 import clr
 clr.AddReference("System.Windows.Forms")
 
-import System.Windows.Forms as WinForms # noqa: E402
-from System import Decimal as ClrDecimal # noqa: E402
-from System import Convert # noqa: E402
-from System import DateTime as WinDateTime # noqa: E402
-from System import Threading # noqa: E402
-from System import Uri # noqa: E402
-from System.Drawing import Size, Point, Color, ContentAlignment, Bitmap # noqa: E402
-from System.Drawing import Icon as WinIcon # noqa: E402
-from System.Drawing import Image as WinImage # noqa: E402
-from toga.constants import LEFT, RIGHT, CENTER, JUSTIFY # noqa: E402
+import System.Windows.Forms as WinForms  # noqa: E402
+from System import Decimal as ClrDecimal  # noqa: E402, F401
+from System import Convert  # noqa: E402, F401
+from System import DateTime as WinDateTime  # noqa: E402, F401
+from System import Threading  # noqa: E402, F401
+from System import Uri  # noqa: E402, F401
+from System.Drawing import Size, Point, Color, ContentAlignment, Bitmap  # noqa: E402
+from System.Drawing import Icon as WinIcon  # noqa: E402, F401
+from System.Drawing import Image as WinImage  # noqa: E402, F401
+from toga.constants import LEFT, RIGHT, CENTER, JUSTIFY  # noqa: E402
 
 
 def TextAlignment(value):
