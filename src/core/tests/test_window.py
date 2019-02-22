858d2d8a843737080a06bce037d6a042eba7be47
GrayR00t <ns28@iitbbs.ac.in>
2019-02-22 06:35:09 +0530

    tab and spaces corrcted in code

--- src/core/tests/test_window.py
+++ src/core/tests/test_window.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from unittest.mock import patch, MagicMock
 import toga
 import toga_dummy
@@ -6,6 +8,7 @@ from toga_dummy.utils import TestCase
 
 
 class TestWindow(TestCase):
+
     def setUp(self):
         super().setUp()
         self.window = toga.Window(factory=toga_dummy.factory)
@@ -13,8 +16,10 @@ class TestWindow(TestCase):
     def test_widget_created(self):
         id = self.window.id
         self.assertIsNotNone(self.window.id)
-        app = toga.App('test_name', 'id.app', factory=toga_dummy.factory)
-        new_app = toga.App('error_name', 'id.error', factory=toga_dummy.factory)
+        app = toga.App('test_name', 'id.app',
+                       factory=toga_dummy.factory)
+        new_app = toga.App('error_name', 'id.error',
+                           factory=toga_dummy.factory)
         self.window.app = app
         with self.assertRaises(Exception):
             self.window.app = new_app
@@ -28,14 +33,18 @@ class TestWindow(TestCase):
         self.assertIsInstance(toolbar, CommandSet)
 
     def test_size_getter_and_setter(self):
+
         # Add some content
+
         mock_content = MagicMock(toga.Box(factory=toga_dummy.factory))
         self.window.content = mock_content
 
         # Confirm defaults
+
         self.assertEqual((640, 480), self.window.size)
 
         # Test setter
+
         new_size = (1200, 40)
         mock_content.reset_mock()
         with patch.object(self.window, '_impl'):
@@ -44,10 +53,13 @@ class TestWindow(TestCase):
             self.window.content.refresh.assert_called_once_with()
 
     def test_position_getter_and_setter(self):
+
         # Confirm defaults
+
         self.assertEqual((100, 100), self.window.position)
 
         # Test setter
+
         new_position = (40, 79)
         with patch.object(self.window, '_impl'):
             self.window.position = new_position
@@ -66,10 +78,12 @@ class TestWindow(TestCase):
             self.window._impl.on_close.assert_called_once_with()
 
     def test_info_dialog(self):
+
         # title and message for testing
+
         title = 'checking_info_dialog'
         message = 'completed'
         with patch.object(self.window, '_impl'):
             self.window.info_dialog(title, message)
-            self.window._impl.info_dialog.assert_called_once_with(title, message)
-            
+            self.window._impl.info_dialog.assert_called_once_with(title,
+                    message)
