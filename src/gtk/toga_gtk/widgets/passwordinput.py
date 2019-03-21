68ec17e66e2b82aadffeae9cb1c3ff8cd6c745ef
Moises <moises.aranas@gmail.com>
2019-03-21 22:14:07 +0100

    toga-GTK: Fixed Textinput implementation to add the change handler, and updated Passwordinput to use super.create
    Signed-off-by: Moises <moises.aranas@gmail.com>

--- src/gtk/toga_gtk/widgets/passwordinput.py
+++ src/gtk/toga_gtk/widgets/passwordinput.py
@@ -5,7 +5,5 @@ from .textinput import TextInput
 
 class PasswordInput(TextInput):
     def create(self):
-        self.native = Gtk.Entry()
-        self.native.interface = self.interface
+        super().create()
         self.native.set_visibility(False)
-        self.native.connect('show', lambda event: self.rehint())
