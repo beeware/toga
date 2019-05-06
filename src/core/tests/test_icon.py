2c1494fec57ffeef40fd6fe277974aabc88985dc
nehararora <nehar.arora@gmail.com>
2019-05-06 10:14:38 -0400

    Fix non-system icon path setup

--- src/core/tests/test_icon.py
+++ src/core/tests/test_icon.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 from unittest.mock import MagicMock
 
@@ -12,9 +13,25 @@ class TestIcon(unittest.TestCase):
 
         self.test_path = "Example.bmp"
         self.icon = toga.Icon(self.test_path)
+        self.resource_icon = toga.Icon(self.test_path, system=True)
 
     def test_icon_bind(self):
         self.assertEqual(self.icon._impl, None)
         self.icon.bind(factory=toga_dummy.factory)
         self.assertEqual(self.icon._impl.interface, self.icon)
         self.assertEqual(self.icon.path, self.test_path)
+
+    def test_icon_file(self):
+        """
+        Validate filename property
+
+        :return: None
+        """
+        # Validate file name/path for non-system icon
+        self.assertEqual(self.icon.filename, self.test_path)
+
+        # Test file name/path for system icon
+        toga_dir = os.path.dirname(toga.__file__)
+        icon_path = os.path.join(toga_dir, "resources",  self.test_path)
+
+        self.assertEqual(self.resource_icon.filename, icon_path)
