5a53b83c0d05609c959b3e3cc0ff9366907f15f0
Dylan Jay <software@pretaweb.com>
2019-10-09 14:12:13 +0700

    formatting fixes

--- examples/twobee/twobee/app.py
+++ examples/twobee/twobee/app.py
@@ -4,30 +4,34 @@ import random
 HEADS = toga.Image('https://beeware.org/static/images/brutus-270.png')
 TAILS = toga.Image('https://beeware.org/project/projects/libraries/toga/toga.png')
 TOSS = toga.Image('https://beeware.org/project/projects/tools/cricket/cricket.png')
-IMG_SIZE = toga.style.Pack(height=75,width=75) #TODO be nice if didn't have to specify size
+IMG_SIZE = toga.style.Pack(height=75, width=75) # TODO be nice if didn't have to specify size
+
 
 def build(app):
-        coins = (toga.ImageView(HEADS, style=IMG_SIZE), toga.ImageView(TAILS, style=IMG_SIZE))
-        announce = toga.Label(text="")
-        buttons = (
-            toga.Button("Two Bee", on_press=lambda b: choose(True)), 
-            toga.Button("Two Not Bee", on_press=lambda b: choose(False))
-            )
-        content = toga.Box(children=[announce, toga.Box(children=coins), toga.Box(children=buttons)],
-            style=toga.style.Pack(direction=toga.style.pack.COLUMN, padding=40, alignment=toga.style.pack.CENTER))
-     
-        def choose(pick_heads):
-            buttons[0].enabled, buttons[1].enabled = (not pick_heads, pick_heads) #TODO allow to pick either instead of disable
+    coins = (toga.ImageView(HEADS, style=IMG_SIZE), toga.ImageView(TAILS, style=IMG_SIZE))
+    announce = toga.Label(text="")
+    buttons = (
+        toga.Button("Two Bee", on_press=lambda b: choose(True)),
+        toga.Button("Two Not Bee", on_press=lambda b: choose(False))
+    )
+    content = toga.Box(children=[announce, toga.Box(children=coins), toga.Box(children=buttons)],
+                       style=toga.style.Pack(direction=toga.style.pack.COLUMN, padding=40, alignment=toga.style.pack.CENTER))
 
-            coins[0].image, coins[1].image, announce.text = (TOSS, TOSS, "Come in spinner")
-            yield 2
+    def choose(pick_heads):
+        # TODO allow to pick either instead of disable
+        buttons[0].enabled, buttons[1].enabled = (not pick_heads, pick_heads)
 
-            for coin in coins: coin.image = random.choice([HEADS, TAILS])
-            is_mine = [(c.image == HEADS) == buttons[1].enabled for c in coins]
-            announce.text = "You Win" if all(is_mine) else "Draw" if any(is_mine) else "I Win"
-            yield 5 #TODO: put in loop so works continously   
+        coins[0].image, coins[1].image, announce.text = (TOSS, TOSS, "Come in spinner")
+        yield 2
 
-        return content
+        for coin in coins:
+            coin.image = random.choice([HEADS, TAILS])
+        is_mine = [(c.image == HEADS) == buttons[1].enabled for c in coins]
+        announce.text = "You Win" if all(is_mine) else "Draw" if any(is_mine) else "I Win"
+        yield 5  # TODO: put in loop so works continously
+
+    return content
+
 
 def main():
     toga.App("TwoBee or not TwoBee", 'org.beeware.twobee', startup=build).main_loop()
