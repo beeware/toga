2750055849ab9fa91886944f0a5890c72fe1d679
Olga Bulat <obulat@gmail.com>
2019-10-14 14:02:23 +0300

    Add placeholder implementation to textinput in Winforms
    Signed-off-by: obulat <obulat@gmail.com>

--- src/winforms/toga_winforms/widgets/textinput.py
+++ src/winforms/toga_winforms/widgets/textinput.py
@@ -1,4 +1,4 @@
-from toga_winforms.libs import WinForms, HorizontalTextAlignment
+from toga_winforms.libs import WinForms, HorizontalTextAlignment, Color
 from travertino.size import at_least
 
 from .base import Widget
@@ -8,18 +8,25 @@ class TextInput(Widget):
     def create(self):
         self.native = WinForms.TextBox()
         self.native.Multiline = False
+        self.native.TextChanged += self.winforms_onTextChanged
+        self.native.Leave += self.winforms_onFocusLost
 
     def set_readonly(self, value):
         self.native.ReadOnly = value
 
     def set_placeholder(self, value):
-        self.native.Text = self.interface.placeholder
+        if self.interface.placeholder and not self.interface.value:
+            self.native.Click += self.winforms_Click
+            self.native.Text = self.interface.placeholder
+            self.native.ForeColor = Color.FromName('GRAY')
 
     def get_value(self):
         return self.native.Text
 
     def set_value(self, value):
-        self.native.Text = value
+        if value:
+            self.native.ForeColor = Color.FromName('BLACK')
+            self.native.Text = value
 
     def set_alignment(self, value):
         self.native.TextAlign = HorizontalTextAlignment(value)
@@ -36,8 +43,18 @@ class TextInput(Widget):
         self.interface.intrinsic.height = self.native.PreferredSize.Height
 
     def set_on_change(self, handler):
-        self.native.TextChanged += self.on_text_change
+        pass
 
-    def on_text_change(self, sender, event):
+    def winforms_onTextChanged(self, sender, event):
         if self.interface._on_change:
             self.interface.on_change(self.interface)
+
+    def winforms_Click(self, sender, event):
+        self.native.SelectAll()
+        if self.interface.placeholder and self.get_value() == self.interface.placeholder:
+            self.native.Clear()
+            self.native.ForeColor = Color.FromName('BLACK')
+
+    def winforms_onFocusLost(self, sender, event):
+        if not self.native.Text and self.interface.placeholder:
+            self.set_placeholder(None)
