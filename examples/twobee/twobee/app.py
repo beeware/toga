1096cdc753d75f23ba9a82ca0c701d5591bcf11c
Dylan Jay <software@pretaweb.com>
2019-10-15 12:10:10 +0700

    add in spinning animation

--- examples/twobee/twobee/app.py
+++ examples/twobee/twobee/app.py
@@ -10,23 +10,31 @@ IMG_SIZE = toga.style.Pack(height=75, width=75)  # TODO be nice if didn't have t
 
 def build(_):
     coins = (toga.ImageView(HEADS, style=IMG_SIZE), toga.ImageView(TAILS, style=IMG_SIZE))
-    announce = toga.Label(text="")
+    announce = toga.Label(text="Pick and win if you get two")
     buttons = (
-        toga.Button("Two Bee", on_press=lambda b: choose(True)),
-        toga.Button("Two Not Bee", on_press=lambda b: choose(False))
+        toga.Button("Bee", on_press=lambda b: choose(True)),
+        toga.Button("Not Bee", on_press=lambda b: choose(False))
     )
-    content = toga.Box(children=[announce, toga.Box(children=coins), toga.Box(children=buttons)],
-                       style=Pack(direction=COLUMN, padding=40, alignment=CENTER))
+    content = toga.Box(children=[
+            announce, 
+            toga.Box(children=coins), 
+            toga.Box(children=buttons)
+        ],
+        style=Pack(direction=COLUMN, padding=40, alignment=CENTER)
+        )
 
     def choose(pick_heads):
         # TODO allow to pick either instead of disable
         buttons[0].enabled, buttons[1].enabled = (not pick_heads, pick_heads)
 
-        coins[0].image, coins[1].image, announce.text = (TOSS, TOSS, "Come in spinner")
-        yield 2
+        speed = 0.08
+        while speed < 0.5:
+            coins[0].image, coins[1].image, announce.text = (TOSS, TOSS, "Come in spinner")
+            yield speed
+            coins[0].image, coins[1].image = [random.choice([HEADS, TAILS]) for _ in coins]
+            yield speed
+            speed = speed * 1.2
 
-        for coin in coins:
-            coin.image = random.choice([HEADS, TAILS])
         is_mine = [(c.image == HEADS) == buttons[1].enabled for c in coins]
         announce.text = "You Win" if all(is_mine) else "Draw" if any(is_mine) else "I Win"
         yield 5  # TODO: put in loop so works continously
@@ -35,4 +43,4 @@ def build(_):
 
 
 def main():
-    toga.App("TwoBee or not TwoBee", 'org.beeware.twobee', startup=build).main_loop()
+    return toga.App("TwoBee or not TwoBee", 'org.beeware.twobee', startup=build)
