f54d1f49918975a0b15a791e5cfe84722ea36515
Sam Schott <ss2151@cam.ac.uk>
2020-01-12 14:34:02 +0000

    added divider example

---
+++ examples/divider/README.rst
@@ -0,0 +1,13 @@
+Divider example
+===============
+
+Divider example for the `Toga widget toolkit`_. The following switch features are present on this example:
+
+* Horizontal and vertical dividers
+
+Quickstart
+~~~~~~~~~~
+
+To run this example:
+
+    $ python -m divider
---
+++ examples/divider/divider/app.py
@@ -0,0 +1,48 @@
+import toga
+from toga.style import Pack
+from toga.constants import COLUMN, ROW
+
+class DividerApp(toga.App):
+
+    def startup(self):
+        # Window class
+        #   Main window of the application with title and size
+        self.main_window = toga.MainWindow(title=self.name, size=(300, 150))
+
+        style = Pack(padding_top=24)
+        substyle = Pack(padding_right=24, flex=1)
+
+        # Add the content on the main window
+        self.main_window.content = toga.Box(
+            children=[
+                toga.Label('Section 1'),
+                toga.Divider(style=style),
+                toga.Label('Section 2', style=style),
+                toga.Divider(style=style),
+                toga.Label('Section 3', style=style),
+                toga.Box(
+                    children=[
+                        toga.DetailedList(['List 1'], style=substyle),
+                        toga.Divider(style=substyle),
+                        toga.DetailedList(['List 2'], style=substyle),
+                    ],
+                    style=Pack(direction=ROW, padding=24, flex=1),
+                ),
+            ],
+            style=Pack(direction=COLUMN, padding=24)
+        )
+
+        # Show the main window
+        self.main_window.show()
+
+    def callbackLabel(self, switch):
+        # Some action when you hit the switch
+        #   In this case the label will change
+        switch.label = "switch is %s" % {0: "off", 1: "on"}[switch.is_on]
+
+
+def main():
+    # Application class
+    #   App name and namespace
+    app = DividerApp('Dividers', 'org.beeware.helloworld')
+    return app
