753c0fe94a818739f0ae59849fa030b217526f49
Sam Schott <ss2151@cam.ac.uk>
2020-01-22 00:23:32 +0000

    use `is_running` property in example

--- examples/activityindicator/activityindicator/app.py
+++ examples/activityindicator/activityindicator/app.py
@@ -7,20 +7,16 @@ class ExampleActivityIndicatorApp(toga.App):
     # Button callback functions
     def do_stuff(self, widget, **kwargs):
 
-        if self._running:
+        if self.spinner.is_running:
             self.spinner.stop()
             self.button.label = 'Start'
-            self._running = False
         else:
             self.spinner.start()
             self.button.label = 'Stop'
-            self._running = True
 
     def startup(self):
         # Set up main window
         self.main_window = toga.MainWindow(title=self.name)
-
-        self._running = False
 
         self.spinner = toga.ActivityIndicator(style=Pack(padding_left=10, height=16))
         self.button = toga.Button('Start', on_press=self.do_stuff, style=Pack(padding_right=10))
