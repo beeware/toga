aba092f536dcdc8d54552d3beda4741f75b8b013
Barak Korren <bkorren@redhat.com>
2020-02-09 22:33:34 +0200

    toga_android.paths - Make tests pass
    Add `paths.sys_resource` and `paths.arbitrary` in Android backend to
    make tests pass despite questionable state of that backend.
    
    Signed-off-by: Barak Korren <bkorren@redhat.com>

--- src/android/toga_android/paths.py
+++ src/android/toga_android/paths.py
@@ -21,5 +21,24 @@ class Paths:
     def logs(self):
         return Path.home() / 'Library' / 'Logs' / App.app.app_id
 
+    def sys_resource(self, next_to):
+        """Return a path to a Toga system resource that resides next to the
+        given Python source file
+
+        Args:
+            next_to (str): A Python source file the resource is next to
+        """
+        return Path(next_to).parent
+
+    def arbitrary(self, path):
+        """Return an arbitrary path representing object
+
+        Args:
+            path (str): A string with the path to wrap. If a relative path is
+                        given, it will be interpreted to be relative to the
+                        application module directory.
+        """
+        return self.app / Path(path)
+
 
 paths = Paths()
