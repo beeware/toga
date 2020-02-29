fd29a776db6b57fbbab778468c2e37fe2327a868
Ioannis Chatzikonstantinou <yconst@gmail.com>
2020-02-29 20:35:17 +0200

    use AddRange in table update

--- src/winforms/toga_winforms/widgets/table.py
+++ src/winforms/toga_winforms/widgets/table.py
@@ -23,19 +23,25 @@ class Table(Widget):
 
     def change_source(self, source):
         self.native.Items.Clear()
-        for index, row in enumerate(self.interface.data):
-            row._impl = WinForms.ListViewItem([
+        items = []
+        for row in self.interface.data:
+            _impl = WinForms.ListViewItem([
                 getattr(row, attr) for attr in self.interface._accessors
             ])
-            self.native.Items.Insert(index, row._impl)
+            row._impl = _impl
+            items.append(_impl)
+        self.native.Items.AddRange(items)
 
     def update_data(self):
         self.native.Items.Clear()
-        for index, row in enumerate(self.interface.data):
-            row._impl = WinForms.ListViewItem([
+        items = []
+        for row in self.interface.data:
+            _impl = WinForms.ListViewItem([
                 getattr(row, attr) for attr in self.interface._accessors
             ])
-            self.native.Items.Insert(index, row._impl)
+            row._impl = _impl
+            items.append(_impl)
+        self.native.Items.AddRange(items)
 
     def insert(self, index, item):
         item._impl = WinForms.ListViewItem([
