9022df36c4cc81461acabc75bdf0e5edbcb5c9da
xueqihui <xueqihui@yahoo.com>
2020-04-16 05:00:23 -0500

    expand image class to support two extra format, data and image object
    to expand image class to support the image format from image data (basically from buffer) and from the image object such as UIImage for ios or NSimage for macos

--- src/cocoa/toga_cocoa/images.py
+++ src/cocoa/toga_cocoa/images.py
@@ -1,15 +1,22 @@
-from toga_cocoa.libs import NSImage, NSURL
+from toga_cocoa.libs import NSImage, NSURL,NSData
 
 
 class Image:
-    def __init__(self, interface, path=None, url=None):
+    def __init__(self, interface, path=None, url=None, data=None,image=None):
         self.interface = interface
         self.path = path
         self.url = url
+        self.data = data
+        self.image=image
 
         if path:
             self.native = NSImage.alloc().initWithContentsOfFile(str(path))
         elif url:
             self.native = NSImage.alloc().initByReferencingURL(
-                NSURL.URLWithString_(url)
-            )
+                NSURL.URLWithString_(url))
+        elif data:
+            self.native = NSImage.alloc().initWithData(data)
+        elif image:
+            self.native = NSImage(image)
+            
+            
