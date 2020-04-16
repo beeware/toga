fd5bc714b39238c077a53122ac067e2e054fa50e
Russell Keith-Magee <russell@keith-magee.com>
2020-04-16 16:49:13 +0800

    Tweaked color lookup to avoid an extra comparison.

--- src/winforms/toga_winforms/colors.py
+++ src/winforms/toga_winforms/colors.py
@@ -4,9 +4,10 @@ CACHE = {None: Color.Empty}
 
 
 def native_color(c):
-    color = CACHE.get(c, None)
-    if color is not None:
-        return color
-    color = Color.FromArgb(int(c.rgba.a * 255), c.rgba.r, c.rgba.g, c.rgba.b)
-    CACHE[c] = color
+    try:
+        color = CACHE[c]
+    except KeyError:
+        color = Color.FromArgb(int(c.rgba.a * 255), c.rgba.r, c.rgba.g, c.rgba.b)
+        CACHE[c] = color
+
     return color
