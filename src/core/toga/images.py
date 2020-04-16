065c11b20cbaab6252bcd6b2bd05bb455fb1b995
xueqihui <xueqihui@yahoo.com>
2020-04-16 05:04:58 -0500

    Expand image class to support additional image format
    Expand image class to support additional image format: data (from buffer) or another image object (UIImage for iOS or NSImage for macos)

--- src/core/toga/images.py
+++ src/core/toga/images.py
@@ -6,11 +6,13 @@ class Image:
         (relative or absolute path) or URL (HTTP or HTTPS). Relative paths
         will be interpreted relative to the application module directory.
     """
-    def __init__(self, path):
+    def __init__(self, path,data,image):
         self.path = path
 
         # Resource is late bound.
         self._impl = None
+        self.data = data
+        self.image= image
 
     def bind(self, factory):
         """
@@ -23,16 +25,21 @@ class Image:
         :returns: The platform implementation
         """
         if self._impl is None:
-            if self.path.startswith('http://') or self.path.startswith('https://'):
-                self._impl = factory.Image(interface=self, url=self.path)
+            if self.data:
+                self._impl=factory.Image(interface=self,url=self.path,data=self.data, image=self.image)
+            elif self.uiimage:
+                self._impl=factory.Image(interface=self,url=self.path,data=self.data, image=self.image)
+            elif self.path.startswith('http://') or self.path.startswith('https://'):
+                self._impl = factory.Image(interface=self, url=self.path,data=self.data,image=self.image)
             else:
                 full_path = factory.paths.app / factory.paths.Path(self.path)
+                print(full_path)
                 if not full_path.exists():
                     raise FileNotFoundError(
                         'Image file {full_path!r} does not exist'.format(
                             full_path=full_path
                         )
                     )
-                self._impl = factory.Image(interface=self, path=full_path)
+                self._impl = factory.Image(interface=self, path=full_path,data=self.data, image=self.image)
 
         return self._impl
