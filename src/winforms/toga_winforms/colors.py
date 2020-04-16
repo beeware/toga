111fec96a2c70a0b89bd361e44992e556842a869
Sagi Shadur <saroad2@gmail.com>
2020-04-16 07:14:52 +0300

    call CACHE get once instead of twice

--- src/winforms/toga_winforms/colors.py
+++ src/winforms/toga_winforms/colors.py
@@ -4,8 +4,9 @@ CACHE = {None: Color.Empty}
 
 
 def native_color(c):
-    if c in CACHE:
-        return CACHE[c]
+    color = CACHE.get(c, None)
+    if color is not None:
+        return color
     color = Color.FromArgb(int(c.rgba.a * 255), c.rgba.r, c.rgba.g, c.rgba.b)
     CACHE[c] = color
     return color
