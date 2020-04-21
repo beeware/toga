a4d8035c9102fef94a6ad100f7537a21f45ade3e
Asheesh Laroia <asheesh@asheesh.org>
2020-04-21 00:08:24 -0700

    Implement Icon consistently with other backends, to mollify test suite

--- src/android/toga_android/icons.py
+++ src/android/toga_android/icons.py
@@ -1,7 +1,9 @@
 class Icon:
-    SIZES = None
     EXTENSIONS = ['.png']
-    path = ''
+    SIZES = None
 
-    def __init__(self, interface, **kwargs):
+    def __init__(self, interface, path):
+        self.interface = interface
+        self.interface._impl = self
+        self.path = path
         interface.factory.not_implemented('Icon')
