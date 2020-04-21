53ad0b6e980737e09bd3bfbd49f5090a7760ead9
Asheesh Laroia <asheesh@asheesh.org>
2020-04-21 00:17:04 -0700

    Trivial commit to kick GitHub Actions

--- src/android/toga_android/icons.py
+++ src/android/toga_android/icons.py
@@ -1,9 +1,9 @@
 class Icon:
-    EXTENSIONS = ['.png']
+    EXTENSIONS = [".png"]
     SIZES = None
 
     def __init__(self, interface, path):
         self.interface = interface
         self.interface._impl = self
         self.path = path
-        interface.factory.not_implemented('Icon')
+        interface.factory.not_implemented("Icon")
