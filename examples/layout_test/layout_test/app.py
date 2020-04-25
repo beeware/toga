2a397a895ee0af6584b31acbdcd51bad42ecef13
Sam Schott <ss2151@cam.ac.uk>
2020-04-25 19:12:06 +0200

    add reparent button and disable unneeded buttons

--- examples/layout_test/layout_test/app.py
+++ examples/layout_test/layout_test/app.py
@@ -1,6 +1,6 @@
 import toga
 from toga.style import Pack
-from toga.constants import ROW, COLUMN, CENTER, BLUE
+from toga.constants import ROW, COLUMN, CENTER
 
 
 class ExampleBoxApp(toga.App):
@@ -17,6 +17,7 @@ class ExampleBoxApp(toga.App):
             label='Remove image',
             style=Pack(padding=10, width=120),
             on_press=self.remove_image,
+            enabled=False,
         )
 
         self.button_insert = toga.Button(
@@ -25,23 +26,31 @@ class ExampleBoxApp(toga.App):
             on_press=self.insert_image,
         )
 
+        self.button_reparent = toga.Button(
+            label='Reparent image',
+            style=Pack(padding=10, width=120),
+            on_press=self.reparent_image,
+            enabled=False,
+        )
+
         self.button_add_to_scroll = toga.Button(
             label='Add new label',
             style=Pack(padding=10, width=120),
             on_press=self.add_label,
         )
 
-        self.scroll_box = toga.Box(style=Pack(direction=COLUMN, padding=10, flex=1, color=BLUE))
+        self.scroll_box = toga.Box(children=[], style=Pack(direction=COLUMN, padding=10, flex=1))
         self.scroll_view = toga.ScrollContainer(content=self.scroll_box, style=Pack(width=120))
 
         icon = toga.Icon('')
-        self.image_view = toga.ImageView(icon, style=Pack(padding=10, width=120, height=120))
+        self.image_view = toga.ImageView(icon, style=Pack(padding=10, width=60, height=60))
 
         self.button_box = toga.Box(
             children=[
                 self.button_add,
-                self.button_remove,
                 self.button_insert,
+                self.button_reparent,
+                self.button_remove,
                 self.button_add_to_scroll,
             ],
             style=Pack(direction=COLUMN),
@@ -52,20 +61,45 @@ class ExampleBoxApp(toga.App):
             style=Pack(direction=ROW, padding=10, alignment=CENTER, flex=1)
         )
 
+        # add a couple of labels to get us started
+        for i in range(3):
+            self.add_label()
+
         self.main_window = toga.MainWindow()
         self.main_window.content = self.box
         self.main_window.show()
 
     def add_image(self, sender):
-        self.button_box.add(self.image_view)
+        self.scroll_box.add(self.image_view)
+
+        self.button_reparent.enabled = True
+        self.button_remove.enabled = True
+        self.button_add.enabled = False
+        self.button_insert.enabled = False
 
     def insert_image(self, sender):
-        self.button_box.insert(1, self.image_view)
+        self.scroll_box.insert(1, self.image_view)
+
+        self.button_reparent.enabled = True
+        self.button_remove.enabled = True
+        self.button_add.enabled = False
+        self.button_insert.enabled = False
 
     def remove_image(self, sender):
-        self.button_box.remove(self.image_view)
+        self.image_view.parent.remove(self.image_view)
 
-    def add_label(self, sender):
+        self.button_reparent.enabled = False
+        self.button_remove.enabled = False
+        self.button_add.enabled = True
+        self.button_insert.enabled = True
+
+    def reparent_image(self, sender):
+        if self.image_view.parent is self.button_box:
+            self.scroll_box.insert(0, self.image_view)
+        elif self.image_view.parent is self.scroll_box:
+            self.button_box.add(self.image_view)
+
+    def add_label(self, sender=None):
         new_label = toga.Label(
             'Label {}'.format(len(self.scroll_box.children)),
             style=Pack(padding=2, width=70)
