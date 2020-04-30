28e85ed46548637dba72be64bdf9f47bbce81dc3
Asheesh Laroia <asheesh@asheesh.org>
2020-04-29 19:44:07 -0700

    Use `Window` as container; set `viewport` everywhere
    Also:
    
    - Use Android's built-in way to find the DPI for self.dpi
      (this reports 420 on my Android Virtual Device)
    
    - Measure width & height as needed in `AndroidViewport`
    
    - Remove now-unnecessary `add_child()` method on `Box`; we can rely on
      the base `Widget` class's existing `add_child()`.
    
    This implementation waits for `show()` before actually making the window
    visible. This works great, but it has the mild downside that it's possible
    to `show()` a `Window` that has no widgets inside it, which is somewhat
    ill-defined.

--- src/android/toga_android/window.py
+++ src/android/toga_android/window.py
@@ -1,22 +1,22 @@
 class AndroidViewport:
     def __init__(self, native):
         self.native = native
-        self.dpi = 96  # FIXME This is almost certainly wrong...
-        # self.dpi = ... self.interface.app._impl.device_scale
+        self.dpi = self.native.getContext().getResources().getDisplayMetrics().densityDpi
 
     @property
     def width(self):
-        return self.native.ClientSize.Width
+        return self.native.getMeasuredWidth()
 
     @property
     def height(self):
-        return self.native.ClientSize.Height
+        return self.native.getMeasuredHeight()
 
 
 class Window:
     def __init__(self, interface):
         self.interface = interface
         self.interface._impl = self
+        self.widget = None
         self.create()
 
     def create(self):
@@ -26,7 +26,9 @@ class Window:
         self.app = app
 
     def set_content(self, widget):
-        self.app.native.setContentView(widget.native)
+        self.widget = widget
+        # Set the widget's viewport to be based on the window's content.
+        self.widget.viewport = AndroidViewport(self.widget.native)
 
     def set_title(self, title):
         pass
@@ -41,7 +43,18 @@ class Window:
         pass
 
     def show(self):
-        pass
+        if not self.widget:
+            print("[Android] Showing an empty window is not supported.")
+            return
+
+        # Set the app's entire contentView to this window: only one Window can
+        # be displayed at a time.
+        self.app.native.setContentView(self.widget.native)
+
+        # Attach child widgets to the this window as their container.
+        for child in self.widget.interface.children:
+            child._impl.container = self.widget
+            child._impl.viewport = self.widget.viewport
 
     def set_full_screen(self, is_full_screen):
         self.interface.factory.not_implemented('Window.set_full_screen()')
