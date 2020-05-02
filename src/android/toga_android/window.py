f932ae008fbceadc8b89c30a38a3d1aeb36c060b
Asheesh Laroia <asheesh@asheesh.org>
2020-05-01 21:04:01 -0700

    Call Android setContentView() in Toga set_content()

--- src/android/toga_android/window.py
+++ src/android/toga_android/window.py
@@ -26,9 +26,17 @@ class Window:
         self.app = app
 
     def set_content(self, widget):
-        self.widget = widget
         # Set the widget's viewport to be based on the window's content.
-        self.widget.viewport = AndroidViewport(self.widget.native)
+        widget.viewport = AndroidViewport(widget.native)
+        # Set the app's entire contentView to this window. This means that calling
+        # Window.set_content() on any Window object automatically updates the app,
+        # meaning that every Window object acts as the MainWindow.
+        self.app.native.setContentView(widget.native)
+
+        # Attach child widgets to the this window as their container.
+        for child in widget.interface.children:
+            child._impl.container = widget
+            child._impl.viewport = widget.viewport
 
     def set_title(self, title):
         pass
@@ -43,18 +51,7 @@ class Window:
         pass
 
     def show(self):
-        if not self.widget:
-            print("[Android] Showing an empty window is not supported.")
-            return
-
-        # Set the app's entire contentView to this window: only one Window can
-        # be displayed at a time.
-        self.app.native.setContentView(self.widget.native)
-
-        # Attach child widgets to the this window as their container.
-        for child in self.widget.interface.children:
-            child._impl.container = self.widget
-            child._impl.viewport = self.widget.viewport
+        pass
 
     def set_full_screen(self, is_full_screen):
         self.interface.factory.not_implemented('Window.set_full_screen()')
