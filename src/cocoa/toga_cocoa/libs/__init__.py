44a9f801a33c7cb63210e92069942e611aa89555
Russell Keith-Magee <russell@keith-magee.com>
2020-05-23 07:42:44 +0800

    Added some flake8 silencers.

--- src/cocoa/toga_cocoa/libs/__init__.py
+++ src/cocoa/toga_cocoa/libs/__init__.py
@@ -1,8 +1,8 @@
-from .appkit import *
-from .core_graphics import *
-from .core_text import *
-from .foundation import *
-from .webkit import *
+from .appkit import *  # noqa: E401, F403
+from .core_graphics import *  # noqa: E401, F403
+from .core_text import *  # noqa: E401, F403
+from .foundation import *  # noqa: E401, F403
+from .webkit import *  # noqa: E401, F403
 
 # macOS always renders at 96dpi. Scaling is handled
 # transparently at the level of the screen compositor.
--- src/gtk/toga_gtk/libs/__init__.py
+++ src/gtk/toga_gtk/libs/__init__.py
@@ -1,5 +1,5 @@
-from .gtk import *
-from .utils import *
+from .gtk import *  # noqa: E401, F403
+from .utils import *  # noqa: E401, F403
 
 # GDK/GTK always renders at 96dpi. When HiDPI mode is enabled, it is
 # managed at the compositor level. See
