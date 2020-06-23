7be81083394c01eb7ab7cafd981c4cf4262bd6e3
Maradonna <marco.jendryczko@gmx.net>
2020-06-23 10:50:43 +0200

    added test_window and moved content

---
+++ src/winforms/tests/test_window.py
@@ -0,0 +1,13 @@
+from unittest.mock import patch, MagicMock
+from toga_dummy.utils import TestCase
+import toga_dummy
+
+class TestWindow(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.window = toga.Window(factory=toga_dummy.factory)
+
+    def test_build_filter(self):
+        test_filter = self.window.build_filter(["txt"])
+        self.assertEqual(test_filter, "txt files (*.txt)|*.txt|All files (*.*)|*.*")
+
