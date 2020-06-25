05ad5ab23d5544d0eca7d8e4668f6a5d0b2a6a4a
Marco Jendryczko <marco.jendryczko@gmx.net>
2020-06-25 12:46:50 +0200

    remove test from test_implementation

--- src/winforms/tests/test_implementation.py
+++ src/winforms/tests/test_implementation.py
@@ -11,24 +11,3 @@ globals().update(
         )
     )
 )
-
-from unittest.mock import patch, MagicMock
-import toga
-import toga_winforms
-import toga_dummy
-from toga.command import CommandSet
-from toga_dummy.utils import TestCase
-
-class TestWindow(TestCase):
-    def setUp(self):
-        super().setUp()
-        self.window = toga.MainWindow(title="title")
-
-    def test_open_file_dialog_with_custom_file_types_no_multiselect(self):
-        title = "Open RTF file"
-        init_dir = None
-        multiselect = False
-        file_types = ['rtf']
-        #TODO: Mock the waiting function of winforms?
-        #mock.patch('tenacity.BaseRetrying.wait', side_effect=lambda *args, **kwargs: 0)
-        self.window.open_file_dialog(title, init_dir, file_types, multiselect)
