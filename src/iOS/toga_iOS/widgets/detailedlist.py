609ddcf19a976085d5446c63947a9bec05debd88
Russell Keith-Magee <russell@keith-magee.com>
2020-09-13 15:53:02 +0800

    Updated iOS implementation of DetailedList.

--- src/iOS/toga_iOS/widgets/detailedlist.py
+++ src/iOS/toga_iOS/widgets/detailedlist.py
@@ -48,13 +48,13 @@ class TogaTableViewController(UITableViewController):
     @objc_method
     def tableView_commitEditingStyle_forRowAtIndexPath_(self, tableView, editingStyle: int, indexPath):
         if editingStyle == UITableViewCellEditingStyleDelete:
-            item = self.interface.data.row(indexPath.row)
+            item = self.interface.data[indexPath.row]
             if editingStyle == UITableViewCellEditingStyleDelete:
                 if self.interface.on_delete:
-                    self.interface.on_delete(self.interface, row=indexPath.row)
+                    self.interface.on_delete(self.interface, row=item)
 
                 tableView.beginUpdates()
-                self.interface.data.remove(item, refresh=False)
+                self.interface.data.remove(item)
                 tableView.deleteRowsAtIndexPaths_withRowAnimation_([indexPath], UITableViewRowAnimationLeft)
                 tableView.endUpdates()
             elif editingStyle == UITableViewCellEditingStyleInsert:
@@ -68,8 +68,14 @@ class TogaTableViewController(UITableViewController):
 
     @objc_method
     def tableView_willSelectRowAtIndexPath_(self, tableView, indexPath):
+        index = indexPath.row
+        if index == -1:
+            selection = None
+        else:
+            selection = self.interface.data[index]
+
         if self.interface.on_select:
-            self.interface.on_select(self.interface, row=indexPath.row)
+            self.interface.on_select(self.interface, row=selection)
 
     # @objc_method
     # def tableView_heightForRowAtIndexPath_(self, tableView, indexPath) -> float:
@@ -118,6 +124,13 @@ class DetailedList(Widget):
 
     def clear(self):
         self.native.reloadData()
+
+    def get_selection(self):
+        index = self.native.indexPathForSelectedRow.row
+        if index != -1:
+            return self.interface.data[index]
+        else:
+            return None
 
     def set_on_select(self, handler):
         # No special handling required
