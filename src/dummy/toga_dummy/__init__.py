714fe767330f1abe3aa80c19f48c9e965c1e7c4a
Russell Keith-Magee <russell@keith-magee.com>
2020-09-13 16:34:58 +0800

    Bumped version number for v0.3.0.dev24 development.

--- src/dummy/toga_dummy/__init__.py
+++ src/dummy/toga_dummy/__init__.py
@@ -8,4 +8,4 @@ from . import factory  # noqa: F401
 # __version__ = '1.2.3'       # Final Release
 # __version__ = '1.2.3.post1' # Post Release 1
 
-__version__ = '0.3.0.dev23'
+__version__ = '0.3.0.dev24'
