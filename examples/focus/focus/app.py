39a6893e5a2b3d500bb4291e43c6395730fe24cf
Sagi Shadur <saroad2@gmail.com>
2020-09-20 09:30:30 +0300

    Add more demonstrations to the focus example

--- examples/focus/focus/app.py
+++ examples/focus/focus/app.py
@@ -1,4 +1,9 @@
+from travertino.constants import COLUMN
+
 import toga
+from toga.style import Pack
+
+WIDGETS_GROUP = toga.Group("Widgets", order=2)
 
 
 class ExampleFocusApp(toga.App):
@@ -15,42 +20,77 @@ class ExampleFocusApp(toga.App):
         self.a_button = toga.Button("A", on_press=self.on_button_press)
         self.b_button = toga.Button("B", on_press=self.on_button_press)
         self.c_button = toga.Button("C", on_press=self.on_button_press)
+        self.text_input = toga.TextInput(
+            placeholder="I get focused on startup.",
+            style=Pack(height=25, width=200, font_size=10)
+        )
+        self.switch = toga.Switch("Switch", on_toggle=self.on_switch_toggle)
+        self.info_label = toga.Label(
+            "Use keyboard shortcuts to focus on the different widgets",
+            style=Pack(font_size=10)
+        )
         # Add the content on the main window
         self.main_window.content = toga.Box(
-            children=[self.a_button, self.b_button, self.c_button]
+            style=Pack(direction=COLUMN), children=[
+                toga.Box(children=[self.a_button, self.b_button, self.c_button]),
+                toga.Box(children=[self.text_input]),
+                toga.Box(children=[self.switch]),
+                toga.Box(children=[self.info_label])
+            ]
         )
 
-        BUTTONS_GROUP = toga.Group("Buttons", order=2)
         self.commands.add(
             toga.Command(
-                lambda widget: self.focus_on(self.a_button),
+                lambda widget: self.focus_with_label(self.a_button),
                 label="Focus on A",
                 shortcut=toga.Key.MOD_1 + "a",
-                group=BUTTONS_GROUP
+                group=WIDGETS_GROUP
             ),
             toga.Command(
-                lambda widget: self.focus_on(self.b_button),
+                lambda widget: self.focus_with_label(self.b_button),
                 label="Focus on B",
                 shortcut=toga.Key.MOD_1 + "b",
-                group=BUTTONS_GROUP
+                group=WIDGETS_GROUP
             ),
             toga.Command(
-                lambda widget: self.focus_on(self.c_button),
+                lambda widget: self.focus_with_label(self.c_button),
                 label="Focus on C",
                 shortcut=toga.Key.MOD_1 + "c",
-                group=BUTTONS_GROUP
+                group=WIDGETS_GROUP
+            ),
+            toga.Command(
+                lambda widget: self.focus_on(self.text_input, "TextInput"),
+                label="Focus on text input",
+                shortcut=toga.Key.MOD_1 + "t",
+                group=WIDGETS_GROUP
+            ),
+            toga.Command(
+                lambda widget: self.focus_with_label(self.switch),
+                label="Focus on switch",
+                shortcut=toga.Key.MOD_1 + "s",
+                group=WIDGETS_GROUP
             )
         )
         # Show the main window
         self.main_window.show()
 
-    @classmethod
-    def on_button_press(cls, widget: toga.Button):
-        print(widget.label, "was pressed.")
+        self.text_input.focus()
 
-    @classmethod
-    def focus_on(cls, button: toga.Button):
-        button.focus()
+    def on_button_press(self, widget: toga.Button):
+        self.info_label.text = "{widget_label} was pressed!".format(
+            widget_label=widget.label
+        )
+
+    def on_switch_toggle(self, widget: toga.Switch):
+        on_off = "on" if widget.is_on else "off"
+        self.info_label.text = "Switch turned {on_off}!".format(on_off=on_off)
+
+    def focus_on(self, widget: toga.Widget, name: str):
+        widget.focus()
+        self.info_label.text = "{name} is focused!".format(name=name)
+
+    def focus_with_label(self, widget: toga.Widget):
+        self.focus_on(widget, name=widget.label)
 
 
 def main():
