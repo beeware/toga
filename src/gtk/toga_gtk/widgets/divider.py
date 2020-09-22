eeb6fdef812cf024b69bcbbe48e31be7c3bfa7c8
Sam Schott <ss2151@cam.ac.uk>
2020-09-22 23:22:55 +0100

    renamed argument

--- src/gtk/toga_gtk/widgets/divider.py
+++ src/gtk/toga_gtk/widgets/divider.py
@@ -20,8 +20,8 @@ class Divider(Widget):
             self.interface.intrinsic.width = at_least(width[0])
             self.interface.intrinsic.height = height[1]
 
-    def set_direction(self, direction):
-        if direction == self.interface.VERTICAL:
+    def set_direction(self, value):
+        if value == self.interface.VERTICAL:
             self.native.set_orientation(Gtk.Orientation.VERTICAL)
         else:
             self.native.set_orientation(Gtk.Orientation.HORIZONTAL)
