f72a3528658b16c67900e29738a64cbd36bab776
Sam Schott <ss2151@cam.ac.uk>
2020-09-23 23:48:33 +0100

    support high-dpi screens in toga_gtk.ImageView

--- src/gtk/toga_gtk/widgets/imageview.py
+++ src/gtk/toga_gtk/widgets/imageview.py
@@ -1,5 +1,5 @@
 
-from ..libs import GdkPixbuf, Gtk
+from ..libs import GdkPixbuf, Gtk, Gdk
 from .base import Widget
 
 
@@ -16,15 +16,28 @@ class ImageView(Widget):
         self._pixbuf = image._impl.native
 
     def rehint(self):
-        height, width = self._resize_max(
-            original_height=self._pixbuf.get_height(),
-            original_width=self._pixbuf.get_width(),
-            max_height=self.native.get_allocated_height(),
-            max_width=self.native.get_allocated_width()
-        )
+        if self._pixbuf:
+            height, width = self._resize_max(
+                original_height=self._pixbuf.get_height(),
+                original_width=self._pixbuf.get_width(),
+                max_height=self.native.get_allocated_height(),
+                max_width=self.native.get_allocated_width()
+            )
 
-        scaled_pixbuf = self._pixbuf.scale_simple(width, height, GdkPixbuf.InterpType.BILINEAR)
-        self._image.set_from_pixbuf(scaled_pixbuf)
+            dpr = self.native.get_scale_factor()
+
+            scaled_pixbuf = self._pixbuf.scale_simple(
+                width * dpr,
+                height * dpr,
+                GdkPixbuf.InterpType.BILINEAR
+            )
+
+            surface = Gdk.cairo_surface_create_from_pixbuf(
+                scaled_pixbuf,
+                0,  # scale: 0 = same as window
+                self.native.get_window()
+            )
+            self._image.set_from_surface(surface)
 
     @staticmethod
     def _resize_max(original_height, original_width, max_height, max_width):
