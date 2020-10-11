baa70df3a18b2d652546a8d39e5eb8d5f6ab962e
Sagi Shadur <saroad2@gmail.com>
2020-10-11 22:56:00 +0300

    Add DocumentApp example

---
+++ examples/document_app/README.rst
@@ -0,0 +1,13 @@
+Document Apps example
+===============
+
+This is example of a document.
+When you load a txt file, it's content will be loaded to the text input.
+The app saved the last three files loaded
+
+Quickstart
+~~~~~~~~~~
+
+To run this example:
+
+    $ python -m document_app
---
+++ examples/document_app/document_app/app.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+
+import toga
+from toga.constants import COLUMN
+from toga.style import Pack
+
+
+class ExampleDocumentApp(toga.DocumentApp):
+    def startup(self):
+        self.main_window = toga.MainWindow(
+            title=self.name, size=(800, 500),
+            resizeable=False, minimizable=False
+        )
+
+        self.text_input = toga.MultilineTextInput()
+        outer_box = toga.Box(
+            style=Pack(direction=COLUMN),
+            children=[
+                toga.Label("Hello!"),
+                self.text_input
+            ]
+        )
+
+        self.main_window.content = outer_box
+
+        # Show the main window
+        self.main_window.show()
+
+
+def load_file(app, file_path):
+    with open(file_path, mode="r") as file_object:
+        app.text_input.value = file_object.read()
+
+
+def main():
+    # Application class
+    #   App name and namespace
+    app = ExampleDocumentApp(
+        'DocumentApp',
+        'org.beeware.widgets.document_app',
+        recent_documents_size=3,
+        load_document=load_file,
+        initial_directory=str(Path(__file__).parent.parent / "resources"),
+        document_types=["txt"]
+    )
+    return app
