e9751c3a686a4d3936602e0e8ae04b72f5827216
Sam Schott <ss2151@cam.ac.uk>
2020-10-20 12:18:03 +0100

    show readme of example

--- examples/examples_overview/examples_overview/app.py
+++ examples/examples_overview/examples_overview/app.py
@@ -1,9 +1,10 @@
 import sys
 import os
 import subprocess
+from pathlib import Path
 import toga
 from toga.style import Pack
-from toga.constants import COLUMN, ROW, RIGHT
+from toga.constants import COLUMN
 
 
 examples_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
@@ -19,59 +20,87 @@ class ExampleExamplesOverviewApp(toga.App):
         env["PYTHONPATH"] = row.path
 
         self._process = subprocess.Popen(
-            [sys.executable, '-m', row.name],
+            [sys.executable, "-m", row.name],
             env=env,
         )
+
+    def reset_code(self, widget, **kwargs):
+        pass
+
+    def on_example_selected(self, widget, row):
+
+        readme_path = row.path / "README.rst"
+
+        if readme_path.is_file():
+            with open(readme_path) as f:
+                readme_text = f.read()
+        else:
+            readme_text = "No README found"
+
+        self.info_view.value = readme_text
 
     def startup(self):
 
         self._process = None
 
-        # Set up main window
+        # ==== Set up main window ======================================================
+
         self.main_window = toga.MainWindow(title=self.name)
 
         # Label for user instructions
-        self.label = toga.Label(
-            'Please select an example and press run',
+        label = toga.Label(
+            "Please select an example to run",
             style=Pack(padding_bottom=10),
         )
 
-        # Table with example programs
+        # ==== Table with examples =====================================================
+
         self.examples = []
 
         # search for all folders that contain modules
         for root, dirs, files in os.walk(examples_dir):
             # skip hidden folders
-            dirs[:] = [d for d in dirs if not d.startswith('.')]
-            if any(name == '__main__.py' for name in files):
-                example_path, example_name = os.path.split(root)
-                self.examples.append(
-                    dict(
-                        name=example_name,
-                        path=example_path
-                    )
-                )
+            dirs[:] = [d for d in dirs if not d.startswith(".")]
+            if any(name == "__main__.py" for name in files):
+                path = Path(root)
+                self.examples.append(dict(name=path.name, path=path.parent))
 
-        self.examples.sort(key=lambda e: e['path'])
+        self.examples.sort(key=lambda e: e["path"])
 
         self.table = toga.Table(
             headings=["Name", "Path"],
             data=self.examples,
             on_double_click=self.run,
+            on_select=self.on_example_selected,
             style=Pack(padding_bottom=10, flex=1),
         )
 
         # Buttons
-        self.btn_run = toga.Button('Run Example', on_press=self.run)
+        self.btn_run = toga.Button("Run Example", on_press=self.run)
+        self.btn_reset = toga.Button("Reset Code", on_press=self.reset_code)
 
-        # Outermost box
-        outer_box = toga.Box(
-            children=[self.label, self.table, self.btn_run],
+        # ==== View of example code ====================================================
+
+        self.info_view = toga.MultilineTextInput(
+            placeholder="Please select example", readonly=True, style=Pack(padding=1)
+        )
+
+        # ==== Assemble layout =========================================================
+
+        left_box = toga.Box(
+            children=[self.table, self.btn_run],
             style=Pack(
                 direction=COLUMN,
+                padding=1,
                 flex=1,
-                padding=10,
-            )
+            ),
+        )
+
+        split_container = toga.SplitContainer(content=[left_box, self.info_view])
+
+        outer_box = toga.Box(
+            children=[label, split_container],
+            style=Pack(padding=10, direction=COLUMN),
         )
 
         # Add the content on the main window
@@ -82,9 +111,11 @@ class ExampleExamplesOverviewApp(toga.App):
 
 
 def main():
-    return ExampleExamplesOverviewApp('Examples Overview', 'org.beeware.widgets.examples_overview')
+    return ExampleExamplesOverviewApp(
+        "Examples Overview", "org.beeware.widgets.examples_overview"
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = main()
     app.main_loop()
