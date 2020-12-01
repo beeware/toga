8f8d0386249d53d0ce756ffe59473cb4a18ce4e0
Kavindu Santhusa <63494497+Ksengine@users.noreply.github.com>
2020-12-01 07:39:29 +0000

    Update QtWidgets.py

--- nursery/qt/toga_qt/libs/QtWidgets.py
+++ nursery/qt/toga_qt/libs/QtWidgets.py
@@ -1,16 +1,1 @@
-from utils import GUI
-
-if GUI == 'pyside2':
-    from PySide2.QtWidgets import *
-
-else:
-    from PySide.QtGui import *
-    from PySide.QtCore import QRect
-
-    def _QWidget_grab(self, rect=QRect(0, 0, -1, -1)):
-        if not rect.isValid():
-            return QPixmap.grabWidget(self)
-        else:
-            return QPixmap.grabWidget(self, rect)
-
-    QWidget.grab = _QWidget_grab
+from PySide2.QtWidgets import *
