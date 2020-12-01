c7e1c74d5de98c653b1876b14316bb35b22eede9
Kavindu Santhusa <63494497+Ksengine@users.noreply.github.com>
2020-12-01 07:08:37 +0000

    ---PyQt5--- - **PySide**

--- nursery/qt/toga_qt/libs/utils.py
+++ nursery/qt/toga_qt/libs/utils.py
@@ -1,19 +1,19 @@
 try:  # try import PyQt5
-    from PyQt5.QtCore import QT_VERSION, QT_VERSION_STR
-    GUI = 'pyqt5'
+    from PySide2.QtCore import qVersion
+    GUI = 'pyside2'
 
 except ImportError:
     try:  # try import PySide2
-        from PySide2.QtCore import qVersion
-
-        GUI = 'pyside2'
-        _major, _minor, _micro = tuple(map(int, qVersion().split(".")[:3]))
-        QT_VERSION = (_major << 16) + (
-            _minor << 8) + _micro  # fix version for pyside2
-        QT_VERSION_STR = '{}.{}.{}'.format(
-            _major, _minor, _micro)  # fix version string for pyside2
+        from PySide.QtCore import qVersion
+        GUI = 'pyside'
 
     except ImportError:
         raise
 
-# TODO: add imports for PyQt4 and PySide with patches.
+_major, _minor, _micro = tuple(map(int, qVersion().split(".")[:3]))
+QT_VERSION = (_major << 16) + (
+    _minor << 8) + _micro  # fix version for pyside
+QT_VERSION_STR = '{}.{}.{}'.format(
+    _major, _minor, _micro)  # fix version string for pyside
+
+# TODO: add patches for and PySide.
