10c4bd97b2fb027ed283b00c527c151a59435564
Kavindu Santhusa <63494497+Ksengine@users.noreply.github.com>
2020-12-01 07:38:04 +0000

    Update utils.py

--- nursery/qt/toga_qt/libs/utils.py
+++ nursery/qt/toga_qt/libs/utils.py
@@ -1,14 +1,4 @@
-try:  # try import PySide2
-    from PySide2.QtCore import qVersion
-    GUI = 'pyside2'
-
-except ImportError:
-    try:  # try import PySide
-        from PySide.QtCore import qVersion
-        GUI = 'pyside'
-
-    except ImportError:
-        raise
+from PySide2.QtCore import qVersion
 
 _major, _minor, _micro = tuple(map(int, qVersion().split(".")[:3]))
 QT_VERSION = (_major << 16) + (
