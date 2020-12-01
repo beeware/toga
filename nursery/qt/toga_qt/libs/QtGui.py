f05f793df417e9ae7df56a3031390572dc27b570
Kavindu Santhusa <63494497+Ksengine@users.noreply.github.com>
2020-12-01 07:41:38 +0000

    Update QtGui.py

--- nursery/qt/toga_qt/libs/QtGui.py
+++ nursery/qt/toga_qt/libs/QtGui.py
@@ -1,8 +1,1 @@
-from utils import GUI
-
-if GUI == 'pyqt5':
-    from PyQt5.QtGui import *
-    # TODO: fix QWheelEvent if needed
-
-else:
-    from PySide2.QtGui import *
+from PySide2.QtGui import *
