f48696fcfefd45029c6d07c95f38d3a9a2434c3d
Kavindu Santhusa <63494497+Ksengine@users.noreply.github.com>
2020-12-02 03:26:28 +0000

    Delete utils.py

--- nursery/qt/toga_qt/libs/utils.py
+++
@@ -1,7 +0,0 @@
-from PySide2.QtCore import qVersion
-
-_major, _minor, _micro = tuple(map(int, qVersion().split(".")[:3]))
-QT_VERSION = (_major << 16) + (
-    _minor << 8) + _micro  # fix version
-QT_VERSION_STR = '{}.{}.{}'.format(
-    _major, _minor, _micro)  # fix version string
