1524345c9b380d7b6afda735e055e3256883dee7
Kavindu Santhusa <63494497+Ksengine@users.noreply.github.com>
2020-12-02 03:42:33 +0000

    direct Qt import

--- nursery/qt/toga_qt/colors.py
+++ nursery/qt/toga_qt/colors.py
@@ -1,10 +1,10 @@
 # Use the Travertino color definitions
 from travertino.colors import *  # noqa: F401,F403
-from toga_qt.libs import QtGui
+from PySide2.QtGui import QColor
 
 
 def native_color(c):
     if isinstance(c, str):
         c = NAMED_COLOR[c]
 
-    return QtGui.QColor.fromRgb(c.rgba.r, c.rgba.g, c.rgba.b, c.rgba.a * 255)
+    return QColor.fromRgb(c.rgba.r, c.rgba.g, c.rgba.b, c.rgba.a * 255)
