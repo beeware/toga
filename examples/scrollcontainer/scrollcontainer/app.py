56d489b4dcd59434a2a0b0b156416224d4e317d9
Tom <sw@t-arn.com>
2020-12-15 13:17:44 +0100

    * added Switches to example

--- examples/scrollcontainer/scrollcontainer/app.py
+++ examples/scrollcontainer/scrollcontainer/app.py
@@ -31,17 +31,31 @@ class ScrollContainerApp(toga.App):
         box = toga.Box()
         box.style.direction = COLUMN
         box.style.padding = 10
+        self.vscrolling = True
+        self.hscrolling = False
+        self.scroller = toga.ScrollContainer(horizontal=self.hscrolling, vertical=self.vscrolling)
+        switch_box = toga.Box(style=Pack(direction=ROW))
+        switch_box.add(toga.Switch('vertical scrolling', is_on=self.vscrolling, on_toggle=self.handle_vscrolling))
+        switch_box.add(toga.Switch('horizontal scrolling', is_on=self.hscrolling, on_toggle=self.handle_hscrolling))
+        box.add(switch_box)
 
         for x in range(100):
             label_text = 'Label {}'.format(x)
             box.add(Item(label_text))
 
-        scroller = toga.ScrollContainer(horizontal=True, vertical=True)
-        scroller.content = box
+        self.scroller.content = box
 
         self.main_window = toga.MainWindow(self.name, size=(400, 700))
-        self.main_window.content = scroller
+        self.main_window.content = self.scroller
         self.main_window.show()
+
+    def handle_hscrolling(self, widget):
+        self.hscrolling = widget.is_on
+        self.scroller.horizontal = self.hscrolling
+
+    def handle_vscrolling(self, widget):
+        self.vscrolling = widget.is_on
+        self.scroller.vertical = self.vscrolling
 
 
 def main():
