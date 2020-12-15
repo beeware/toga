0af996c81af169f308a44d034dd4084e3a3da8b8
Tom <sw@t-arn.com>
2020-12-15 17:12:46 +0100

    * first working version with onTouch listener

--- src/android/toga_android/widgets/scrollcontainer.py
+++ src/android/toga_android/widgets/scrollcontainer.py
@@ -1,40 +1,59 @@
 from travertino.size import at_least
 
-from ..libs import android_widgets
+from ..libs import android_widgets, android
 from .base import Widget
 from toga_android.window import AndroidViewport
 
+class TogaOnTouchListener(android.View__OnTouchListener):
+    is_scrolling_enabled = True
+    scrollview_impl = None
+
+    def __init__(self, scrollview_impl):
+        super().__init__()
+        self.scrollview_impl = scrollview_impl
+        print('TogaOnTouchListener.scrollview_impl=' + str(self.scrollview_impl))
+
+    def onTouch(self, view, motion_event):
+        if self.is_scrolling_enabled:
+            return self.scrollview_impl.onTouchEvent(motion_event)
+        else:
+            return True
+
+
 class ScrollContainer(Widget):
-    hScrollView = None
     vScrollView = None
+    vScrollListener = None
+    hScrollView = None
+    hScrollListener = None
 
     def create(self):
         print('ScrollContainer.create()')
         print('self.interface.vertical='+str(self.interface.vertical))
         print('self.interface.horizontal='+str(self.interface.horizontal))
         print('self.interface.content='+str(self.interface.content))
-        if self.interface.vertical:
-            self.vScrollView = android_widgets.ScrollView(self._native_activity)
-            vScrollView_layout_params = android_widgets.LinearLayout__LayoutParams(
-                android_widgets.LinearLayout__LayoutParams.MATCH_PARENT,
-                android_widgets.LinearLayout__LayoutParams.MATCH_PARENT
-            )
-            vScrollView_layout_params.gravity = android_widgets.Gravity.TOP
-            self.vScrollView.setLayoutParams(vScrollView_layout_params)
-            print('Created vertical ScrollView')
-            self.native = self.vScrollView
-        if self.interface.horizontal:
-            self.hScrollView = android_widgets.HorizontalScrollView(self._native_activity)
-            hScrollView_layout_params = android_widgets.LinearLayout__LayoutParams(
-                android_widgets.LinearLayout__LayoutParams.MATCH_PARENT,
-                android_widgets.LinearLayout__LayoutParams.MATCH_PARENT
-            )
-            hScrollView_layout_params.gravity = android_widgets.Gravity.LEFT
-            print('Created horizontal ScrollView')
-            self.native = self.hScrollView
-            if (self.interface.vertical):
-                self.vScrollView.addView(self.hScrollView, hScrollView_layout_params)
-                self.native = self.vScrollView
+        self.vScrollView = android_widgets.ScrollView(self._native_activity)
+        vScrollView_layout_params = android_widgets.LinearLayout__LayoutParams(
+            android_widgets.LinearLayout__LayoutParams.MATCH_PARENT,
+            android_widgets.LinearLayout__LayoutParams.MATCH_PARENT
+        )
+        vScrollView_layout_params.gravity = android_widgets.Gravity.TOP
+        self.vScrollView.setLayoutParams(vScrollView_layout_params)
+        self.vScrollListener = TogaOnTouchListener(scrollview_impl=self.vScrollView)
+        self.vScrollListener.is_scrolling_enabled = self.interface.vertical
+        self.vScrollView.setOnTouchListener(self.vScrollListener)
+        self.native = self.vScrollView
+        print('Created vertical ScrollView')
+        self.hScrollView = android_widgets.HorizontalScrollView(self._native_activity)
+        hScrollView_layout_params = android_widgets.LinearLayout__LayoutParams(
+            android_widgets.LinearLayout__LayoutParams.MATCH_PARENT,
+            android_widgets.LinearLayout__LayoutParams.MATCH_PARENT
+        )
+        hScrollView_layout_params.gravity = android_widgets.Gravity.LEFT
+        self.hScrollListener = TogaOnTouchListener(scrollview_impl=self.hScrollView)
+        self.hScrollListener.is_scrolling_enabled = self.interface.horizontal
+        self.hScrollView.setOnTouchListener(self.hScrollListener)
+        print('Created horizontal ScrollView')
+        self.vScrollView.addView(self.hScrollView, hScrollView_layout_params)
         print('self.native='+str(self.native))
 
     def set_content(self, widget):
@@ -50,27 +69,17 @@ class ScrollContainer(Widget):
         if widget_parent is not None:
             print('Removing existing parent from widget')
             widget_parent.removeView(widget.native)
-        if self.hScrollView is not None:
-            self.hScrollView.addView(widget.native, content_view_params)
-        else:
-            self.vScrollView.addView(widget.native, content_view_params)
+        self.hScrollView.addView(widget.native, content_view_params)
         for child in widget.interface.children:
             child._impl.container = widget
         print('Added content to ScrollContainer')
 
     def set_vertical(self, value):
         print('ScrollContainer.set_vertical(): '+str(value))
-        if (value is True and self.vScrollView is None) or (value is False and self.vScrollView is not None):
-            self.vScrollView = None
-            self.hScrollView = None
-            self.create()
+        self.vScrollListener.is_scrolling_enabled = value
 
     def set_horizontal(self, value):
-        print('ScrollContainer.set_horizontal(): '+str(value))
-        if (value is True and self.hScrollView is None) or (value is False and self.hScrollView is not None):
-            self.vScrollView = None
-            self.hScrollView = None
-            self.create()
+        self.hScrollListener.is_scrolling_enabled = value
 
     def rehint(self):
         # Android can crash when rendering some widgets until they have their layout params set. Guard for that case.
