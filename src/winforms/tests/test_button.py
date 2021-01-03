20d45f26a8b07f876c7195472892de6b1f362016
Maradonna <4048848+Maradonna90@users.noreply.github.com>
2021-01-03 18:20:49 +0100

    button test

---
+++ src/winforms/tests/test_button.py
@@ -0,0 +1,21 @@
+from toga_dummy.utils import TestCase
+from toga_winforms.widgets import button
+
+
+def generator(interface, obj):
+    for i in range(5):
+        obj.val = i
+        yield 1
+
+
+class TestButton(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.button = button.Button
+
+    def test_winforms_click(self):
+        self.val = None
+        button = self.button()
+        button.set_on_press(generator)
+        button.on_press()
+        self.assertEqual(self.val, 4)
