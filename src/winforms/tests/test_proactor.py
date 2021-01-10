5c92c532b32ea2a547ac1e5d0e2d76c36032fbb8
Maradonna <4048848+Maradonna90@users.noreply.github.com>
2021-01-10 11:29:47 +0100

    async decorator

--- src/winforms/tests/test_proactor.py
+++ src/winforms/tests/test_proactor.py
@@ -2,6 +2,7 @@ from toga_dummy.utils import TestCase
 from toga_winforms.libs import proactor, WinForms
 import unittest
 import unittest.mock as mock
+import asyncio
 
 
 class Counter(object):
@@ -11,13 +12,27 @@ class Counter(object):
     def increment(self):
         self.count += 1
 
+
+def async_test(f):
+    def wrapper(*args, **kwargs):
+        coro = asyncio.coroutine(f)
+        future = coro(*args, **kwargs)
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(future)
+    return wrapper
+
+
 class TestWindow(TestCase):
     def setUp(self):
         super().setUp()
 
+    @async_test
     def test_proactor_loop(self):
         print("=====================================================================")
         c = Counter()
         with mock.patch.object(Counter, 'increment', wraps=c.increment) as fake_increment:
-            self.proactor = proactor.WinformsProactorEventLoop().run_forever(WinForms.ApplicationContext())
+            self.loop = proactor.WinformsProactorEventLoop()
+            asyncio.set_event_loop(self.loop)
+            self.app_context = WinForms.ApplicationContext()
+            self.loop.run_forever(self.app_context)
             unittest.TestCase.assertGreaterEqual(1, fake_increment.count)
