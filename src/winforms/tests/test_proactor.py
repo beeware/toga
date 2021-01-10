b821b23748b7627f6426739c60bd010ae2793c2c
Maradonna <4048848+Maradonna90@users.noreply.github.com>
2021-01-10 12:30:06 +0100

    use IsolatedAsync test

--- src/winforms/tests/test_proactor.py
+++ src/winforms/tests/test_proactor.py
@@ -1,8 +1,8 @@
-from toga_dummy.utils import TestCase
 from toga_winforms.libs import proactor, WinForms
 import unittest
 import unittest.mock as mock
 import asyncio
+from unittest import IsolatedAsyncioTestCase
 
 
 class Counter(object):
@@ -13,20 +13,8 @@ class Counter(object):
         self.count += 1
 
 
-def async_test(f):
-    def wrapper(*args, **kwargs):
-        coro = asyncio.coroutine(f)
-        future = coro(*args, **kwargs)
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(future)
-    return wrapper
-
-
-class TestWindow(TestCase):
-    def setUp(self):
-        super().setUp()
-
-    # @async_test
+class TestProactor(IsolatedAsyncioTestCase):
+    
     async def test_proactor_loop(self):
         print("=====================================================================")
         c = Counter()
