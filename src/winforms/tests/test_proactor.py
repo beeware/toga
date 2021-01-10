95360c9519bfd8275bb357c7602b9049a21c999b
Maradonna <4048848+Maradonna90@users.noreply.github.com>
2021-01-10 10:27:06 +0100

    minimize workflow update proactor

--- src/winforms/tests/test_proactor.py
+++ src/winforms/tests/test_proactor.py
@@ -1,11 +1,23 @@
 from toga_dummy.utils import TestCase
 from toga_winforms.libs import proactor, WinForms
 import unittest
+import mock
 
+
+class Counter(object):
+    def __init__(self):
+        self.count = 0
+
+    def increment(self):
+        self.count += 1
 
 class TestWindow(TestCase):
     def setUp(self):
         super().setUp()
 
     def test_proactor_loop(self):
-        self.proactor = proactor.WinformsProactorEventLoop().run_forever(WinForms.ApplicationContext())
+        print("=====================================================================")
+        c = Counter()
+        with mock.patch.object(Counter, 'increment', wraps=c.increment) as fake_increment:
+            self.proactor = proactor.WinformsProactorEventLoop().run_forever(WinForms.ApplicationContext())
+            unittest.TestCase.assertGreaterEqual(1, fake_increment.count)
