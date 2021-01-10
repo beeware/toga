48c4f383b777732813c9279d59ce7fb5f3922d54
Maradonna <4048848+Maradonna90@users.noreply.github.com>
2021-01-10 12:41:53 +0100

    use await

--- src/winforms/tests/test_proactor.py
+++ src/winforms/tests/test_proactor.py
@@ -15,11 +15,7 @@ class Counter(object):
 
 def async_test(coro):
     def wrapper(*args, **kwargs):
-        loop = asyncio.new_event_loop()
-        try:
-            return loop.run_until_complete(coro(*args, **kwargs))
-        finally:
-            loop.close()
+        await coro
     return wrapper
 
 
