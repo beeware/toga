2db2627bfa8ea3b6007addeea27b2ec23dce94e3
Maradonna <4048848+Maradonna90@users.noreply.github.com>
2021-01-10 15:54:25 +0100

    test without patching

--- src/winforms/tests/test_proactor.py
+++ src/winforms/tests/test_proactor.py
@@ -22,15 +22,15 @@ class TestProactor(unittest.TestCase):
     def test_proactor_loop(self):
         print("=====================================================================")
         c = Counter()
-        with mock.patch.object(Counter, 'increment', wraps=c.increment) as fake_increment:
-            thread = Thread(target=self.loop.run_forever, args=(self.app_context))
-            thread.start()
-            # await asyncio.sleep(5)
-            print('Started!')
-            self.loop.call_soon_threadsafe(self.loop.stop)  # here
-            print('Requested stop!')
-            thread.join()
-            # self.loop.run_forever(self.app_context)
-            print('Finished!')
-            print("fake_increment:", fake_increment)
-            unittest.TestCase.assertGreaterEqual(1, fake_increment.count)
+        # with mock.patch.object(Counter, 'increment', wraps=c.increment) as fake_increment:
+        thread = Thread(target=self.loop.run_forever, args=(self.app_context))
+        thread.start()
+        # await asyncio.sleep(5)
+        print('Started!')
+        self.loop.call_soon_threadsafe(self.loop.stop)  # here
+        print('Requested stop!')
+        thread.join()
+        # self.loop.run_forever(self.app_context)
+        print('Finished!')
+        # print("fake_increment:", fake_increment)
+        # unittest.TestCase.assertGreaterEqual(1, fake_increment.count)
