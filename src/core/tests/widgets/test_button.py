f1cbf1dfe7fe2026fe1c991ef5f23deaccc8c6fc
root <root@localhost.localdomain>
2021-03-10 09:55:11 +0000

    Fix testcases to match the new impl of `_handler` function

--- src/core/tests/widgets/test_button.py
+++ src/core/tests/widgets/test_button.py
@@ -38,7 +38,7 @@ class ButtonTests(TestCase):
         self.btn.on_press = callback
         self.assertEqual(self.btn.on_press._raw, callback)
         self.assertEqual(
-            self.btn.on_press('widget', a=1),
+            self.btn.on_press(self.btn, a=1),
             "called <class 'toga.widgets.button.Button'> with {'a': 1}"
         )
         self.assertValueSet(self.btn, 'on_press', self.btn.on_press)
