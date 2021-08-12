00798868d63d9bd3854f1fbe076eaddd05e6b296
Tom <sw@t-arn.com>
2021-08-12 13:49:31 +0200

    Removed libs/android.py

--- src/android/toga_android/libs/android.py
+++
@@ -1,7 +0,0 @@
-from rubicon.java import JavaClass, JavaInterface
-
-
-ClipboardManager = JavaClass("android/content/ClipboardManager")
-ClipData = JavaClass("android/content/ClipData")
-MotionEvent = JavaClass("android/view/MotionEvent")
-View__OnTouchListener = JavaInterface("android/view/View$OnTouchListener")
