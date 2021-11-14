51d2c1753bfc7f3e35f0bb1b6fb58a414ab46ebb
Russell Keith-Magee <russell@keith-magee.com>
2021-11-14 13:15:10 +0800

    Applied black formatting to new example.

--- examples/font/font/app.py
+++ examples/font/font/app.py
@@ -11,10 +11,10 @@ class ExampleFontExampleApp(toga.App):
         self.textpanel.value = ""
 
     def do_monospace_button(self, widget):
-        self.textpanel.value += widget.label + '\n'
+        self.textpanel.value += widget.label + "\n"
 
     def do_icon_button(self, widget):
-        self.textpanel.value += widget.id + '\n'
+        self.textpanel.value += widget.id + "\n"
 
     def startup(self):
         # Set up main window
@@ -22,39 +22,64 @@ class ExampleFontExampleApp(toga.App):
 
         # Buttons
         btn_style = Pack(flex=1)
-        btn_clear = toga.Button('Clear', on_press=self.do_clear, style=btn_style)
-        btn1 = toga.Button('Monospace', on_press=self.do_monospace_button, style=Pack(font_family='monospace'))
-        toga.Font.register('awesome-free-solid', 'resources/Font Awesome 5 Free-Solid-900.otf')
-        btn2 = toga.Button('\uf0c5', id='copy', on_press=self.do_icon_button,
-                           style=Pack(font_family='awesome-free-solid', font_size=14, width=50))
-        btn3 = toga.Button('\uf0ea', id='paste', on_press=self.do_icon_button,
-                           style=Pack(font_family='awesome-free-solid', font_size=14, width=50))
-        btn4 = toga.Button('\uf0a9', id='arrow-right', on_press=self.do_icon_button,
-                           style=Pack(font_family='awesome-free-solid', font_size=14, width=50))
-        btn_box = toga.Box(
-            children=[
-                btn_clear,
-                btn1, btn2, btn3, btn4
-            ],
-            style=Pack(direction=ROW)
+        btn_clear = toga.Button("Clear", on_press=self.do_clear, style=btn_style)
+        btn1 = toga.Button(
+            "Monospace",
+            on_press=self.do_monospace_button,
+            style=Pack(font_family="monospace"),
         )
-        toga.Font.register('Endor', 'resources/ENDOR___.ttf')
-        lbl1 = toga.Label('Endor', style=Pack(font_family='Endor', font_size=14))
-        lbl2 = toga.Label('Endor bold', style=Pack(font_family='Endor', font_size=14, font_weight='bold'))
-        lbl3 = toga.Label('Endor italic', style=Pack(font_family='Endor', font_size=14, font_style='italic'))
-        lbl4 = toga.Label('Endor bold italic',
-                          style=Pack(font_family='Endor', font_size=14, font_weight='bold', font_style='italic'))
+        toga.Font.register(
+            "awesome-free-solid", "resources/Font Awesome 5 Free-Solid-900.otf"
+        )
+        btn2 = toga.Button(
+            "\uf0c5",
+            id="copy",
+            on_press=self.do_icon_button,
+            style=Pack(font_family="awesome-free-solid", font_size=14, width=50),
+        )
+        btn3 = toga.Button(
+            "\uf0ea",
+            id="paste",
+            on_press=self.do_icon_button,
+            style=Pack(font_family="awesome-free-solid", font_size=14, width=50),
+        )
+        btn4 = toga.Button(
+            "\uf0a9",
+            id="arrow-right",
+            on_press=self.do_icon_button,
+            style=Pack(font_family="awesome-free-solid", font_size=14, width=50),
+        )
+        btn_box = toga.Box(
+            children=[btn_clear, btn1, btn2, btn3, btn4], style=Pack(direction=ROW)
+        )
+        toga.Font.register("Endor", "resources/ENDOR___.ttf")
+        lbl1 = toga.Label("Endor", style=Pack(font_family="Endor", font_size=14))
+        lbl2 = toga.Label(
+            "Endor bold",
+            style=Pack(font_family="Endor", font_size=14, font_weight="bold"),
+        )
+        lbl3 = toga.Label(
+            "Endor italic",
+            style=Pack(font_family="Endor", font_size=14, font_style="italic"),
+        )
+        lbl4 = toga.Label(
+            "Endor bold italic",
+            style=Pack(
+                font_family="Endor",
+                font_size=14,
+                font_weight="bold",
+                font_style="italic",
+            ),
+        )
 
-        self.textpanel = toga.MultilineTextInput(readonly=False, style=Pack(flex=1), placeholder='Ready.')
+        self.textpanel = toga.MultilineTextInput(
+            readonly=False, style=Pack(flex=1), placeholder="Ready."
+        )
 
         # Outermost box
         outer_box = toga.Box(
             children=[btn_box, lbl1, lbl2, lbl3, lbl4, self.textpanel],
-            style=Pack(
-                flex=1,
-                direction=COLUMN,
-                padding=10
-            )
+            style=Pack(flex=1, direction=COLUMN, padding=10),
         )
 
         # Add the content on the main window
@@ -65,9 +90,9 @@ class ExampleFontExampleApp(toga.App):
 
 
 def main():
-    return ExampleFontExampleApp('Font Example', 'org.beeware.widgets.font')
+    return ExampleFontExampleApp("Font Example", "org.beeware.widgets.font")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = main()
     app.main_loop()
