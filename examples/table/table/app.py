549701e1368c312c9567997600e8a5084ce42749
Tom Arn <sw@t-arn.com>
2021-12-21 14:47:40 +0100

    applied black formatting to fix flake

--- examples/table/table/app.py
+++ examples/table/table/app.py
@@ -4,16 +4,16 @@ import toga
 from toga.constants import COLUMN, ROW
 from toga.style import Pack
 
-headings = ['Title', 'Year', 'Rating', 'Genre']
+headings = ["Title", "Year", "Rating", "Genre"]
 bee_movies = [
-    ('The Secret Life of Bees', '2008', '7.3', 'Drama'),
-    ('Bee Movie', '2007', '6.1', 'Animation, Adventure, Comedy'),
-    ('Bees', '1998', '6.3', 'Horror'),
-    ('The Girl Who Swallowed Bees', '2007', '7.5', 'Short'),
-    ('Birds Do It, Bees Do It', '1974', '7.3', 'Documentary'),
-    ('Bees: A Life for the Queen', '1998', '8.0', 'TV Movie'),
-    ('Bees in Paradise', '1944', '5.4', 'Comedy, Musical'),
-    ('Keeper of the Bees', '1947', '6.3', 'Drama')
+    ("The Secret Life of Bees", "2008", "7.3", "Drama"),
+    ("Bee Movie", "2007", "6.1", "Animation, Adventure, Comedy"),
+    ("Bees", "1998", "6.3", "Horror"),
+    ("The Girl Who Swallowed Bees", "2007", "7.5", "Short"),
+    ("Birds Do It, Bees Do It", "1974", "7.3", "Documentary"),
+    ("Bees: A Life for the Queen", "1998", "8.0", "TV Movie"),
+    ("Bees in Paradise", "1944", "5.4", "Comedy, Musical"),
+    ("Keeper of the Bees", "1947", "6.3", "Drama"),
 ]
 
 
@@ -22,13 +22,19 @@ class ExampleTableApp(toga.App):
 
     # Table callback functions
     def on_select_handler1(self, widget, row, **kwargs):
-        self.label_table1.text = 'You selected row: {}'.format(row.title) if row is not None else 'No row selected'
+        self.label_table1.text = (
+            "You selected row: {}".format(row.title)
+            if row is not None
+            else "No row selected"
+        )
 
     def on_select_handler2(self, widget, row, **kwargs):
         if self.table2.selection is not None:
-            self.label_table2.text = 'Rows selected: {}'.format(len(self.table2.selection))
+            self.label_table2.text = "Rows selected: {}".format(
+                len(self.table2.selection)
+            )
         else:
-            self.label_table2.text = 'No row selected'
+            self.label_table2.text = "No row selected"
 
     # Button callback functions
     def insert_handler(self, widget, **kwargs):
@@ -40,7 +46,7 @@ class ExampleTableApp(toga.App):
         elif len(self.table1.data) > 0:
             self.table1.data.remove(self.table1.data[0])
         else:
-            print('Table is empty!')
+            print("Table is empty!")
 
     def clear_handler(self, widget, **kwargs):
         self.table1.data.clear()
@@ -53,43 +59,64 @@ class ExampleTableApp(toga.App):
             # Try to delete the "genre" column by accessor.
             # If the column doesn't exist, this will raise a ValueError,
             # which means we need to add it.
-            self.table1.remove_column('genre')
+            self.table1.remove_column("genre")
         except ValueError:
             # Add the genre column. We provide the column *title*,
             # which is automatically converted into the data accessor `genre`.
             # If the data accessor can't be determined from the column title,
             # you could manually specify the accessor here, too.
-            self.table1.add_column('Genre')
+            self.table1.add_column("Genre")
 
     def startup(self):
         self.main_window = toga.MainWindow(title=self.name)
 
         # Label to show which row is currently selected.
-        self.label_table1 = toga.Label('Ready.', style=Pack(flex=1, padding_right=5))
-        self.label_table2 = toga.Label('Try multiple row selection.', style=Pack(flex=1, padding_left=5))
-        labelbox = toga.Box(children=[self.label_table1, self.label_table2], style=Pack(flex=0, padding_top=5))
+        self.label_table1 = toga.Label("Ready.", style=Pack(flex=1, padding_right=5))
+        self.label_table2 = toga.Label(
+            "Try multiple row selection.", style=Pack(flex=1, padding_left=5)
+        )
+        labelbox = toga.Box(
+            children=[self.label_table1, self.label_table2],
+            style=Pack(flex=0, padding_top=5),
+        )
 
         # Change font size
         lbl_fontlabel = toga.Label("Font size =")
         self.lbl_fontsize = toga.Label("10")
-        btn_reduce_size = toga.Button(" - ", on_press=self.reduce_fontsize, style=Pack(width=40))
-        btn_increase_size = toga.Button(" + ", on_press=self.increase_fontsize, style=Pack(width=40))
+        btn_reduce_size = toga.Button(
+            " - ", on_press=self.reduce_fontsize, style=Pack(width=40)
+        )
+        btn_increase_size = toga.Button(
+            " + ", on_press=self.increase_fontsize, style=Pack(width=40)
+        )
         font_box = toga.Box(
-            children=[lbl_fontlabel, self.lbl_fontsize, btn_reduce_size, btn_increase_size],
-            style=Pack(direction=ROW, padding_bottom=5)
+            children=[
+                lbl_fontlabel,
+                self.lbl_fontsize,
+                btn_reduce_size,
+                btn_increase_size,
+            ],
+            style=Pack(direction=ROW, padding_bottom=5),
         )
 
         # Data to populate the table.
         if toga.platform.current_platform in ["android", "ios"]:
             table_data = bee_movies[:4]
         else:
-            table_data = bee_movies*1000
+            table_data = bee_movies * 1000
 
         self.table1 = toga.Table(
             headings=headings,
             data=table_data,
-            style=Pack(flex=1, padding_right=5, font_family="monospace", font_size=int(self.lbl_fontsize.text),
-                       font_style="italic"), multiple_select=False, on_select=self.on_select_handler1
+            style=Pack(
+                flex=1,
+                padding_right=5,
+                font_family="monospace",
+                font_size=int(self.lbl_fontsize.text),
+                font_style="italic",
+            ),
+            multiple_select=False,
+            on_select=self.on_select_handler1,
         )
 
         self.table2 = toga.Table(
@@ -97,21 +124,31 @@ class ExampleTableApp(toga.App):
             data=self.table1.data,
             multiple_select=True,
             style=Pack(flex=1, padding_left=5),
-            on_select=self.on_select_handler2
+            on_select=self.on_select_handler2,
         )
 
         tablebox = toga.Box(children=[self.table1, self.table2], style=Pack(flex=1))
 
         # Buttons
         btn_style = Pack(flex=1)
-        btn_insert = toga.Button('Insert Row', on_press=self.insert_handler, style=btn_style)
-        btn_delete = toga.Button('Delete Row', on_press=self.delete_handler, style=btn_style)
-        btn_clear = toga.Button('Clear Table', on_press=self.clear_handler, style=btn_style)
-        btn_reset = toga.Button('Reset Table', on_press=self.reset_handler, style=btn_style)
-        btn_toggle = toga.Button('Toggle Column', on_press=self.toggle_handler, style=btn_style)
+        btn_insert = toga.Button(
+            "Insert Row", on_press=self.insert_handler, style=btn_style
+        )
+        btn_delete = toga.Button(
+            "Delete Row", on_press=self.delete_handler, style=btn_style
+        )
+        btn_clear = toga.Button(
+            "Clear Table", on_press=self.clear_handler, style=btn_style
+        )
+        btn_reset = toga.Button(
+            "Reset Table", on_press=self.reset_handler, style=btn_style
+        )
+        btn_toggle = toga.Button(
+            "Toggle Column", on_press=self.toggle_handler, style=btn_style
+        )
         btn_box = toga.Box(
             children=[btn_insert, btn_delete, btn_clear, btn_reset, btn_toggle],
-            style=Pack(direction=ROW, padding_bottom=5)
+            style=Pack(direction=ROW, padding_bottom=5),
         )
 
         # Most outer box
@@ -121,7 +158,7 @@ class ExampleTableApp(toga.App):
                 flex=1,
                 direction=COLUMN,
                 padding=10,
-            )
+            ),
         )
 
         # Add the content on the main window
@@ -142,10 +179,11 @@ class ExampleTableApp(toga.App):
         font = toga.Font("monospace", font_size)
         self.table1._impl.set_font(font)
 
+
 def main():
-    return ExampleTableApp('Table', 'org.beeware.widgets.table')
+    return ExampleTableApp("Table", "org.beeware.widgets.table")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = main()
     app.main_loop()
