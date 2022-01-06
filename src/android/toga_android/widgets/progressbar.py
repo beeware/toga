30869c46d5e5c7d6fb8cfd6b242090e7a3ac6123
Tom Arn <sw@t-arn.com>
2022-01-06 13:39:31 +0100

    * fixed None reference

--- src/android/toga_android/widgets/progressbar.py
+++ src/android/toga_android/widgets/progressbar.py
@@ -49,7 +49,7 @@ class ProgressBar(Widget):
     def rehint(self):
         # Android can crash when rendering some widgets until
         # they have their layout params set. Guard for that case.
-        if self.native.getLayoutParams() is None:
+        if not self.native.getLayoutParams():
             return
         self.native.measure(
             View__MeasureSpec.UNSPECIFIED,
