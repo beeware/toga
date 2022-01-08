06b4d3d0e275f61494d54a56f91a93e455c00d92
Mansour Moufid <mansourmoufid@gmail.com>
2022-01-07 20:07:01 -0500

    Define a readable representation of some types to help debugging.

--- src/cocoa/toga_cocoa/libs/__init__.py
+++ src/cocoa/toga_cocoa/libs/__init__.py
@@ -24,3 +24,20 @@ from .core_graphics import *  # noqa: F401, F403
 from .core_text import *  # noqa: F401, F403
 from .foundation import *  # noqa: F401, F403
 from .webkit import *  # noqa: F401, F403
+
+
+def NSPoint__repr__(self):
+    return '<NSPoint x={} y={}>'.format(self.x, self.y)
+
+
+def NSRect__repr__(self):
+    return '<NSRect origin={} size={}>'.format(self.origin, self.size)
+
+
+def NSSize__repr__(self):
+    return '<NSSize width={} height={}>'.format(self.width, self.height)
+
+
+NSPoint.__repr__ = NSPoint__repr__
+NSRect.__repr__ = NSRect__repr__
+NSSize.__repr__ = NSSize__repr__
