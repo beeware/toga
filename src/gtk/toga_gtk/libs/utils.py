21b3bbbd193b8f3e3d803e414668ebd4fad1de0e
Malcolm Smith <smith@chaquo.com>
2022-06-09 00:40:53 +0000

    GTK: fix horizontal alignment of multi-line labels

--- src/gtk/toga_gtk/libs/utils.py
+++ src/gtk/toga_gtk/libs/utils.py
@@ -1,11 +1,13 @@
 from toga.constants import CENTER, JUSTIFY, LEFT, RIGHT
 
+from . import Gtk
+
 
 def gtk_alignment(alignment):
-    "Convert Toga alignments in to arguments compatible with Gtk.set_alignment"
+    "Convert Toga alignments into arguments compatible with Gtk."
     return {
-        LEFT: (0.0, 0.5),
-        RIGHT: (1.0, 0.5),
-        CENTER: (0.5, 0.5),
-        JUSTIFY: (0.0, 0.0),
+        LEFT: (0.0, Gtk.Justification.LEFT),
+        RIGHT: (1.0, Gtk.Justification.RIGHT),
+        CENTER: (0.5, Gtk.Justification.CENTER),
+        JUSTIFY: (0.0, Gtk.Justification.FILL),
     }[alignment]
