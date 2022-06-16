c732d9fb72c981a30b2fb341c96480d43d164d50
Tom Arn <sw@t-arn.com>
2022-06-16 22:56:24 +0200

    * fixed flake

--- examples/tutorial3/tutorial/app.py
+++ examples/tutorial3/tutorial/app.py
@@ -6,7 +6,7 @@ class Graze(toga.App):
     def startup(self):
         self.main_window = toga.MainWindow(title=self.name)
 
-        self.webview = toga.WebView(on_webview_load=self.on_webview_loaded,style=Pack(flex=1))
+        self.webview = toga.WebView(on_webview_load=self.on_webview_loaded, style=Pack(flex=1))
         self.url_input = toga.TextInput(
             initial='https://beeware.org/',
             style=Pack(flex=1)
@@ -43,6 +43,7 @@ class Graze(toga.App):
 
     def on_webview_loaded(self, widget):
         self.url_input.value = self.webview.url
+
 
 def main():
     return Graze('Graze', 'org.beeware.graze')
