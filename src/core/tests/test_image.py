e5dae823b6d82e737f86861d295b3d8a9984b0a7
shape-warrior-t <91752863+shape-warrior-t@users.noreply.github.com>
2022-08-17 01:18:08 -0600

    Cover all three path specifications
    Introduce tests for all three ways to specify a path for `toga.Image`: local file path as a string, local file path as a `pathlib.Path`, and URL as a string.

--- src/core/tests/test_image.py
+++ src/core/tests/test_image.py
@@ -14,22 +14,45 @@ class ImageTests(TestCase):
         toga.App(formal_name='Image Test App',
                  app_id='org.beeware.test_image',
                  factory=toga_dummy.factory)
-        self.path = 'path/to/image.jpg'
-        self.image = toga.Image(path=self.path)
+        self.file_path = Path('path/to/image.jpg')
+        self.url_path = 'http://website.com/image.jpg'
+        self.path_file_image = toga.Image(path=self.file_path)
+        self.str_file_image = toga.Image(path=str(self.file_path))
+        self.url_image = toga.Image(path=self.url_path)
 
-    def test_object_created(self):
+    def test_path_file_image_binding(self):
         # Image is initially unbound
-        self.assertIsNone(self.image._impl)
+        self.assertIsNone(self.path_file_image._impl)
 
         # Bind the image; the file doesn't exist, so it raises an error.
         try:
-            self.image.bind(factory=toga_dummy.factory)
+            self.path_file_image.bind(factory=toga_dummy.factory)
             self.fail('The image should not bind')
         except FileNotFoundError:
             pass
 
-        # self.assertEqual(self.image._impl.interface, self.image)
-        # self.assertActionPerformedWith(self.image, 'load image', path=self.path)
+        # self.assertEqual(self.path_file_image._impl.interface, self.path_file_image)
+        # self.assertActionPerformedWith(self.path_file_image, 'load image', path=self.file_path)
 
-    def test_path(self):
-        self.assertEqual(self.image.path, Path(self.path))
+    def test_str_file_image_binding(self):
+        # Image is initially unbound
+        self.assertIsNone(self.str_file_image._impl)
+
+        # Bind the image; the file doesn't exist, so it raises an error.
+        try:
+            self.str_file_image.bind(factory=toga_dummy.factory)
+            self.fail('The image should not bind')
+        except FileNotFoundError:
+            pass
+
+        # self.assertEqual(self.str_file_image._impl.interface, self.str_file_image)
+        # self.assertActionPerformedWith(self.str_file_image, 'load image', path=str(self.file_path))
+
+    def test_path_file_image_path(self):
+        self.assertEqual(self.path_file_image.path, self.file_path)
+
+    def test_str_file_image_path(self):
+        self.assertEqual(self.str_file_image.path, self.file_path)
+
+    def test_url_image_path(self):
+        self.assertEqual(self.url_image.path, self.url_path)
