24ed0edb5ef7f0cac38e26043e97a356e513b37e
Bruno Rino <bruno.rino@gmail.com>
2022-08-17 16:07:23 +0200

    Check availablity of the native platform factory before it is overriden
    Signed-off-by: Bruno Rino <bruno.rino@gmail.com>

--- src/core/tests/test_platform.py
+++ src/core/tests/test_platform.py
@@ -3,16 +3,19 @@ import toga_dummy
 from toga_dummy.utils import TestCase
 
 
-class NumberInputTests(TestCase):
+class PlatformTests(TestCase):
     def setUp(self):
-        super().setUp()
         # We can't test the default behaviour of ``get_platform_factory()`` in CI:
         # the CI environment is only installing toga_core and toga_dummy.
+        # This test must be done before the call to super().setUp(), which will set 
+        # the default_factory.
         try:
             get_platform_factory()
             self.native_platform_factory_available = True
         except ModuleNotFoundError:
             self.native_platform_factory_available = False
+
+        super().setUp()
 
     def test_get_platform_factory(self):
         if self.native_platform_factory_available:
