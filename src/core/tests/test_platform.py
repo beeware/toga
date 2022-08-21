ad9db8ab28ce93e9b18247924c923ec61d18daa4
Bruno Rino <bruno.rino@gmail.com>
2022-08-21 19:16:56 +0200

    Mock toga_gtk for the CI environment benefit
    Signed-off-by: Bruno Rino <bruno.rino@gmail.com>

--- src/core/tests/test_platform.py
+++ src/core/tests/test_platform.py
@@ -1,39 +1,33 @@
+import unittest
+from unittest.mock import MagicMock, patch
+
 from toga.platform import get_platform_factory, default_factory
 import toga_dummy
-from toga_dummy.utils import TestCase
 
-
-# We can't test the default behaviour of ``get_platform_factory()`` in CI:
-# the CI environment only installs toga_core and toga_dummy.
 try:
-    get_platform_factory()
-    native_platform_factory_available = True
+    import toga_gtk
 except ModuleNotFoundError:
-    native_platform_factory_available = False
+    # toga_gtk is not installed in the CI environment; we need to mock it.
+    toga_gtk = MagicMock()
 
-
-class PlatformTests(TestCase):
+@patch.dict('sys.modules', toga_gtk=toga_gtk)
+class PlatformTests(unittest.TestCase):
 
     def test_get_platform_factory(self):
-        if not native_platform_factory_available:
-            self.skipTest('Native platform factory not available')
         factory = get_platform_factory()
         self.assertIsNotNone(factory)
-        self.assertIsNotNone(factory.App)
+        self.assertNotEqual(factory, toga_dummy.factory)
 
     def test_get_platform_factory_defined(self):
         factory = get_platform_factory(factory=toga_dummy.factory)
-        self.assertEquals(factory, toga_dummy.factory)
+        self.assertEqual(factory, toga_dummy.factory)
 
     def test_default_platform(self):
         default_factory(toga_dummy.factory)
         factory = get_platform_factory()
-        self.assertEquals(factory, toga_dummy.factory)
+        self.assertEqual(factory, toga_dummy.factory)
+
         default_factory(None)
-
-        if not native_platform_factory_available:
-            self.skipTest('Native platform factory not available')
-
         factory = get_platform_factory()
         self.assertIsNotNone(factory)
-        self.assertNotEquals(factory, toga_dummy.factory)
+        self.assertNotEqual(factory, toga_dummy.factory)
