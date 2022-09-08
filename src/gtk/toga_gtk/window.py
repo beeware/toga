0246027a537d96617340bf823c90bc6493f27c89
Russell Keith-Magee <russell@keith-magee.com>
2022-09-08 10:42:18 +0800

    Add a GTK implementation of hide() and visible.

--- src/gtk/toga_gtk/window.py
+++ src/gtk/toga_gtk/window.py
@@ -81,8 +81,9 @@ class Window:
                 item_impl.set_draw(False)
             else:
                 item_impl = Gtk.ToolButton()
-                icon_impl = cmd.icon.bind(self.interface.factory)
-                item_impl.set_icon_widget(icon_impl.native_32)
+                if cmd.icon:
+                    icon_impl = cmd.icon.bind(self.interface.factory)
+                    item_impl.set_icon_widget(icon_impl.native_32)
                 item_impl.set_label(cmd.text)
                 item_impl.set_tooltip_text(cmd.tooltip)
                 item_impl.connect("clicked", wrapped_handler(cmd, cmd.action))
@@ -134,10 +135,10 @@ class Window:
         self.interface.content._impl.min_height = self.interface.content.layout.height
 
     def hide(self):
-        self.interface.not_implemented("Window.hide()")
+        self.native.hide()
 
     def get_visible(self):
-        self.interface.not_implemented("Window.get_visible()")
+        return self.native.get_property("visible")
 
     def gtk_delete_event(self, widget, data):
         if self._is_closing:
