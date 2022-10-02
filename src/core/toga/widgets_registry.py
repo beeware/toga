1ee07589dabf71d76f742ae6711390a6f7ebe0c4
Sagi Shadur <saroad2@gmail.com>
2022-10-02 08:39:29 +0300

    Implement `WidgetsRegistry`

---
+++ src/core/toga/widgets_registry.py
@@ -0,0 +1,35 @@
+from toga.widgets.base import Widget
+
+
+class WidgetsRegistry:
+
+    def __init__(self):
+        self._id_to_widget = {}
+
+    def extend(self, *widgets):
+        for widget in widgets:
+            self.add(widget)
+
+    def add(self, widget):
+        if widget.id in self._id_to_widget:
+            # Prevent from adding the same widget twice
+            # or adding 2 widgets with the same id
+            raise KeyError(
+                f'There is already a widget with "{widget.id}" id'
+            )
+        self._id_to_widget[widget.id] = widget
+
+    def remove(self, id):
+        del self._id_to_widget[id]
+
+    def __getitem__(self, id):
+        return self._id_to_widget[id]
+
+    def __iter__(self):
+        return iter(self._id_to_widget.values())
+
+    def __len__(self):
+        return len(self._id_to_widget)
+
+    def __repr__(self):
+        return repr(self._id_to_widget)
