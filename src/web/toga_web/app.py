d4f61f0c0588bfbafb5f8fe400c5fb2ba2d5bb27
Russell Keith-Magee <russell@keith-magee.com>
2022-10-09 13:21:30 +0800

    Refactored menu creation.

--- src/web/toga_web/app.py
+++ src/web/toga_web/app.py
@@ -35,55 +35,38 @@ class App:
             ),
         )
 
-        # self.menubar.innerHTML = f"""
-        #     <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-        #         <div class="container">
-        #             <a class="navbar-brand" href="#">
-        #                 <img src="static/logo-32.png"
-        #                     class="d-inline-block align-top"
-        #                     alt=""
-        #                     loading="lazy">
-        #                 {self.interface.formal_name}
-        #             </a>
-        #             <button class="navbar-toggler" type="button"
-        #                     data-toggle="collapse" data-target="#navbarsExample07"
-        #                     aria-controls="navbarsExample07" aria-expanded="false"
-        #                     aria-label="Toggle navigation">
-        #                 <span class="navbar-toggler-icon"></span>
-        #             </button>
-
-        #             <div class="collapse navbar-collapse" id="navbarsExample07">
-        #                 <ul class="navbar-nav mr-auto">
-        #                     <!--li class="nav-item">
-        #                         <a class="nav-link" href="#">Menu</a>
-        #                     </li>
-        #                     <li class="nav-item">
-        #                         <a class="nav-link disabled" href="#">Disabled menu</a>
-        #                     </li-->
-        #                 </ul>
-        #                 <ul class="navbar-nav ml-auto">
-        #                     <li class="nav-item dropdown">
-        #                         <a class="nav-link dropdown-toggle"
-        #                             href="http://example.com" id="dropdown07"
-        #                             data-toggle="dropdown"
-        #                             aria-haspopup="true"
-        #                             aria-expanded="false">Help</a>
-        #                         <div class="dropdown-menu" aria-labelledby="dropdown07">
-        #                             <a class="dropdown-item" href="#">About</a>
-        #                             <a class="dropdown-item" href="#">Preferences</a>
-        #                         </div>
-        #                     </li>
-        #                 </ul>
-        #             </div>
-        #         </div>
-        #     </nav>
-        # """
-
         # Create the menus.
         self.create_menus()
 
         # Call user code to populate the main window
         self.interface.startup()
+
+    def _create_submenu(self, group, items):
+        submenu = create_element(
+            "li",
+            classes=["nav-item", "dropdown"],
+            children=[
+                create_element(
+                    "a",
+                    id=f"menu-{id(group)}",
+                    classes=[
+                        "nav-link",
+                        "dropdown-toggle",
+                    ],
+                    data_toggle="dropdown",
+                    aria_haspopup="true",
+                    aria_expanded="false",
+                    content=group.text,
+                ),
+                create_element(
+                    "div",
+                    classes=["dropdown-menu"],
+                    aria_labelledby=f"menu-{id(group)}",
+                    children=items,
+                ),
+            ],
+        )
+        return submenu
 
     def create_menus(self):
         self._menu_groups = {}
@@ -113,65 +96,17 @@ class App:
             "ul",
             classes=["navbar-nav", "mr-auto"],
         )
-
-        for group, items in self._menu_groups.items():
-            if group != toga.Group.HELP:
-                submenu = create_element(
-                    "li",
-                    classes=["nav-item", "dropdown"],
-                    children=[
-                        create_element(
-                            "a",
-                            id=f"menu-{id(group)}",
-                            classes=[
-                                "nav-link",
-                                "dropdown-toggle",
-                            ],
-                            data_toggle="dropdown",
-                            aria_haspopup="true",
-                            aria_expanded="false",
-                            content=group,
-                        ),
-                        create_element(
-                            "div",
-                            classes=["dropdown-menu"],
-                            aria_labelledby=f"menu-{id(group)}",
-                            children=items,
-                        ),
-                    ],
-                )
-                menu_container.appendChild(submenu)
-
         help_menu_container = create_element(
             "ul",
             classes=["navbar-nav", "ml-auto"],
-            children=[
-                create_element(
-                    "li",
-                    classes=["nav-item", "dropdown"],
-                    children=[
-                        create_element(
-                            "a",
-                            id="help-menu",
-                            classes=[
-                                "nav-link",
-                                "dropdown-toggle",
-                            ],
-                            data_toggle="dropdown",
-                            aria_haspopup="true",
-                            aria_expanded="false",
-                            content="Help",
-                        ),
-                        create_element(
-                            "div",
-                            classes=["dropdown-menu"],
-                            aria_labelledby="help-menu",
-                            children=self._menu_groups[toga.Group.HELP],
-                        ),
-                    ],
-                )
-            ],
         )
+
+        for group, items in self._menu_groups.items():
+            submenu = self._create_submenu(group, items)
+            if group != toga.Group.HELP:
+                menu_container.appendChild(submenu)
+            else:
+                help_menu_container.appendChild(submenu)
 
         self.menubar = create_element(
             "header",
