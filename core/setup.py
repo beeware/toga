f302563f6642f0554f573ca44548ab6e54f9411d
Malcolm Smith <smith@chaquo.com>
2022-11-09 21:19:13 +0000

    Move core and meta packages to setuptools-scm

--- core/setup.py
+++ core/setup.py
@@ -1,20 +1,6 @@
-#!/usr/bin/env python
-import re
-
 from setuptools import setup
-
-# Version handline needs to be programatic because
-# we can't import toga to compute the version;
-# and to support versioned subpackage dependencies
-with open("src/toga/__init__.py", encoding="utf8") as version_file:
-    version_match = re.search(
-        r"^__version__ = ['\"]([^'\"]*)['\"]", version_file.read(), re.M
-    )
-    if version_match:
-        version = version_match.group(1)
-    else:
-        raise RuntimeError("Unable to find version string.")
+from setuptools_scm import get_version
 
 setup(
-    version=version,
+    version=get_version(root=".."),
 )
