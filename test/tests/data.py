c3cf4379a4c7d351d81d605b0552ed00cff53d51
Malcolm Smith <smith@chaquo.com>
2022-12-03 18:25:33 +0000

    Reduce colors list

--- test/tests/data.py
+++ test/tests/data.py
@@ -5,11 +5,21 @@ TEXTS = ["", " ", "a", "ab", "abc", "hello world", "hello\nworld"]
 
 
 # TODO: include None
-components = [0, 1, 2, 128, 253, 254, 255]
 COLORS = [
     rgba(r, g, b, a)
-    for r in components
-    for g in components
-    for b in components
+    for r, g, b in [
+        # Black, gray, white,
+        (0, 0, 0),
+        (1, 1, 1),
+        (10, 10, 10),
+        (128, 128, 128),
+        (245, 245, 245),
+        (254, 254, 254),
+        (255, 255, 255),
+        # Primaries
+        (255, 0, 0),
+        (0, 255, 0),
+        (0, 0, 255),
+    ]
     for a in [0.0, 0.01, 0.1, 0.5, 0.9, 0.99, 1.0]
 ]
