2e2f184dfe6c07fa903bd07f709138291645c0e8
Russell Keith-Magee <russell@keith-magee.com>
2023-01-11 14:11:27 +0800

    Experiment with iOS coverage.

--- testbed/tests/testbed.py
+++ testbed/tests/testbed.py
@@ -11,7 +11,7 @@ import pytest
 from testbed.app import main
 
 
-def run_tests(app, cov):
+def run_tests(app):
     project_path = Path(__file__).parent.parent
     os.chdir(project_path)
 
@@ -37,32 +37,6 @@ def run_tests(app, cov):
             project_path / "tests",
         ]
     )
-
-    # FIXME: Coverage reporting doesn't work on Android & iOS (yet!) This is for
-    # two reasons:
-    # 1. On Android, the code being covered needs to be unpacked and readable
-    #    for a coverage report to be generated. This should be fixed by
-    #    extractPackages
-    # 2. The main thread where coverage has been started dies before the this
-    #    thread; as a result, the garbage collection on the tracer function
-    #    (coverage.pytracer._trace():132) raises an IndexError because the data
-    #    stack is empty.
-    if hasattr(sys, "getandroidapilevel"):
-        print("***No coverage report on Android***")
-    elif sys.platform == "ios":
-        print("***No coverage report on iOS***")
-    # Only print a coverage report if the test suite passed.
-    elif app.returncode == 0:
-        cov.stop()
-        total = cov.report(
-            precision=1,
-            skip_covered=True,
-            show_missing=True,
-        )
-        if total < 100.0:
-            print("Test coverage is incomplete")
-            # Uncomment the next line to enforce test coverage
-            # TODO: app.returncode = 1
 
     app.add_background_task(lambda app, **kwargs: app.exit())
 
@@ -109,14 +83,45 @@ if __name__ == "__main__":
         source_pkgs=[toga_backend],
     )
     cov.start()
-
+    print("INITIAL TRACERS")
+    for tracer in cov._collector.tracers:
+        print(tracer, tracer.data_stack)
+    print("START TESTS")
     # Create the test app, starting the test suite as a background task
     app = main()
-    thread = Thread(target=partial(run_tests, app, cov))
+    thread = Thread(target=partial(run_tests, app))
     app.add_background_task(lambda app, *kwargs: thread.start())
 
     # Add an on_exit handler that will terminate the test suite.
     def exit_suite(app, **kwargs):
+        # FIXME: Coverage reporting doesn't work on Android (yet!) This is for
+        # two reasons:
+        # 1. On Android, the code being covered needs to be unpacked and readable
+        #    for a coverage report to be generated. This should be fixed by
+        #    extractPackages
+        # 2. The main thread where coverage has been started dies before the this
+        #    thread; as a result, the garbage collection on the tracer function
+        #    (coverage.pytracer._trace():132) raises an IndexError because the data
+        #    stack is empty.
+        if hasattr(sys, "getandroidapilevel"):
+            print("***No coverage report on Android***")
+        # Only print a coverage report if the test suite passed.
+        elif app.returncode == 0:
+            print("STOP COVERAGE", cov)
+            for tracer in cov._collector.tracers:
+                print(tracer, tracer.data_stack)
+            # cov.stop()
+            print("COVERAGE STOPPPED")
+            total = cov.report(
+                precision=1,
+                skip_covered=True,
+                show_missing=True,
+            )
+            if total < 100.0:
+                print("Test coverage is incomplete")
+                # Uncomment the next line to enforce test coverage
+                # TODO: app.returncode = 1
+
         print(f">>>>>>>>>> EXIT {app.returncode} <<<<<<<<<<")
         return True
 
