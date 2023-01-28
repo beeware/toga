c2717e58c987b8d1019e0b2ee06afe581f098e51
Malcolm Smith <smith@chaquo.com>
2023-01-28 14:56:26 +0000

    Fix testbed coverage on desktop platforms, and add a top-level exception handler on the test thread

--- testbed/tests/testbed.py
+++ testbed/tests/testbed.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import tempfile
+import traceback
 from functools import partial
 from pathlib import Path
 from threading import Thread
@@ -12,51 +13,58 @@ from testbed.app import main
 
 
 def run_tests(app, cov):
-    project_path = Path(__file__).parent.parent
-    os.chdir(project_path)
-    app.returncode = pytest.main(
-        [
-            # Output formatting
-            "-vv",
-            "--no-header",
-            "--tb=native",
-            "-rP",  # Show stdout from all tests, even if they passed.
-            "--color=no",
-            # Run all async tests and fixtures using pytest-asyncio.
-            "--asyncio-mode=auto",
-            # Override the cache directory to be somewhere known writable
-            "-o",
-            f"cache_dir={tempfile.gettempdir()}/.pytest_cache",
-            project_path / "tests",
-        ]
-    )
-
-    # WORKAROUND: On Android, the main thread where coverage has been started
-    # dies before this thread; as a result, the garbage collection on the tracer
-    # function raises an IndexError because the data stack is empty for that
-    # thread. This has been reported as
-    # https://github.com/nedbat/coveragepy/issues/1542 and a PR submitted; This
-    # workaround can be removed once that PR is available in a production
-    # version of coverage.
-    for tracer in cov._collector.tracers:
-        if len(tracer.data_stack) == 0:
-            print("Backfilling empty coverage stack...")
-            tracer.data_stack.append((None, None, None, None))
-
-    # Only print a coverage report if the test suite passed.
-    if app.returncode == 0:
-        cov.stop()
-        total = cov.report(
-            precision=1,
-            skip_covered=True,
-            show_missing=True,
+    try:
+        project_path = Path(__file__).parent.parent
+        os.chdir(project_path)
+        app.returncode = pytest.main(
+            [
+                # Output formatting
+                "-vv",
+                "--no-header",
+                "--tb=native",
+                "-rP",  # Show stdout from all tests, even if they passed.
+                "--color=no",
+                # Run all async tests and fixtures using pytest-asyncio.
+                "--asyncio-mode=auto",
+                # Override the cache directory to be somewhere known writable
+                "-o",
+                f"cache_dir={tempfile.gettempdir()}/.pytest_cache",
+                project_path / "tests",
+            ]
         )
-        if total < 100.0:
-            print("Test coverage is incomplete")
-            # Uncomment the next line to enforce test coverage
-            # TODO: app.returncode = 1
 
-    app.add_background_task(lambda app, **kwargs: app.exit())
+        # WORKAROUND: On Android, the main thread where coverage has been started
+        # dies before this thread; as a result, the garbage collection on the tracer
+        # function raises an IndexError because the data stack is empty for that
+        # thread. This has been reported as
+        # https://github.com/nedbat/coveragepy/issues/1542 and a PR submitted; This
+        # workaround can be removed once that PR is available in a production
+        # version of coverage.
+        #
+        # Desktop platforms use CTracer, which doesn't have a data_stack attribute, but
+        # that's OK because desktop platforms don't have this threading issue anyway.
+        for tracer in cov._collector.tracers:
+            if hasattr(tracer, "data_stack") and len(tracer.data_stack) == 0:
+                print("Backfilling empty coverage stack...")
+                tracer.data_stack.append((None, None, None, None))
+
+        # Only print a coverage report if the test suite passed.
+        if app.returncode == 0:
+            cov.stop()
+            total = cov.report(
+                precision=1,
+                skip_covered=True,
+                show_missing=True,
+            )
+            if total < 100.0:
+                print("Test coverage is incomplete")
+                # Uncomment the next line to enforce test coverage
+                # TODO: app.returncode = 1
+    except BaseException:
+        traceback.print_exc()
+        app.returncode = 1
+    finally:
+        app.add_background_task(lambda app, **kwargs: app.exit())
 
 
 if __name__ == "__main__":
