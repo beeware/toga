08525ce9f54353660bcc3599a0827492e9d4b02a
Russell Keith-Magee <russell@keith-magee.com>
2023-03-03 12:29:10 +0800

    Add missing retain that causes iOS crashes.

--- iOS/src/toga_iOS/colors.py
+++ iOS/src/toga_iOS/colors.py
@@ -9,7 +9,7 @@ def native_color(c):
     except KeyError:
         color = UIColor.colorWithRed(
             c.rgba.r / 255, green=c.rgba.g / 255, blue=c.rgba.b / 255, alpha=c.rgba.a
-        )
+        ).retain()
         CACHE[c] = color
 
     return color
