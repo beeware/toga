40140cd080f53c5c375527431b972122501b1d4b
Russell Keith-Magee <russell@keith-magee.com>
2023-03-21 13:46:27 +0800

    Update docs and tests for Box.

--- core/tests/widgets/test_box.py
+++ core/tests/widgets/test_box.py
@@ -1,16 +1,33 @@
 import toga
-from toga_dummy.utils import TestCase
+from toga_dummy.utils import (
+    assert_action_not_performed,
+    assert_action_performed,
+)
 
 
-class BoxTests(TestCase):
-    def setUp(self):
-        super().setUp()
-        self.children = [toga.Widget()]
-        self.box = toga.Box(children=self.children)
+def test_create_box():
+    "A Box can be created."
+    box = toga.Box()
+    # Round trip the impl/interface
+    assert box._impl.interface == box
 
-    def test_widget_created(self):
-        self.assertEqual(self.box._impl.interface, self.box)
-        self.assertActionPerformed(self.box, "create Box")
+    assert_action_performed(box, "create Box")
+    assert_action_not_performed(box, "add child")
 
-    def test_children_added(self):
-        self.assertEqual(self.box._children, self.children)
+
+def test_create_box_with_children():
+    "A Box can be created with children."
+    child1 = toga.Box()
+    child2 = toga.Box()
+    box = toga.Box(children=[child1, child2])
+
+    # Round trip the impl/interface
+    assert box._impl.interface == box
+
+    assert_action_performed(box, "create Box")
+    # The impl-level add-child will not be called,
+    # because the box hasn't been assigned to a window
+    assert_action_not_performed(box, "add child")
+
+    # But the box will have children.
+    assert box.children == [child1, child2]
