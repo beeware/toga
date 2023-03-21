51b3799e576a7b0aa4dd82e3a7a9482422b97ef9
Russell Keith-Magee <russell@keith-magee.com>
2023-03-21 11:05:51 +0800

    Add protection against missing probe modules.

--- testbed/tests/widgets/probe.py
+++ testbed/tests/widgets/probe.py
@@ -1,7 +1,15 @@
 from importlib import import_module
 
+import pytest
+
 
 def get_probe(widget):
     name = type(widget).__name__
-    module = import_module(f"tests_backend.widgets.{name.lower()}")
-    return getattr(module, f"{name}Probe")(widget)
+
+    try:
+        module = import_module(f"tests_backend.widgets.{name.lower()}")
+        return getattr(module, f"{name}Probe")(widget)
+    except ModuleNotFoundError:
+        pytest.skip(f"No platform probe module found for widget {name!r}")
+    except AttributeError:
+        pytest.skip(f"No platform probe found for widget {name!r}")
