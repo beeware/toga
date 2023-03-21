bf80c3d474f762f6d67a0e790fc6757e186b6052
Russell Keith-Magee <russell@keith-magee.com>
2023-03-21 14:53:23 +0800

    Ensure style attributes are applied to GTK Spinner.

--- gtk/src/toga_gtk/widgets/activityindicator.py
+++ gtk/src/toga_gtk/widgets/activityindicator.py
@@ -5,6 +5,8 @@ from .base import Widget
 class ActivityIndicator(Widget):
     def create(self):
         self.native = Gtk.Spinner()
+        self.native.set_name(f"toga-{self.interface.id}")
+        self.native.get_style_context().add_class("toga")
         self.native.interface = self.interface
 
     def is_running(self):
