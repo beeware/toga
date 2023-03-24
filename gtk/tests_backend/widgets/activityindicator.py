68026328dfb8035fe8412e6380723b6f5e926d02
Russell Keith-Magee <russell@keith-magee.com>
2023-03-24 14:54:19 +0800

    Modify activityindiator tests to use widget API directly.

--- gtk/tests_backend/widgets/activityindicator.py
+++ gtk/tests_backend/widgets/activityindicator.py
@@ -5,7 +5,3 @@ from .base import SimpleProbe
 
 class ActivityIndicatorProbe(SimpleProbe):
     native_class = Gtk.Spinner
-
-    @property
-    def is_running(self):
-        return self.native.get_property("active")
