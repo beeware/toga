1b1a573e4a52575f80ce13c39e02687b761cf967
Russell Keith-Magee <russell@keith-magee.com>
2023-05-03 06:56:41 +0800

    Unpin versions in attic projects to avoid dependabot alerts.

--- attic/falcon/setup.py
+++ attic/falcon/setup.py
@@ -6,11 +6,9 @@ from setuptools import setup
 # Version handline needs to be programatic because
 # we can't import toga_falcon to compute the version;
 # and to support versioned subpackage dependencies
-with open('toga_falcon/__init__.py', encoding='utf8') as version_file:
+with open("toga_falcon/__init__.py", encoding="utf8") as version_file:
     version_match = re.search(
-        r"^__version__ = ['\"]([^'\"]*)['\"]",
-        version_file.read(),
-        re.M
+        r"^__version__ = ['\"]([^'\"]*)['\"]", version_file.read(), re.M
     )
     if version_match:
         version = version_match.group(1)
@@ -20,6 +18,6 @@ with open('toga_falcon/__init__.py', encoding='utf8') as version_file:
 setup(
     version=version,
     install_requires=[
-        'toga-core==%s' % version,
+        "toga-core==%s" % version,
     ],
 )
