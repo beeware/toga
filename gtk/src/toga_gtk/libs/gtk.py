c8134fcb2823ce7e00a6e7a333e88318647d642a
Russell Keith-Magee <russell@keith-magee.com>
2023-05-03 14:29:54 +0800

    Ensure webkit2 libraries are available for testing.

--- gtk/src/toga_gtk/libs/gtk.py
+++ gtk/src/toga_gtk/libs/gtk.py
@@ -13,16 +13,16 @@ if Gdk.Screen.get_default() is None:  # pragma: no cover
 try:
     gi.require_version("WebKit2", "4.0")
     from gi.repository import WebKit2  # noqa: F401
-except ImportError:  # pragma: no cover
+except (ValueError, ImportError):  # pragma: no cover
     WebKit2 = None
 
 try:
     gi.require_version("Pango", "1.0")
     from gi.repository import Pango
-except ImportError:  # pragma: no cover
+except (ValueError, ImportError):  # pragma: no cover
     Pango = None
 
 try:
     import cairo
-except ImportError:  # pragma: no cover
+except (ValueError, ImportError):  # pragma: no cover
     cairo = None
