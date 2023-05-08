78b04e2892fddceb54e1ff18faff32e1d9f9ef48
MuhammadMouradG <muhammad.mourad1995@gmail.com>
2023-05-08 03:46:37 +0200

    Create the app HeaderBar and Primary menu

--- gtk/src/toga_gtk/app.py
+++ gtk/src/toga_gtk/app.py
@@ -105,11 +105,29 @@ class App:
         # Set up the default commands for the interface.
         self.interface.commands.add(
             Command(
+                None,
+                "Preferences",
+                group=toga.Group.APP,
+                order=sys.maxsize-3, 
+            ),
+            Command(
+                None,
+                "Keyboard Shortcuts",
+                group=toga.Group.APP,
+                order=sys.maxsize-2,
+            ),
+            Command(
+                None,
+                "Help",
+                group=toga.Group.APP,
+                order=sys.maxsize-1,
+            ),
+            Command(
                 lambda _: self.interface.about(),
                 "About " + self.interface.name,
-                group=toga.Group.HELP,
+                group=toga.Group.APP,
+                order=sys.maxsize, 
             ),
-            Command(None, "Preferences", group=toga.Group.APP),
         )
 
     def gtk_activate(self, data=None):
@@ -120,8 +138,19 @@ class App:
         self._menu_items = {}
         self._menu_groups = {}
 
-        # Create the menu for the top level menubar.
-        menubar = Gio.Menu()
+        # Create the menus
+        popover = Gtk.PopoverMenu()
+        primary_menu_content = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, margin=10, spacing=10)
+        popover.add(primary_menu_content)
+        popover.set_position(Gtk.PositionType.BOTTOM)
+
+        menu_icon = Gtk.Image(icon_name="open-menu-symbolic", icon_size=Gtk.IconSize.MENU)
+        menu_button = Gtk.MenuButton(image=menu_icon, popover=popover)
+        menu_button.show_all()
+
+        app_header_bar = self.interface.main_window._impl.header_bar
+        app_header_bar.pack_end(menu_button)
+
         section = None
         for cmd in self.interface.commands:
             if cmd == GROUP_BREAK:
@@ -129,11 +158,11 @@ class App:
             elif cmd == SECTION_BREAK:
                 section = None
             else:
-                submenu = self._submenu(cmd.group, menubar)
+                # submenu = self._submenu(cmd.group, primary_menu_box)
 
-                if section is None:
-                    section = Gio.Menu()
-                    submenu.append_section(None, section)
+                # if section is None:
+                #     section = Gio.Menu()
+                #     submenu.append_section(None, section)
 
                 cmd_id = "command-%s" % id(cmd)
                 action = Gio.SimpleAction.new(cmd_id, None)
@@ -145,16 +174,51 @@ class App:
                 self._menu_items[action] = cmd
                 self.native.add_action(action)
 
-                item = Gio.MenuItem.new(cmd.text, "app." + cmd_id)
+                item = Gtk.ModelButton(action_name="app." + cmd_id, label=cmd.text)
+
                 if cmd.shortcut:
-                    item.set_attribute_value(
-                        "accel", GLib.Variant("s", gtk_accel(cmd.shortcut))
+                    self.native.set_accels_for_action(
+                        "app." + cmd_id, [gtk_accel(cmd.shortcut)]
                     )
 
-                section.append_item(item)
+                primary_menu_content.add(item)
+        primary_menu_content.show_all()
 
-        # Set the menu for the app.
-        self.native.set_menubar(menubar)
+        # # Create the menu for the top level menubar.
+        # menubar = Gio.Menu()
+        # section = None
+        # for cmd in self.interface.commands:
+        #     if cmd == GROUP_BREAK:
+        #         section = None
+        #     elif cmd == SECTION_BREAK:
+        #         section = None
+        #     else:
+        #         submenu = self._submenu(cmd.group, menubar)
+
+        #         if section is None:
+        #             section = Gio.Menu()
+        #             submenu.append_section(None, section)
+
+        #         cmd_id = "command-%s" % id(cmd)
+        #         action = Gio.SimpleAction.new(cmd_id, None)
+        #         if cmd.action:
+        #             action.connect("activate", gtk_menu_item_activate(cmd))
+
+        #         cmd._impl.native.append(action)
+        #         cmd._impl.set_enabled(cmd.enabled)
+        #         self._menu_items[action] = cmd
+        #         self.native.add_action(action)
+
+        #         item = Gio.MenuItem.new(cmd.text, "app." + cmd_id)
+        #         if cmd.shortcut:
+        #             item.set_attribute_value(
+        #                 "accel", GLib.Variant("s", gtk_accel(cmd.shortcut))
+        #             )
+
+        #         section.append_item(item)
+
+        # # Set the menu for the app.
+        # self.native.set_menubar(menubar)
 
     def _submenu(self, group, menubar):
         try:
