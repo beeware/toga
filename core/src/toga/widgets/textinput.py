9dc20b915f73fdd375b00536a032d90ab688bbba
Russell Keith-Magee <russell@keith-magee.com>
2023-05-17 12:07:49 +0800

    Complete docstrings for TextInput.

--- core/src/toga/widgets/textinput.py
+++ core/src/toga/widgets/textinput.py
@@ -1,4 +1,4 @@
-import warnings
+from typing import List, Optional
 
 from toga.handlers import wrapped_handler
 
@@ -6,88 +6,62 @@ from .base import Widget
 
 
 class TextInput(Widget):
-    """A widget get user input.
-
-    Args:
-        id (str): An identifier for this widget.
-        style (:obj:`Style`): An optional style object. If no style is provided then
-            a new one will be created for the widget.
-        value (str): The initial text for the input.
-        placeholder (str): If no input is present this text is shown.
-        readonly (bool):  Whether a user can write into the text input, defaults to `False`.
-        on_change (Callable): Method to be called when text is changed in text box
-        on_confirm (Callable): Method to be called when return is pressed
-        validators (list): list of validators to run on the value of the text box. Should
-            return None is value is valid and an error message if not.
-        on_change (``callable``): The handler to invoke when the text changes.
-        on_confirm (``callable``): The handler to invoke when return is pressed.
-        on_gain_focus (:obj:`callable`): Function to execute when get focused.
-        on_lose_focus (:obj:`callable`): Function to execute when lose focus.
-    """
-
-    MIN_WIDTH = 100
-
     def __init__(
         self,
         id=None,
         style=None,
-        factory=None,  # DEPRECATED!
-        value=None,
-        placeholder=None,
-        readonly=False,
+        value: Optional[str] = None,
+        readonly: bool = False,
+        placeholder: Optional[str] = None,
         on_change=None,
         on_confirm=None,
         on_gain_focus=None,
         on_lose_focus=None,
-        validators=None,
-        initial=None,  # DEPRECATED!
+        validators: Optional[List[callable]] = None,
     ):
-        super().__init__(id=id, style=style)
+        """Create a new single-line text input widget.
 
-        ######################################################################
-        # 2022-09: Backwards compatibility
-        ######################################################################
-        # factory no longer used
-        if factory:
-            warnings.warn("The factory argument is no longer used.", DeprecationWarning)
-        ######################################################################
-        # End backwards compatibility.
-        ######################################################################
+        Inherits from :class:`~toga.widgets.base.Widget`.
+
+        :param id: The ID for the widget.
+        :param style: A style object. If no style is provided, a default style
+            will be applied to the widget.
+        :param value: The initial content to display in the widget.
+        :param readonly: Can the value of the widget be modified by the user?
+        :param placeholder: The content to display as a placeholder when there
+            is no user content to display.
+        :param on_change: A handler that will be invoked when the the value of
+            the widget changes as a result of user input.
+        :param on_confirm: A handler that will be invoked when the user accepts
+            the value of the input (usually by pressing Return on the keyboard).
+        :param on_gain_focus: A handler that will be invoked when the widget
+            gains input focus.
+        :param on_lose_focus: A handler that will be invoked when the widget
+            loses input focus.
+        :param validators: A list of validators to run on the value of the
+            input.
+        """
+        super().__init__(id=id, style=style)
 
         # Create a platform specific implementation of the widget
         self._create()
 
-        ##################################################################
-        # 2022-07: Backwards compatibility
-        ##################################################################
-
-        # initial replaced with value
-        if initial is not None:
-            if value is not None:
-                raise ValueError(
-                    "Cannot specify both `initial` and `value`; "
-                    "`initial` has been deprecated, use `value`"
-                )
-            else:
-                warnings.warn("`initial` has been renamed `value`", DeprecationWarning)
-            value = initial
-
-        ##################################################################
-        # End backwards compatibility.
-        ##################################################################
-
         self.placeholder = placeholder
         self.readonly = readonly
 
-        # Set the actual value before on_change, because we do not want on_change triggered by it
-        # However, we need to prime the handler property in case it is accessed.
+        # Set the actual value before on_change, because we do not want
+        # on_change triggered by it However, we need to prime the handler
+        # property in case it is accessed.
         self._on_change = None
         self._on_confirm = None
+
+        # Set the list of validators before we set the initial value so that
+        # validation is performed on the initial value
+        self.validators = validators
         self.value = value
 
         self.on_change = on_change
         self.on_confirm = on_confirm
-        self.validators = validators
         self.on_lose_focus = on_lose_focus
         self.on_gain_focus = on_gain_focus
 
@@ -95,48 +69,44 @@ class TextInput(Widget):
         self._impl = self.factory.TextInput(interface=self)
 
     @property
-    def readonly(self):
-        """Whether a user can write into the text input.
+    def readonly(self) -> bool:
+        """Can the value of the widget be modified by the user?
 
-        Returns:
-            ``True`` if only read is possible.
-            ``False`` if read and write is possible.
+        This only controls manual changes by the user (i.e., typing at the
+        keyboard). Programmatic changes are permitted while the widget has
+        ``readonly`` enabled.
         """
-        return self._readonly
+        return self._impl.get_readonly()
 
     @readonly.setter
     def readonly(self, value):
-        self._readonly = value
-        self._impl.set_readonly(value)
+        self._impl.set_readonly(bool(value))
 
     @property
     def placeholder(self):
-        """The placeholder text.
+        """The placeholder text for the widget.
 
-        Returns:
-            The placeholder text as a ``str``.
+        A value of ``None`` will be interpreted and returned as an empty string.
+        Any other object will be converted to a string using ``str()``.
         """
-        return self._placeholder
+        return self._impl.get_placeholder()
 
     @placeholder.setter
     def placeholder(self, value):
-        if value is None:
-            self._placeholder = ""
-        else:
-            self._placeholder = str(value)
-        self._impl.set_placeholder(value)
+        self._impl.set_placeholder("" if value is None else str(value))
+        self.refresh()
 
     @property
-    def value(self):
-        """The value of the text input field.
+    def value(self) -> str:
+        """The text to display in the widget.
 
-        Returns:
-            The current text of the widget as a ``str``.
+        A value of ``None`` will be interpreted and returned as an empty string.
+        Any other object will be converted to a string using ``str()``.
         """
         return self._impl.get_value()
 
     @value.setter
-    def value(self, value):
+    def value(self, value: str):
         if value is None:
             v = ""
         else:
@@ -144,30 +114,25 @@ class TextInput(Widget):
         self._impl.set_value(v)
 
     @property
-    def is_valid(self):
+    def is_valid(self) -> bool:
+        "Does the value of the widget currently pass all validators without error?"
         return self._impl.is_valid()
 
     @property
     def on_change(self):
-        """The handler to invoke when the value changes.
+        """The handler to invoke when the value of the widget changes.
 
-        Returns:
-            The function ``callable`` that is called on a content change.
+        This is only invoked in response to user-generated changes.
         """
         return self._on_change
 
     @on_change.setter
     def on_change(self, handler):
-        """Set the handler to invoke when the value is changed.
-
-        Args:
-            handler (:obj:`callable`): The handler to invoke when the value is changed.
-        """
         self._on_change = wrapped_handler(self, handler)
-        self._impl.set_on_change(self._on_change)
 
     @property
-    def validators(self):
+    def validators(self) -> List[callable]:
+        """The list of validators being used to check input on the widget."""
         return self._validators
 
     @validators.setter
@@ -180,33 +145,29 @@ class TextInput(Widget):
 
     @property
     def on_gain_focus(self):
-        """The handler to invoke when the widget get focus.
-
-        Returns:
-            The function ``callable`` that is called on widget focus gain.
-        """
+        """The handler to invoke when the widget gains input focus."""
         return self._on_gain_focus
 
     @on_gain_focus.setter
     def on_gain_focus(self, handler):
         self._on_gain_focus = wrapped_handler(self, handler)
-        self._impl.set_on_gain_focus(self._on_gain_focus)
 
     @property
     def on_lose_focus(self):
-        """The handler to invoke when the widget lose focus.
-
-        Returns:
-            The function ``callable`` that is called on widget focus loss.
-        """
+        """The handler to invoke when the widget loses input focus."""
         return self._on_lose_focus
 
     @on_lose_focus.setter
     def on_lose_focus(self, handler):
         self._on_lose_focus = wrapped_handler(self, handler)
-        self._impl.set_on_lose_focus(self._on_lose_focus)
 
-    def validate(self):
+    def validate(self) -> bool:
+        """Validate the current value of the widget.
+
+        If a problem is found, the widget will be put into an error state.
+
+        :returns: ``True`` if the input is valid; ``False`` otherwise.
+        """
         error_message = None
         for validator in self.validators:
             if error_message is None:
@@ -221,7 +182,9 @@ class TextInput(Widget):
 
     @property
     def on_confirm(self):
-        """The handler to invoke when return is pressed."""
+        """The handler to invoke when the user accepts the value of the widget,
+        usually by pressing return/enter on the keyboard.
+        """
         return self._on_confirm
 
     @on_confirm.setter
