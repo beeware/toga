2ccbd409984c464c5dab01faf6b0249b49e67faf
Russell Keith-Magee <russell@keith-magee.com>
2023-05-19 07:25:55 +0800

    Tweak GTK libs to get coverage.

--- gtk/src/toga_gtk/libs/gtk.py
+++ gtk/src/toga_gtk/libs/gtk.py
@@ -5,32 +5,27 @@ gi.require_version("Gtk", "3.0")
 
 from gi.repository import Gdk, GdkPixbuf, Gio, GLib, GObject, Gtk  # noqa: E402, F401
 
-if Gdk.Screen.get_default() is None:
+if Gdk.Screen.get_default() is None:  # pragma: no cover
     raise RuntimeError(
         "Cannot identify an active display. Is the ``DISPLAY`` environment variable set correctly?"
     )
 
-# The following import will fail if WebKit or its API wrappers aren't
-# installed; handle failure gracefully
-# (see https://github.com/beeware/toga/issues/26)
-# Accept any API version greater than 3.0
-WebKit2 = None
-for version in ["4.0", "3.0"]:
-    try:
-        gi.require_version("WebKit2", version)
-        from gi.repository import WebKit2  # noqa: F401
-
-        break
-    except (ImportError, ValueError):
-        pass
+# The following imports will fail if the underlying libraries or their API
+# wrappers aren't installed; handle failure gracefully (see
+# https://github.com/beeware/toga/issues/26)
+try:
+    gi.require_version("WebKit2", "4.0")
+    from gi.repository import WebKit2  # noqa: F401
+except ImportError:  # pragma: no cover
+    WebKit2 = None
 
 try:
     gi.require_version("Pango", "1.0")
-    from gi.repository import Pango
-except ImportError:
+    from gi.repository import Pango  # noqa: F401
+except ImportError:  # pragma: no cover
     Pango = None
 
 try:
-    import cairo
-except ImportError:
+    import cairo  # noqa: F401
+except ImportError:  # pragma: no cover
     cairo = None
