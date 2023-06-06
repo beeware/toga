7dc18a490c856791f26157df29398b78a6d030dc
Russell Keith-Magee <russell@keith-magee.com>
2023-06-06 15:53:49 +0800

    Tweaks for iOS and Android.

--- iOS/src/toga_iOS/paths.py
+++ iOS/src/toga_iOS/paths.py
@@ -5,14 +5,14 @@ class Paths:
     def __init__(self, interface):
         self.interface = interface
 
-    def get_config_paths(self):
+    def get_config_path(self):
         return Path.home() / "Library" / "Application support" / "Config"
 
-    def get_data_paths(self):
+    def get_data_path(self):
         return Path.home() / "Documents"
 
-    def get_cache_paths(self):
+    def get_cache_path(self):
         return Path.home() / "Library" / "Caches"
 
-    def get_logs_paths(self):
+    def get_logs_path(self):
         return Path.home() / "Library" / "Application support" / "Logs"
