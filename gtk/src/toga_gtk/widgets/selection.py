afde98107b15405772fa60fba298c3d1f87adb2d
MuhammadMouradG <muhammad.mourad1995@gmail.com>
2023-06-11 22:29:09 +0200

    [selection] migrate the selection widget

--- gtk/src/toga_gtk/widgets/selection.py
+++ gtk/src/toga_gtk/widgets/selection.py
@@ -6,35 +6,50 @@ from .base import Widget
 
 class Selection(Widget):
     def create(self):
-        self.native = Gtk.ComboBoxText.new()
-        self.native.connect("changed", self.gtk_on_select)
+        self.string_list = Gtk.StringList()
 
-    def gtk_on_select(self, widget):
+        self.native = Gtk.DropDown.new(model=self.string_list)
+        self.native.set_show_arrow(True)
+        self.native.connect("notify::selected-item", self.gtk_on_select)
+
+    def gtk_on_select(self, widget, data):
         if self.interface.on_select:
             self.interface.on_select(widget)
 
     def remove_all_items(self):
-        self.native.remove_all()
+        items_num = self.string_list.get_n_items()
+        self.string_list.splice(0, items_num, None)
 
     def add_item(self, item):
-        self.native.append_text(item)
+        self.string_list.take(item)
 
         # Gtk.ComboBox does not select the first item, so it's done here.
         if not self.get_selected_item():
             self.select_item(item)
 
     def select_item(self, item):
-        self.native.set_active(self.interface.items.index(item))
+        self.native.set_selected(self.interface.items.index(item))
 
     def get_selected_item(self):
-        return self.native.get_active_text()
+        item = self.native.get_selected_item()
+        if item:
+            return item.get_string()
+        return None
 
     def rehint(self):
-        # width = self.native.get_preferred_width()
-        height = self.native.get_preferred_height()
+        # print(
+        #     "REHINT",
+        #     self,
+        #     self.native.get_preferred_size()[0].width,
+        #     self.native.get_preferred_size()[0].height,
+        # )
+        min_size, size = self.native.get_preferred_size()
+
+        if min_size.width > self.interface._MIN_WIDTH:
+            self.interface._MIN_WIDTH = min_size.width
 
         self.interface.intrinsic.width = at_least(self.interface._MIN_WIDTH)
-        self.interface.intrinsic.height = height[1]
+        self.interface.intrinsic.height = size.height
 
     def set_on_select(self, handler):
         # No special handling required
