4ec985b8a62b2c2c6e0f72b5aec9325d6e018cf6
MuhammadMouradG <muhammad.mourad1995@gmail.com>
2023-06-11 10:17:49 +0200

    [label] migratet label widget

--- gtk/src/toga_gtk/widgets/label.py
+++ gtk/src/toga_gtk/widgets/label.py
@@ -24,12 +24,13 @@ class Label(Widget):
         self.native.set_text(value)
 
     def rehint(self):
-        # print("REHINT", self,
-        #     self.native.get_preferred_width(), self.native.get_preferred_height(),
-        #     getattr(self, '_fixed_height', False), getattr(self, '_fixed_width', False)
+        # print(
+        #     "REHINT",
+        #     self,
+        #     self.native.get_preferred_size()[0].width,
+        #     self.native.get_preferred_size()[0].height,
         # )
-        width = self.native.get_preferred_width()
-        height = self.native.get_preferred_height()
+        min_size, size = self.native.get_preferred_size()
 
-        self.interface.intrinsic.width = at_least(width[0])
-        self.interface.intrinsic.height = height[1]
+        self.interface.intrinsic.width = at_least(min_size.width)
+        self.interface.intrinsic.height = size.height
