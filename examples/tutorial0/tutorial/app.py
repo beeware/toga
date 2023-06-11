bd24cbf27a381074a6be491eb0b960320980320a
MuhammadMouradG <muhammad.mourad1995@gmail.com>
2023-06-11 17:06:40 +0200

    [error] remove unintended commits

--- examples/tutorial0/tutorial/app.py
+++ examples/tutorial0/tutorial/app.py
@@ -3,23 +3,15 @@ import toga
 
 def button_handler(widget):
     print("hello")
-    widget.style.color = "green"
 
-def button_handler2(widget):
-    print("hello")
-    del button.style.color
 
 def build(app):
     box = toga.Box()
 
-    global button
     button = toga.Button("Hello world", on_press=button_handler)
-    button2 = toga.Button("Hello world", on_press=button_handler2)
-    button2.style.padding = 50
     button.style.padding = 50
     button.style.flex = 1
     box.add(button)
-    box.add(button2)
 
     return box
 
