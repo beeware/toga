f558ab840c4343934d6f7248e1359ca1cbd3773a
Russell Keith-Magee <russell@keith-magee.com>
2023-06-22 09:00:46 +0800

    Clarify types and exception docstrings.

--- core/src/toga/images.py
+++ core/src/toga/images.py
@@ -17,16 +17,12 @@ class Image:
 
         An image must be provided either a ``path`` or ``data``, but not both.
 
-        Raises ``FileNotFoundError`` if the a path is provided, but that path does not
-        exist.
-
-        Raises ``ValueError`` if the path or data cannot be loaded as an image.
-
         :param path: Path to the image to load. This can be specified as a string, or as
-              a :any:`pathlib.Path` object. The path can be an absolute file system
-              path, or a path relative to the module that defines your Toga application
-              class.
+            a :any:`pathlib.Path` object. The path can be an absolute file system path,
+            or a path relative to the module that defines your Toga application class.
         :param data: A bytes object with the contents of an image in a supported format.
+        :raises FileNotFoundError: If a path is provided, but that path does not exist.
+        :raises ValueError: If the path or data cannot be loaded as an image.
         """
         if path is None and data is None:
             raise ValueError("Either path or data must be set.")
@@ -62,7 +58,7 @@ class Image:
         """The height of the image, in pixels."""
         return self._impl.get_height()
 
-    def save(self, path):
+    def save(self, path: str | Path):
         """Save image to given path.
 
         The file format of the saved image will be determined by the extension of
