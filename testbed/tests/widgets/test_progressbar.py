ced26bb37a6e01e9dffe2bf8a2ef0c000238a5f2
Russell Keith-Magee <russell@keith-magee.com>
2023-07-07 10:06:34 +0800

    Silence a test cleanup warning when running iOS tests non-slow.

--- testbed/tests/widgets/test_progressbar.py
+++ testbed/tests/widgets/test_progressbar.py
@@ -1,5 +1,3 @@
-import asyncio
-
 import pytest
 
 import toga
@@ -71,10 +69,10 @@ async def test_start_stop_indeterminate(widget, probe):
 
     # Start the progress bar
     widget.start()
-    # We need to actually sleep here, rather than just wait for a redraw,
-    # because some platforms implement their own animation, and we need
-    # to give that animation time to run.
-    await asyncio.sleep(0.1)
+    # We need to enforce a short sleep here, rather than just wait for a redraw, because
+    # some platforms implement their own animation, and we need to give that animation
+    # time to run.
+    await probe.redraw("Indeterminate progress bar is running", delay=0.1)
 
     # Widget should now be started
     assert widget.is_running
@@ -84,7 +82,7 @@ async def test_start_stop_indeterminate(widget, probe):
 
     # Try to change the progress bar value
     widget.value = 0.37
-    await probe.redraw("Progress bar should be changed to 0.37")
+    await probe.redraw("Progress bar should be changed to 0.37", delay=0.1)
 
     # Probe is still running; value doesn't change
     assert widget.is_running
@@ -94,7 +92,7 @@ async def test_start_stop_indeterminate(widget, probe):
 
     # Start the progress bar again. This should be a no-op.
     widget.start()
-    await probe.redraw("Progress bar should be started again")
+    await probe.redraw("Progress bar should be started again", delay=0.1)
 
     # Probe is still running; value doesn't change
     assert widget.is_running
@@ -104,7 +102,7 @@ async def test_start_stop_indeterminate(widget, probe):
 
     # Stop the progress bar
     widget.stop()
-    await probe.redraw("Progress bar should be stopped again")
+    await probe.redraw("Progress bar should be stopped again", delay=0.1)
 
     # Widget should now be stopped
     assert not widget.is_running
@@ -125,7 +123,7 @@ async def test_animation_starts_on_max_change(widget, probe):
 
     # Switch to indeterminate
     widget.max = None
-    await probe.redraw("Progress bar should be switched to indeterminate")
+    await probe.redraw("Progress bar should be switched to indeterminate", delay=0.1)
 
     # Widget is still running, animation has started
     assert widget.is_running
@@ -133,7 +131,7 @@ async def test_animation_starts_on_max_change(widget, probe):
 
     # Switch back to determinate
     widget.max = 50
-    await probe.redraw("Progress bar should be switched to determinate")
+    await probe.redraw("Progress bar should be switched to determinate", delay=0.1)
 
     # Widget is still running, animation has stopped
     assert widget.is_running
