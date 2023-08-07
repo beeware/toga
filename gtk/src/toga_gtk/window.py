ba232fdff5ca3cd3a4aa7798d9fa7149d072e700
proneon267 <45512885+proneon267@users.noreply.github.com>
2023-08-07 04:12:29 -0700

    Modified GTK backend to address reported error.

--- gtk/src/toga_gtk/window.py
+++ gtk/src/toga_gtk/window.py
@@ -155,8 +155,7 @@ class Window:
 
     def get_primary_screen(self):
         display = Gdk.Display.get_default()
-        primary_monitor = display.get_primary_monitor()
-        monitor_native = display.get_monitor(primary_monitor)
+        monitor_native = display.get_primary_monitor()
         for screen in self.interface._app.screens:
             if monitor_native == screen._impl.native:
                 return screen._impl
