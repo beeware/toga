61c68a7f4a93f2df5a2a485b880941fa19ed4597
Daniel Mouris <dgmouris@gmail.com>
2023-09-25 12:24:46 -0600

    Remove libs folder completely

--- android/src/toga_android/libs/android/__init__.py
+++
@@ -1,8 +0,0 @@
-from rubicon.java import JavaClass
-
-R__attr = JavaClass("android/R$attr")
-R__color = JavaClass("android/R$color")
-R__drawable = JavaClass("android/R$drawable")
-R__id = JavaClass("android/R$id")
-R__layout = JavaClass("android/R$layout")
-R__style = JavaClass("android/R$style")
