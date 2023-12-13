ec4078a17a60c2fe7ce340e2337c803ba8d7d28b
Russell Keith-Magee <russell@keith-magee.com>
2023-12-13 13:00:37 +0800

    Add core Camera API tests.

---
+++ dummy/src/toga_dummy/hardware/camera.py
@@ -0,0 +1,53 @@
+from toga.hardware.camera import Camera as TogaCamera
+
+from ..utils import LoggedObject
+
+
+class Camera(LoggedObject):
+    def __init__(self, interface):
+        self.interface = interface
+
+        # -1: permission *could* be granted, but hasn't been
+        # 1: permission has been granted
+        # 0: permission has been denied, or can't be granted
+        self._has_photo_permission = -1
+
+    def has_photo_permission(self, allow_unknown=False):
+        self._action("has photo permission")
+        if allow_unknown:
+            return abs(self._has_photo_permission)
+        else:
+            return self._has_photo_permission > 0
+
+    def request_photo_permission(self, future):
+        self._action("request photo permission")
+        self._has_photo_permission = abs(self._has_photo_permission)
+        future.set_result(self._has_photo_permission != 0)
+
+    def get_devices(self):
+        self._action("get devices")
+        return [TogaCamera.REAR, TogaCamera.FRONT]
+
+    def has_flash(self, device):
+        self._action("has flash", device=device)
+        # Front camera doesn't have a flash.
+        return device == TogaCamera.REAR
+
+    def take_photo(self, future, device, flash):
+        if self.has_photo_permission(allow_unknown=True):
+            self._action(
+                "take photo",
+                permission_requested=self._has_photo_permission < 0,
+                device=device,
+                flash=flash,
+            )
+
+            # Requires that the user has first called `simulate_photo()` with the
+            # photo to be captured.
+            future.set_result(self._photo)
+            del self._photo
+        else:
+            raise PermissionError("App does not have permission to take photos")
+
+    def simulate_photo(self, image):
+        self._photo = image
