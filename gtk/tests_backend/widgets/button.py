78483f2c0d0a601eb0f428b68090432c15d3c596
Russell Keith-Magee <russell@keith-magee.com>
2023-12-31 16:19:16 +0800

    Add Winforms and GTK icon buttons.

--- gtk/tests_backend/widgets/button.py
+++ gtk/tests_backend/widgets/button.py
@@ -1,3 +1,5 @@
+import pytest
+
 from toga.colors import TRANSPARENT
 from toga_gtk.libs import Gtk
 
@@ -10,6 +12,16 @@ class ButtonProbe(SimpleProbe):
     @property
     def text(self):
         return self.native.get_label()
+
+    def assert_no_icon(self):
+        assert self.native.get_image() is None
+
+    def assert_icon_size(self):
+        icon = self.native.get_image().get_pixbuf()
+        if icon:
+            assert (icon.get_width(), icon.get_height()) == (32, 32)
+        else:
+            pytest.fail("Icon does not exist")
 
     @property
     def background_color(self):
