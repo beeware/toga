0eff81a65391edf507136d481fcda5de2ef9e455
MuhammadMuradG <muhammad.murad.eg@gmail.com>
2024-01-27 00:43:36 +0200

    [imageview] Fix backend implementation.

--- gtk/src/toga_gtk/widgets/imageview.py
+++ gtk/src/toga_gtk/widgets/imageview.py
@@ -4,54 +4,59 @@ from ..libs import Gdk, GdkPixbuf, Gtk
 from .base import Widget
 
 
+class TogaPicture(Gtk.Picture):
+    def do_size_allocate(self, width, height, baseline):
+        Gtk.Picture.do_size_allocate(self, width, height, baseline)
+        if self.interface.image:
+            self._impl.set_scaled_pixbuf(self.interface.image, width, height)
+
+
 class ImageView(Widget):
     def create(self):
-        self.native = Gtk.Image
-        self.native.connect("size-allocate", self.gtk_size_allocate)
+        self.native = TogaPicture
         self._aspect_ratio = None
 
     def set_image(self, image):
         if image:
-            self.set_scaled_pixbuf(image._impl.native, self.native.get_allocation())
+            self.set_scaled_pixbuf(
+                image,
+                self.native.compute_bounds(self.native)[1].size.width,
+                self.native.compute_bounds(self.native)[1].size.height,
+            )
         else:
-            self.native.set_from_paintable(None)
+            self.native.set_paintable(None)
 
-    def gtk_size_allocate(self, widget, allocation):
-        # GTK doesn't have any native image resizing; so, when the Gtk.Image
-        # has a new size allocated, we need to manually scale the native pixbuf
-        # to the preferred size as a result of resizing the image.
-        if self.interface.image:
-            self.set_scaled_pixbuf(self.interface.image._impl.native, allocation)
-
-    def set_scaled_pixbuf(self, image, allocation):
+    def set_scaled_pixbuf(self, image, width, height):
         if self._aspect_ratio is None:
             # Don't preserve aspect ratio; image fits the available space.
-            image_width = allocation.width
-            image_height = allocation.height
+            self.native.set_content_fit(Gtk.ContentFit.FILL)
+            image_width = width
+            image_height = height
         else:
             # Determine what the width/height of the image would be
             # preserving the aspect ratio. If the scaled size exceeds
             # the allocated size, then that isn't the dimension
             # being preserved.
-            candidate_width = int(allocation.height * self._aspect_ratio)
-            candidate_height = int(allocation.width / self._aspect_ratio)
-            if candidate_width > allocation.width:
-                image_width = allocation.width
+            self.native.set_content_fit(Gtk.ContentFit.CONTAIN)
+            candidate_width = int(height * self._aspect_ratio)
+            candidate_height = int(width / self._aspect_ratio)
+            if candidate_width > width:
+                image_width = width
                 image_height = candidate_height
             else:
                 image_width = candidate_width
-                image_height = allocation.height
+                image_height = height
 
         # Minimum image size is 1x1
         image_width = max(1, image_width)
         image_height = max(1, image_height)
 
         # Scale the pixbuf to fit the provided space.
-        scaled = self.interface.image._impl.native.scale_simple(
+        scaled = image._impl.native.scale_simple(
             image_width, image_height, GdkPixbuf.InterpType.BILINEAR
         )
 
-        self.native.set_from_paintable(Gdk.Texture.new_for_pixbuf(scaled))
+        self.native.set_paintable(Gdk.Texture.new_for_pixbuf(scaled))
 
     def rehint(self):
         width, height, self._aspect_ratio = rehint_imageview(
