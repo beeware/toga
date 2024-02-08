d29340fca79daff974b41b2e9530606b073cc9cd
Russell Keith-Magee <russell@keith-magee.com>
2024-02-08 11:41:02 +0800

    Extend demo app to control and update pins.

--- examples/mapview/mapview/app.py
+++ examples/mapview/mapview/app.py
@@ -27,6 +27,18 @@ class ExampleMapViewApp(toga.App):
     def where_am_i(self, widget, **kwargs):
         self.label.text = self.map_view.location
 
+    def goto_pin_1(self, widget, **kwargs):
+        self.map_view.location = self.pin_1.location
+
+    def goto_pin_2(self, widget, **kwargs):
+        self.map_view.location = self.pin_2.location
+
+    def goto_pin_3(self, widget, **kwargs):
+        self.map_view.location = self.pin_3.location
+
+    def move_carmen(self, widget, **kwargs):
+        self.pin_3.location = (random.uniform(-70, 70), random.uniform(-180, 180))
+
     def pin_selected(self, widget, pin, **kwargs):
         self.label.text = f"Pin selected: {pin}"
 
@@ -34,30 +46,29 @@ class ExampleMapViewApp(toga.App):
         # Set up main window
         self.main_window = toga.MainWindow()
 
+        self.pin_1 = toga.MapPin(
+            (-41.2784, 174.7767),
+            title="The Bee Hive",
+            subtitle="NZ Parliament building",
+        )
+        self.pin_2 = toga.MapPin(
+            (36.47032, -86.65138),
+            title="The White House",
+            subtitle="They have beehives!",
+        )
+
         self.map_view = toga.MapView(
             style=Pack(flex=1),
-            pins=[
-                toga.MapPin(
-                    (-41.2784, 174.7767),
-                    title="The Bee Hive",
-                    subtitle="NZ Parliament building",
-                ),
-                toga.MapPin(
-                    (36.47032, -86.65138),
-                    title="The White House",
-                    subtitle="They have beehives!",
-                ),
-            ],
+            pins=[self.pin_1, self.pin_2],
             on_select=self.pin_selected,
         )
 
         # Add a pin at runtime.
-        self.map_view.pins.add(
-            toga.MapPin(
-                (random.uniform(-70, 70), random.uniform(-180, 180)),
-                title="Carmen Sandiego",
-            )
+        self.pin_3 = toga.MapPin(
+            (random.uniform(-70, 70), random.uniform(-180, 180)),
+            title="Carmen Sandiego",
         )
+        self.map_view.pins.add(self.pin_3)
 
         btn_style = Pack(flex=1)
         # Location buttons
@@ -80,12 +91,22 @@ class ExampleMapViewApp(toga.App):
             style=Pack(direction=ROW, padding=5),
         )
 
+        # Point Of Interest buttons
+        btn_pin_1 = toga.Button("Pin 1", on_press=self.goto_pin_1, style=btn_style)
+        btn_pin_2 = toga.Button("Pin 2", on_press=self.goto_pin_2, style=btn_style)
+        btn_pin_3 = toga.Button("Pin 3", on_press=self.goto_pin_3, style=btn_style)
+        btn_move = toga.Button("Move", on_press=self.move_carmen, style=btn_style)
+        pin_box = toga.Box(
+            children=[btn_pin_1, btn_pin_2, btn_pin_3, btn_move],
+            style=Pack(direction=ROW, padding=5),
+        )
+
         # Label to show responses.
         self.label = toga.Label("Ready.", style=Pack(padding=5))
 
         # Add the content on the main window
         self.main_window.content = toga.Box(
-            children=[self.map_view, location_box, zoom_box, self.label],
+            children=[self.map_view, location_box, zoom_box, pin_box, self.label],
             style=Pack(flex=1, direction=COLUMN),
         )
 
