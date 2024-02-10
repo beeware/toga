f5bfaded819cdbf25cee4d48cf0b16f1a7a8ee19
Charles Whittington <charleswhitt@gmail.com>
2024-02-10 13:39:21 -0500

    Accidentally didn't add the converter to the commit

---
+++ dummy/src/toga_dummy/image_converter.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+import toga
+
+
+class CustomImage:
+    pass
+
+
+class CustomImageSubclass(CustomImage):
+    pass
+
+
+image_class = CustomImage
+
+
+def convert_from_format(image_in_format):
+    return (Path(__file__).parent / "resources/sample.png").read_bytes()
+
+
+def convert_to_format(data, image_class):
+    image = image_class()
+    image.size = toga.Image(data).size
+    return image
