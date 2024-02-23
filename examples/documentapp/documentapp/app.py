3cf7414c0d73b0c067da84eb64c69f8cf8aeb06a
Russell Keith-Magee <russell@keith-magee.com>
2024-02-23 10:41:12 +0800

    Update example DocumentApp, and add SimpleApp and StatusIconApp examples.

--- examples/documentapp/documentapp/app.py
+++ examples/documentapp/documentapp/app.py
@@ -2,8 +2,8 @@ import toga
 
 
 class ExampleDocument(toga.Document):
-    def __init__(self, path, app):
-        super().__init__(path=path, document_type="Example Document", app=app)
+    document_type = "Example Document"
+    default_extension = ".exampledoc"
 
     async def can_close(self):
         return await self.main_window.question_dialog(
@@ -13,21 +13,28 @@ class ExampleDocument(toga.Document):
 
     def create(self):
         # Create the main window for the document.
-        self.main_window = toga.DocumentMainWindow(
-            doc=self,
-            title=f"Example: {self.path.name}",
-        )
+        self.main_window = toga.DocumentMainWindow(doc=self)
         self.main_window.content = toga.MultilineTextInput()
 
     def read(self):
         with self.path.open() as f:
-            self.content = f.read()
+            self.main_window.content.value = f.read()
 
-        self.main_window.content.value = self.content
+    def write(self):
+        with self.path.open("w") as f:
+            f.write(self.main_window.content.value)
+
+
+class ExampleDocumentApp(toga.App):
+    def startup(self):
+        # A document app has no main window. A window (or windows) will be created from
+        # the document(s) specified at the command line; or if no document is specified,
+        # the platform will determine how to create an empty document.
+        self.main_window = None
 
 
 def main():
-    return toga.DocumentApp(
+    return ExampleDocumentApp(
         "Document App",
         "org.beeware.toga.examples.documentapp",
         document_types={
