df35463de3699a65df4c137d8e97e2cb6e7d688e
Russell Keith-Magee <russell@keith-magee.com>
2024-03-22 12:38:20 +0800

    Add geolocation to the hardware example.

--- examples/hardware/hardware/app.py
+++ examples/hardware/hardware/app.py
@@ -12,11 +12,21 @@ class ExampleHardwareApp(toga.App):
         except NotImplementedError:
             print("The Camera API is not implemented on this platform")
 
+        try:
+            # This will provide a prompt for geolocation permissions at startup.
+            # If permission is denied, the app will continue.
+            self.geolocation.request_permission()
+        except NotImplementedError:
+            print("The Geolocation API is not implemented on this platform")
+
+        #############################################################
+        # Camera
+        #############################################################
         self.photo = toga.ImageView(
             image=toga.Image("resources/default.png"), style=Pack(width=200)
         )
 
-        main_box = toga.Box(
+        camera_box = toga.Box(
             children=[
                 toga.Box(
                     children=[
@@ -45,8 +55,50 @@ class ExampleHardwareApp(toga.App):
             style=Pack(direction=COLUMN, padding_bottom=20),
         )
 
+        #############################################################
+        # Geolocation
+        #############################################################
+
+        self.map_view = toga.MapView(style=Pack(flex=1))
+        self.pin = None
+        self.geolocation.on_change = self.location_changed
+
+        geo_box = toga.Box(
+            children=[
+                self.map_view,
+                toga.Box(
+                    children=[
+                        toga.Button(
+                            "Update", on_press=self.update_location, style=Pack(flex=1)
+                        ),
+                        toga.Button(
+                            "Start",
+                            on_press=self.start_location_updates,
+                            style=Pack(flex=1),
+                        ),
+                        toga.Button(
+                            "Stop",
+                            on_press=self.stop_location_updates,
+                            style=Pack(flex=1),
+                        ),
+                    ],
+                    style=Pack(padding=5),
+                ),
+            ],
+            style=Pack(direction=COLUMN),
+        )
+
+        #############################################################
+        # Main app
+        #############################################################
+
         self.main_window = toga.MainWindow(title=self.formal_name)
-        self.main_window.content = main_box
+        self.main_window.content = toga.OptionContainer(
+            content=[
+                toga.OptionItem("Camera", camera_box),
+                toga.OptionItem("Geo", geo_box),
+            ]
+        )
         self.main_window.show()
 
     async def take_photo(self, widget, **kwargs):
@@ -60,6 +112,31 @@ class ExampleHardwareApp(toga.App):
             await self.main_window.info_dialog(
                 "Oh no!", "You have not granted permission to take photos"
             )
+
+    def location_changed(self, geo, location, altitude, **kwargs):
+        print("APPLY LOCATION CHANGE")
+        self.map_view.zoom = 16
+        self.map_view.location = location
+
+        if self.pin is None:
+            self.pin = toga.MapPin(location, title="Here!")
+            self.map_view.pins.add(self.pin)
+        else:
+            self.pin.location = location
+
+    async def update_location(self, widget, **kwargs):
+        print("Request location update")
+        location = await self.geolocation.current_location
+        print("GOT LOCATION", location)
+        self.location_changed(None, location, None)
+
+    def start_location_updates(self, widget, **kwargs):
+        print("START LOCATION UPDATES")
+        self.geolocation.start()
+
+    def stop_location_updates(self, widget, **kwargs):
+        print("STOP LOCATION UPDATES")
+        self.geolocation.stop()
 
 
 def main():
