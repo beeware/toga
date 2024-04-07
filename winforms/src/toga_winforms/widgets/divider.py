4b5ed6f322f5c67fe41cdcc806f2ad20d39d72a6
proneon267 <proneon267@gmail.com>
2024-04-07 05:55:06 -0700

    Fixed winforms divider implementation

--- winforms/src/toga_winforms/widgets/divider.py
+++ winforms/src/toga_winforms/widgets/divider.py
@@ -1,6 +1,7 @@
 from decimal import ROUND_UP
 
 import System.Windows.Forms as WinForms
+from System.Drawing import Color
 from travertino.size import at_least
 
 from .base import Widget
@@ -8,8 +9,7 @@ from .base import Widget
 
 class Divider(Widget):
     def create(self):
-        self.native = WinForms.Label()
-        self.native.BorderStyle = WinForms.BorderStyle.Fixed3D
+        self.native = WinForms.Panel()
         self.native.AutoSize = False
 
         self._direction = self.interface.HORIZONTAL
@@ -20,11 +20,18 @@ class Divider(Widget):
     def set_direction(self, value):
         self._direction = value
         if value == self.interface.HORIZONTAL:
-            self.native.Height = 2
+            self.native.Height = 1
             self.native.Width = 0
         else:
             self.native.Height = 0
-            self.native.Width = 2
+            self.native.Width = 1
+
+    def set_background_color(self, color):
+        if color is None:
+            # Background color needs to be set or else divider will not be visible.
+            self.native.BackColor = Color.Gray
+        else:
+            super().set_background_color(color)
 
     def rehint(self):
         if self.get_direction() == self.interface.HORIZONTAL:
