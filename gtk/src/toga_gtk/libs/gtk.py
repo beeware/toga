fb9e4a89751a4400a1bc78e01ee97e3a4007748c
Russell Keith-Magee <russell@keith-magee.com>
2024-05-01 09:40:46 +0800

    Get the no-cover in the right place this time.

--- gtk/src/toga_gtk/libs/gtk.py
+++ gtk/src/toga_gtk/libs/gtk.py
@@ -16,8 +16,8 @@ if Gdk.Screen.get_default() is None:  # pragma: no cover
 try:
     try:
         gi.require_version("WebKit2", "4.1")
-    except ValueError:
-        gi.require_version("WebKit2", "4.0")  # pragma: no cover
+    except ValueError:  # pragma: no cover
+        gi.require_version("WebKit2", "4.0")
     from gi.repository import WebKit2  # noqa: F401
 except (ImportError, ValueError):  # pragma: no cover
     WebKit2 = None
