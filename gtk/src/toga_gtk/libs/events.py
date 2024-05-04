f1e71493e5265f89430d114734dab5d3b6a0476c
Russell Keith-Magee <russell@keith-magee.com>
2024-05-04 13:22:38 +0800

    Add some asyncio-glib patches that haven't been merged.

--- gtk/src/toga_gtk/libs/events.py
+++ gtk/src/toga_gtk/libs/events.py
@@ -3,22 +3,27 @@
 #
 #     https://github.com/jhenstridge/asyncio-glib
 #
+# It includes patches from pull requests against that project:
+#
+#   * #9, contributed by Benjamin Berg
+#   * #6, contributed by Niels Avonds
+#
+# ------------------------------------------------------------------------
 # Copyright (C) James Henstridge
 #
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) any later version.
+# This library is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 2.1 of the License, or (at your option)
+# any later version.
 #
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
 #
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
-#
+# You should have received a copy of the GNU Lesser General Public License along
+# with this library; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 ##########################################################################
 
 import asyncio
@@ -26,23 +31,18 @@ import selectors
 
 from .gtk import GLib
 
-try:
-    g_main_loop_run = super(GLib.MainLoop, GLib.MainLoop).run
-except AttributeError:
-    g_main_loop_run = GLib.MainLoop.run
-
 
 class _SelectorSource(GLib.Source):
     """A GLib source that gathers selectors."""
 
-    def __init__(self, main_loop):
+    def __init__(self):
         super().__init__()
         self._fd_to_tag = {}
         self._fd_to_events = {}
-        self._main_loop = main_loop
+        self._iteration_timeout = 0
 
     def prepare(self):
-        return False, -1
+        return False, self._iteration_timeout
 
     def check(self):
         return False
@@ -53,10 +53,11 @@ class _SelectorSource(GLib.Source):
             events = self._fd_to_events.setdefault(fd, 0)
             if condition & GLib.IOCondition.IN:
                 events |= selectors.EVENT_READ
+            if condition & GLib.IOCondition.HUP:
+                events |= selectors.EVENT_READ
             if condition & GLib.IOCondition.OUT:
                 events |= selectors.EVENT_WRITE
             self._fd_to_events[fd] = events
-        self._main_loop.quit()
         return GLib.SOURCE_CONTINUE
 
     def register(self, fd, events):
@@ -65,6 +66,7 @@ class _SelectorSource(GLib.Source):
         condition = GLib.IOCondition(0)
         if events & selectors.EVENT_READ:
             condition |= GLib.IOCondition.IN
+            condition |= GLib.IOCondition.HUP
         if events & selectors.EVENT_WRITE:
             condition |= GLib.IOCondition.OUT
         self._fd_to_tag[fd] = self.add_unix_fd(fd, condition)
@@ -85,8 +87,7 @@ class GLibSelector(selectors._BaseSelectorImpl):
     def __init__(self, context):
         super().__init__()
         self._context = context
-        self._main_loop = GLib.MainLoop.new(self._context, False)
-        self._source = _SelectorSource(self._main_loop)
+        self._source = _SelectorSource()
         self._source.attach(self._context)
 
     def close(self):
@@ -104,21 +105,15 @@ class GLibSelector(selectors._BaseSelectorImpl):
         return key
 
     def select(self, timeout=None):
-        may_block = True
-        self._source.set_ready_time(-1)
+        # Calling .set_ready_time() always causes a mainloop iteration to finish.
         if timeout is not None:
-            if timeout > 0:
-                self._source.set_ready_time(
-                    GLib.get_monotonic_time() + int(timeout * 1000000)
-                )
-            else:
-                may_block = False
+            # Negative timeout implies immediate dispatch
+            self._source._iteration_timeout = int(max(0, timeout) * 1000)
+        else:
+            self._source._iteration_timeout = -1
 
         self._source.clear()
-        if may_block:
-            g_main_loop_run(self._main_loop)
-        else:
-            self._context.iteration(False)
+        self._context.iteration(False)
 
         ready = []
         for key in self.get_map().values():
@@ -132,6 +127,7 @@ class GtkEventLoop(asyncio.SelectorEventLoop):
     def __init__(self):
         selector = GLibSelector(GLib.MainContext.default())
         super().__init__(selector)
+        self._clock_resolution = 1e-3
 
 
 class GtkEventLoopPolicy(asyncio.DefaultEventLoopPolicy):
