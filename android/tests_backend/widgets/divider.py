aad928cfe39a94e991752d44c59d831b5e0a7ce1
proneon267 <proneon267@gmail.com>
2024-05-08 08:55:09 -0700

    Minor modification on android test backend

--- android/tests_backend/widgets/divider.py
+++ android/tests_backend/widgets/divider.py
@@ -1,16 +1,7 @@
-from android.graphics import Color
 from java import jclass
 
 from .base import SimpleProbe
-from .properties import assert_color, toga_color
 
 
 class DividerProbe(SimpleProbe):
     native_class = jclass("android.view.View")
-
-    def assert_background_color(self, color):
-        actual_background_color = toga_color(self.native.getBackground().getColor())
-        if color is None:
-            assert_color(actual_background_color, toga_color(Color.LTGRAY))
-        else:
-            assert_color(actual_background_color, color)
