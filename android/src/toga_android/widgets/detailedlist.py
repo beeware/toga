679755e0747ce18b6c80ef2f77336594a7a5d3c5
Gwen Sarapata <gwendolynsarapata@gmail.com>
2024-05-22 13:32:56 -0400

    Soften Runtime Requirement for Android SwipeRefreshLayout #2454

--- android/src/toga_android/widgets/detailedlist.py
+++ android/src/toga_android/widgets/detailedlist.py
@@ -6,8 +6,15 @@ from android.content import DialogInterface
 from android.graphics import Rect
 from android.view import Gravity, View
 from android.widget import ImageView, LinearLayout, RelativeLayout, ScrollView, TextView
-from androidx.swiperefreshlayout.widget import SwipeRefreshLayout
 from java import dynamic_proxy
+
+try:
+    from androidx.swiperefreshlayout.widget import SwipeRefreshLayout
+except ImportError:  # pragma: no cover
+    # Import will fail if SwipeRefreshLayout is not listed in dependencies
+    # No cover due to not being able to test in CI
+    SwipeRefreshLayout = None
+
 
 from .base import Widget
 
@@ -89,6 +96,12 @@ class OnRefreshListener(dynamic_proxy(SwipeRefreshLayout.OnRefreshListener)):
 
 class DetailedList(Widget):
     def create(self):
+        if SwipeRefreshLayout is None:  # pragma: no cover
+            raise RuntimeError(
+                "Unable to import SwipeRefreshLayout. Ensure that the AndroidX Swipe Refresh Layout "
+                "widget package (androidx.swiperefreshlayout:swiperefreshlayout:1.1.0)"
+                "is listed in your app's dependencies."
+            )
         # get the selection color from the current theme
         attrs = [R.attr.colorBackground, R.attr.colorControlHighlight]
         typed_array = self._native_activity.obtainStyledAttributes(attrs)
