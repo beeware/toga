284f8d55655ab14c69476ed08117c99bfc389c41
KRRT7 <106575910+KRRT7@users.noreply.github.com>
2024-06-01 22:13:47 +0000

    various loaders
    Update factory.py
    
    Cam Lock
    
    sorted lazy mobile
    
    lazy mobiles
    
    final final stretch
    
    final stretch
    
    pre-commit
    
    coverage
    
    fix up the fixup of the fixups
    
    fixup the fixups
    
    fixup
    
    precommit
    
    fixup
    
    fixup
    
    not_impl fn
    
    hiccup
    
    precommit

--- winforms/src/toga_winforms/factory.py
+++ winforms/src/toga_winforms/factory.py
@@ -1,80 +1,63 @@
-from toga import NotImplementedWarning
+import importlib
 
-from . import dialogs
-from .app import App, MainWindow
-from .command import Command
-from .fonts import Font
-from .icons import Icon
-from .images import Image
-from .paths import Paths
-from .widgets.box import Box
-from .widgets.button import Button
-from .widgets.canvas import Canvas
-from .widgets.dateinput import DateInput
-from .widgets.detailedlist import DetailedList
-from .widgets.divider import Divider
-from .widgets.imageview import ImageView
-from .widgets.label import Label
-from .widgets.mapview import MapView
-from .widgets.multilinetextinput import MultilineTextInput
-from .widgets.numberinput import NumberInput
-from .widgets.optioncontainer import OptionContainer
-from .widgets.passwordinput import PasswordInput
-from .widgets.progressbar import ProgressBar
-from .widgets.scrollcontainer import ScrollContainer
-from .widgets.selection import Selection
-from .widgets.slider import Slider
-from .widgets.splitcontainer import SplitContainer
-from .widgets.switch import Switch
-from .widgets.table import Table
-from .widgets.textinput import TextInput
-from .widgets.timeinput import TimeInput
-from .widgets.webview import WebView
-from .window import Window
+from toga import NotImplementedWarning
 
 
 def not_implemented(feature):
-    NotImplementedWarning.warn("Winforms", feature)  # pragma: nocover
+    NotImplementedWarning.warn("winforms", feature)
 
 
-__all__ = [
-    "not_implemented",
-    "App",
-    "MainWindow",
-    "Command",
-    # Resources
-    "Font",
-    "Icon",
-    "Image",
-    "Paths",
-    "dialogs",
-    # Widgets
-    "Box",
-    "Button",
-    "Canvas",
-    "DateInput",
-    "DetailedList",
-    "Divider",
-    "ImageView",
-    "Label",
-    "MapView",
-    "MultilineTextInput",
-    "NumberInput",
-    "OptionContainer",
-    "PasswordInput",
-    "ProgressBar",
-    "ScrollContainer",
-    "Selection",
-    "Slider",
-    "SplitContainer",
-    "Switch",
-    "Table",
-    "TextInput",
-    "TimeInput",
-    "WebView",
-    "Window",
-]
+winforms_factory_imports = {
+    "App": "toga_winforms.app",
+    "MainWindow": "toga_winforms.app",
+    "Command": "toga_winforms.command",
+    "dialogs": "toga_winforms",
+    "Font": "toga_winforms.fonts",
+    "Icon": "toga_winforms.icons",
+    "Image": "toga_winforms.images",
+    "Paths": "toga_winforms.paths",
+    "Box": "toga_winforms.widgets.box",
+    "Button": "toga_winforms.widgets.button",
+    "Canvas": "toga_winforms.widgets.canvas",
+    "DetailedList": "toga_winforms.widgets.detailedlist",
+    "Divider": "toga_winforms.widgets.divider",
+    "DateInput": "toga_winforms.widgets.dateinput",
+    "ImageView": "toga_winforms.widgets.imageview",
+    "Label": "toga_winforms.widgets.label",
+    "MapView": "toga_winforms.widgets.mapview",
+    "MultilineTextInput": "toga_winforms.widgets.multilinetextinput",
+    "NumberInput": "toga_winforms.widgets.numberinput",
+    "OptionContainer": "toga_winforms.widgets.optioncontainer",
+    "PasswordInput": "toga_winforms.widgets.passwordinput",
+    "ProgressBar": "toga_winforms.widgets.progressbar",
+    "ScrollContainer": "toga_winforms.widgets.scrollcontainer",
+    "Selection": "toga_winforms.widgets.selection",
+    "Slider": "toga_winforms.widgets.slider",
+    "SplitContainer": "toga_winforms.widgets.splitcontainer",
+    "Switch": "toga_winforms.widgets.switch",
+    "Table": "toga_winforms.widgets.table",
+    "TextInput": "toga_winforms.widgets.textinput",
+    "TimeInput": "toga_winforms.widgets.timeinput",
+    "WebView": "toga_winforms.widgets.webview",
+    "Window": "toga_winforms.window",
+}
+
+__all__ = list(winforms_factory_imports.keys()) + ["not_implemented"]
 
 
-def __getattr__(name):  # pragma: no cover
-    raise NotImplementedError(f"Toga's Winforms backend doesn't implement {name}")
+def __getattr__(name):
+    try:
+        module_name = winforms_factory_imports[name]
+
+        has_dot = module_name.find(".") != -1
+        if not has_dot:
+            module_name = f"toga_winforms.{name}"
+    except KeyError:
+        raise NotImplementedError(
+            f"Toga's Winforms backend doesn't implement '{name}'"
+        ) from None
+    else:
+        module = importlib.import_module(module_name)
+        value = getattr(module, name) if has_dot else module
+        globals()[name] = value
+        return value
