d357b0bcac8921430d8bf88c114596fe6db0a236
Russell Keith-Magee <russell@keith-magee.com>
2024-06-07 11:34:26 +0800

    Remove a safety catch that is no longer required.

--- winforms/src/toga_winforms/app.py
+++ winforms/src/toga_winforms/app.py
@@ -204,11 +204,6 @@ class App:
         return submenu
 
     def create_menus(self):
-        if self.interface.main_window is None:  # pragma: no branch
-            # The startup method may create commands before creating the window, so
-            # we'll call create_menus again after it returns.
-            return
-
         window = self.interface.main_window._impl
         menubar = window.native.MainMenuStrip
         if menubar:
