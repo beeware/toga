b2d0c4782f134e168b4093e0415a7eddde6f86f1
proneon267 <45512885+proneon267@users.noreply.github.com>
2024-06-12 08:33:13 -0700

    Update testbed/tests/widgets/test_button.py
    Co-authored-by: Russell Keith-Magee <russell@keith-magee.com>

--- testbed/tests/widgets/test_button.py
+++ testbed/tests/widgets/test_button.py
@@ -101,4 +101,4 @@ async def test_background_color_transparent(widget, probe):
     widget.style.background_color = TRANSPARENT
     await probe.redraw("Button background color should be reset to the default color")
 
-    assert_background_color(probe.background_color, resetted_background_color)
+    assert_background_color(probe.background_color, original_background_color)
