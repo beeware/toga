564c5ed2fe7c3b069d62df491e5c7746d3720731
proneon267 <proneon267@gmail.com>
2024-06-22 18:48:22 -0700

    Updated to latest `main` branch

--- core/tests/window/test_window.py
+++ core/tests/window/test_window.py
@@ -457,8 +457,8 @@ def test_non_resizable_window_state(state):
     non_resizable_window.close()
 
 
-def test_window_state_invalid_value(window):
-    # Setting window state to any value other than a WindowState enum should raise a ValueError
+def test_window_state_invalid_parameter(window):
+    # Setting window state to any value other than a WindowState enum should raise a ValueError.
     with pytest.raises(
         ValueError,
         match="Invalid type for state parameter. Expected WindowState enum type.",
