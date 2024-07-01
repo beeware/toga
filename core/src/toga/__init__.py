84b1241b4ce3b82c0d4cf7282f96c58f6faff5d4
KRRT7 <106575910+KRRT7@users.noreply.github.com>
2024-07-01 08:53:28 +0000

    toga_core lazy loader

--- core/src/toga/__init__.py
+++ core/src/toga/__init__.py
@@ -1,54 +1,81 @@
 from __future__ import annotations
 
+import importlib
+import importlib.util
 import warnings
-from pathlib import Path
 
-from .app import App, DocumentApp
-from .colors import hsl, hsla, rgb, rgba
-from .command import Command, Group
-from .dialogs import (
-    ConfirmDialog,
-    ErrorDialog,
-    InfoDialog,
-    OpenFileDialog,
-    QuestionDialog,
-    SaveFileDialog,
-    SelectFolderDialog,
-    StackTraceDialog,
-)
-from .documents import Document
-from .fonts import Font
-from .icons import Icon
-from .images import Image
-from .keys import Key
-from .types import LatLng, Position, Size
-from .widgets.activityindicator import ActivityIndicator
-from .widgets.base import Widget
-from .widgets.box import Box
-from .widgets.button import Button
-from .widgets.canvas import Canvas
-from .widgets.dateinput import DateInput, DatePicker
-from .widgets.detailedlist import DetailedList
-from .widgets.divider import Divider
-from .widgets.imageview import ImageView
-from .widgets.label import Label
-from .widgets.mapview import MapPin, MapView
-from .widgets.multilinetextinput import MultilineTextInput
-from .widgets.numberinput import NumberInput
-from .widgets.optioncontainer import OptionContainer, OptionItem
-from .widgets.passwordinput import PasswordInput
-from .widgets.progressbar import ProgressBar
-from .widgets.scrollcontainer import ScrollContainer
-from .widgets.selection import Selection
-from .widgets.slider import Slider
-from .widgets.splitcontainer import SplitContainer
-from .widgets.switch import Switch
-from .widgets.table import Table
-from .widgets.textinput import TextInput
-from .widgets.timeinput import TimeInput, TimePicker
-from .widgets.tree import Tree
-from .widgets.webview import WebView
-from .window import DocumentMainWindow, MainWindow, Window
+toga_core_imports = {
+    "ActivityIndicator": "toga.widgets.activityindicator",
+    "App": "toga.app",
+    "DocumentApp": "toga.app",
+    "DocumentMainWindow": "toga.app",
+    "MainWindow": "toga.app",
+    "Widget": "toga.widgets.base",
+    "Box": "toga.widgets.box",
+    "Button": "toga.widgets.button",
+    "Canvas": "toga.widgets.canvas",
+    "hsl": "toga.colors",
+    "hsla": "toga.colors",
+    "rgb": "toga.colors",
+    "rgba": "toga.colors",
+    "Command": "toga.command",
+    "Group": "toga.command",
+    "DateInput": "toga.widgets.dateinput",
+    "DatePicker": "toga.widgets.dateinput",
+    "DetailedList": "toga.widgets.detailedlist",
+    "ConfirmDialog": "toga.dialogs",
+    "ErrorDialog": "toga.dialogs",
+    "InfoDialog": "toga.dialogs",
+    "OpenFileDialog": "toga.dialogs",
+    "QuestionDialog": "toga.dialogs",
+    "SaveFileDialog": "toga.dialogs",
+    "SelectFolderDialog": "toga.dialogs",
+    "StackTraceDialog": "toga.dialogs",
+    "Divider": "toga.widgets.divider",
+    "Document": "toga.documents",
+    "Font": "toga.fonts",
+    "Icon": "toga.icons",
+    "Image": "toga.images",
+    "ImageView": "toga.widgets.imageview",
+    "Key": "toga.keys",
+    "Label": "toga.widgets.label",
+    "MapPin": "toga.widgets.mapview",
+    "MapView": "toga.widgets.mapview",
+    "MultilineTextInput": "toga.widgets.multilinetextinput",
+    "NumberInput": "toga.widgets.numberinput",
+    "OptionContainer": "toga.widgets.optioncontainer",
+    "OptionItem": "toga.widgets.optioncontainer",
+    "PasswordInput": "toga.widgets.passwordinput",
+    "ProgressBar": "toga.widgets.progressbar",
+    "ScrollContainer": "toga.widgets.scrollcontainer",
+    "Selection": "toga.widgets.selection",
+    "Slider": "toga.widgets.slider",
+    "SplitContainer": "toga.widgets.splitcontainer",
+    "Switch": "toga.widgets.switch",
+    "Table": "toga.widgets.table",
+    "TextInput": "toga.widgets.textinput",
+    "TimeInput": "toga.widgets.timeinput",
+    "TimePicker": "toga.widgets.timeinput",
+    "Tree": "toga.widgets.tree",
+    "LatLng": "toga.types",
+    "Position": "toga.types",
+    "Size": "toga.types",
+    "WebView": "toga.widgets.webview",
+    "Window": "toga.window",
+}
+__all__ = list(toga_core_imports.keys())
+
+
+def __getattr__(name):
+    try:
+        module_name = toga_core_imports[name]
+    except KeyError:
+        raise AttributeError(f"module '{__name__}' has no attribute '{name}'") from None
+    else:
+        module = importlib.import_module(module_name)
+        value = getattr(module, name)
+        globals()[name] = value
+        return value
 
 
 class NotImplementedWarning(RuntimeWarning):
@@ -56,84 +83,12 @@ class NotImplementedWarning(RuntimeWarning):
     # single argument (the warning message). Use a factory method to avoid reproducing
     # the message format and the warn invocation.
     @classmethod
-    def warn(cls, platform: str, feature: str) -> None:
+    def warn(self, platform: str, feature: str):
         """Raise a warning that a feature isn't implemented on a platform."""
         warnings.warn(NotImplementedWarning(f"[{platform}] Not implemented: {feature}"))
 
 
-__all__ = [
-    "NotImplementedWarning",
-    # Applications
-    "App",
-    "DocumentApp",
-    # Commands
-    "Command",
-    "Group",
-    # Documents
-    "Document",
-    # Dialogs
-    "ConfirmDialog",
-    "ErrorDialog",
-    "InfoDialog",
-    "OpenFileDialog",
-    "QuestionDialog",
-    "SaveFileDialog",
-    "SelectFolderDialog",
-    "StackTraceDialog",
-    # Keys
-    "Key",
-    # Resources
-    "hsl",
-    "hsla",
-    "rgb",
-    "rgba",
-    "Font",
-    "Icon",
-    "Image",
-    # Types
-    "LatLng",
-    "Position",
-    "Size",
-    # Widgets
-    "ActivityIndicator",
-    "Box",
-    "Button",
-    "Canvas",
-    "DateInput",
-    "DetailedList",
-    "Divider",
-    "ImageView",
-    "Label",
-    "MapPin",
-    "MapView",
-    "MultilineTextInput",
-    "NumberInput",
-    "OptionContainer",
-    "OptionItem",
-    "PasswordInput",
-    "ProgressBar",
-    "ScrollContainer",
-    "Selection",
-    "Slider",
-    "SplitContainer",
-    "Switch",
-    "Table",
-    "TextInput",
-    "TimeInput",
-    "Tree",
-    "WebView",
-    "Widget",
-    # Windows
-    "DocumentMainWindow",
-    "MainWindow",
-    "Window",
-    # Deprecated widget names
-    "DatePicker",
-    "TimePicker",
-]
-
-
-def _package_version(file: Path | str | None, name: str) -> str:
+def _package_version(file, name):
     try:
         # Read version from SCM metadata
         # This will only exist in a development environment
@@ -142,10 +97,7 @@ def _package_version(file: Path | str | None, name: str) -> str:
         # Excluded from coverage because a pure test environment (such as the one
         # used by tox in CI) won't have setuptools_scm
         return get_version(root="../../..", relative_to=file)  # pragma: no cover
-    except (
-        ModuleNotFoundError,
-        LookupError,
-    ):  # pragma: no-cover-if-missing-setuptools_scm
+    except (ModuleNotFoundError, LookupError):
         # If setuptools_scm isn't in the environment, the call to import will fail.
         # If it *is* in the environment, but the code isn't a git checkout (e.g.,
         # it's been pip installed non-editable) the call to get_version() will fail.
