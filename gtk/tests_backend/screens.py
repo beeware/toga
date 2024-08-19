14b92b297cd38d6907d14f6752a7cbefbeed0385
Russell Martin <russell@rjm.li>
2024-08-18 23:26:44 -0400

    Tweak Wayland detection for robustness

--- gtk/tests_backend/screens.py
+++ gtk/tests_backend/screens.py
@@ -1,7 +1,7 @@
 import pytest
-from gi.repository import GdkX11
 
 from toga.images import Image as TogaImage
+from toga_gtk.libs import IS_WAYLAND, GdkX11
 
 from .probe import BaseProbe
 
@@ -12,7 +12,7 @@ class ScreenProbe(BaseProbe):
         self.screen = screen
         self._impl = screen._impl
         self.native = screen._impl.native
-        if self.IS_WAYLAND:
+        if IS_WAYLAND:
             # The native display type on Wayland is `__gi__.GdkWaylandMonitor`
             # However, that class can't be imported directly.
             pass
@@ -20,7 +20,7 @@ class ScreenProbe(BaseProbe):
             assert isinstance(self.native, GdkX11.X11Monitor)
 
     def get_screenshot(self, format=TogaImage):
-        if self.IS_WAYLAND:
+        if IS_WAYLAND:
             pytest.skip("Screen.as_image() is not implemented on Wayland.")
         else:
             return self.screen.as_image(format=format)
