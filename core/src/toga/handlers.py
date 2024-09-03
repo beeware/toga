5ec0b31f20d2a12ce1156b76093af2ac1110466b
Russell Martin <russell@rjm.li>
2024-09-03 14:12:01 -0400

    Always sleep between iterations of generator handlers

--- core/src/toga/handlers.py
+++ core/src/toga/handlers.py
@@ -66,8 +66,7 @@ async def long_running_task(
         try:
             while True:
                 delay = next(generator)
-                if delay:
-                    await asyncio.sleep(delay)
+                await asyncio.sleep(delay if delay else 0)
         except StopIteration as e:
             result = e.value
     except Exception as e:
