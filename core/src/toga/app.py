33a96944416a680df37023430eee9101134d1e38
proneon267 <proneon267@gmail.com>
2024-11-01 22:21:40 -0700

    Added note to docs

--- core/src/toga/app.py
+++ core/src/toga/app.py
@@ -827,7 +827,12 @@ class App:
 
     @property
     def current_window(self) -> Window | None:
-        """Return the currently active window."""
+        """The window currently in active focus.
+
+        On macOS, when a :any:`Dialog` is dismissed, the window from which the dialog
+        was initiated becomes the :any:`App.current_window`, regardless of whether the
+        :any:`App.current_window` property was modified.
+        """
         window = self._impl.get_current_window()
         if window is None:
             return None
