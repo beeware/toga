ae05877553f9dbc0bc98f25428e2c55e4a860e04
Elliana May <me@mause.me>
2024-11-24 19:20:54 +1100

    test: probes

---
+++ winforms/tests_backend/hardware/location.py
@@ -0,0 +1,24 @@
+from .hardware import HardwareProbe
+
+
+class LocationProbe(HardwareProbe):
+    def cleanup(self):
+        pass
+
+    def allow_permissions(self):
+        pass
+
+    def allow_permission(self):
+        pass
+
+    def grant_permission(self):
+        pass
+
+    def reject_permission(self):
+        pass
+
+    def add_location(self, loc):
+        pass
+
+    def allow_background_permission(self):
+        pass
