4e7517a571624a38bd77d673f4fd775eb95af4ee
sarayourfriend <git@sarayourfriend.pictures>
2024-12-02 16:52:01 +1100

    Allow module-level skip

--- testbed/tests/hardware/probe.py
+++ testbed/tests/hardware/probe.py
@@ -5,9 +5,9 @@ from ..conftest import skip_if_unbundled_app, skip_on_platforms
 
 def list_probes(hardware, skip_platforms=(), skip_unbundled=False):
     """Retrieve the probe classes for the hardware."""
-    skip_on_platforms(*skip_platforms)
+    skip_on_platforms(*skip_platforms, allow_module_level=True)
     if skip_unbundled:
-        skip_if_unbundled_app()
+        skip_if_unbundled_app(allow_module_level=True)
 
     module = import_module(f"tests_backend.hardware.{hardware.lower()}")
 
