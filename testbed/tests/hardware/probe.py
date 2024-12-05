e64ac5e3c5bb50d377e707392141aa8cfd89714f
sarayourfriend <git@sarayourfriend.pictures>
2024-12-05 12:45:14 +1100

    Simplify and document ``list_probes``

--- testbed/tests/hardware/probe.py
+++ testbed/tests/hardware/probe.py
@@ -3,13 +3,18 @@ from importlib import import_module
 from ..conftest import skip_if_unbundled_app, skip_on_platforms
 
 
-def list_probes(hardware, skip_platforms=(), skip_unbundled=False):
-    """Retrieve the probe classes for the hardware."""
+def list_probes(hardware: str, skip_platforms: tuple[str] = (), skip_unbundled: bool = False):
+    """Retrieve the probe classes for the hardware.
+    
+    :param hardware: the hardware for which to list the probes,
+        matching the module name for (e.g., "location", "camera")
+    :param skip_platforms: platforms to skip
+    :param skip_unbundled: whether to skip unbundled apps"""
     skip_on_platforms(*skip_platforms, allow_module_level=True)
     if skip_unbundled:
         skip_if_unbundled_app(allow_module_level=True)
 
-    module = import_module(f"tests_backend.hardware.{hardware.lower()}")
+    module = import_module(f"tests_backend.hardware.{hardware}")
 
     explicit_probes = getattr(module, "PROBES", None)
     if explicit_probes:
