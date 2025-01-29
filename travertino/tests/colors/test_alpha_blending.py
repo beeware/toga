9014af9f4fc12e30e634ff0f4c4c62f20a9b6542
proneon267 <proneon267@gmail.com>
2025-01-29 13:13:56 -0800

    Added changelog

--- travertino/tests/colors/test_alpha_blending.py
+++ travertino/tests/colors/test_alpha_blending.py
@@ -57,33 +57,31 @@ def test_alpha_blending_over(front_color, back_color, expected_blended_color):
 @pytest.mark.parametrize(
     "front_color, back_color",
     [
-        # fmt: off
         # Black, gray, white front colors
-        (rgba(0, 0, 0, 1.0),        rgba(50, 128, 200, 0.9)),
-        (rgba(128, 128, 128, 1.0),  rgba(50, 128, 200, 0.5)),
-        (rgba(255, 255, 255, 1.0),  rgba(50, 128, 200, 0.0)),
+        (rgba(0, 0, 0, 1.0), rgba(50, 128, 200, 0.9)),
+        (rgba(128, 128, 128, 1.0), rgba(50, 128, 200, 0.5)),
+        (rgba(255, 255, 255, 1.0), rgba(50, 128, 200, 0.0)),
         # Primaries
-        (rgba(255, 0, 0, 1.0),      rgba(0, 255, 0, 1.0)),
-        (rgba(0, 255, 0, 1.0),      rgba(255, 0, 0, 1.0)),
-        (rgba(0, 0, 255, 1.0),      rgba(255, 0, 0, 1.0)),
+        (rgba(255, 0, 0, 1.0), rgba(0, 255, 0, 1.0)),
+        (rgba(0, 255, 0, 1.0), rgba(255, 0, 0, 1.0)),
+        (rgba(0, 0, 255, 1.0), rgba(255, 0, 0, 1.0)),
         # Color with different channel values, including transparency
-        (rgba(50, 128, 200, 0.5),   rgba(128, 128, 128, 1.0)),
-        (rgba(50, 128, 200, 0.9),   rgba(0, 0, 0, 1.0)),
-        (rgba(50, 128, 200, 1.0),   rgba(0, 0, 0, 0)),
+        (rgba(50, 128, 200, 0.5), rgba(128, 128, 128, 1.0)),
+        (rgba(50, 128, 200, 0.9), rgba(0, 0, 0, 1.0)),
+        (rgba(50, 128, 200, 1.0), rgba(0, 0, 0, 0)),
         # Both front_color and back_color having intermediate alpha
         (rgba(100, 200, 255, 0.15), rgba(255, 100, 200, 0.85)),
-        (rgba(120, 180, 240, 0.2),  rgba(240, 120, 180, 0.8)),
-        (rgba(150, 50, 100, 0.4),   rgba(100, 150, 50, 0.6)),
-        (rgba(50, 60, 70, 0.55),    rgba(70, 80, 90, 0.45)),
-        (rgba(255, 255, 0, 0.3),    rgba(0, 255, 255, 0.7)),
+        (rgba(120, 180, 240, 0.2), rgba(240, 120, 180, 0.8)),
+        (rgba(150, 50, 100, 0.4), rgba(100, 150, 50, 0.6)),
+        (rgba(50, 60, 70, 0.55), rgba(70, 80, 90, 0.45)),
+        (rgba(255, 255, 0, 0.3), rgba(0, 255, 255, 0.7)),
         #
         # The blended color of these pairs, cannot be deblended to get the front
         # color. This is because when alpha channel of the front color is 0, then
         # the blended color will be equal to the back color. Therefore, all of the
         # information about the original front color will be lost.
-        (rgba(0, 0, 0, 0),          rgba(50, 128, 200, 1.0)),
-        (rgba(50, 128, 200, 0.0),   rgba(255, 255, 255, 1.0)),
-        # fmt: on
+        (rgba(0, 0, 0, 0), rgba(50, 128, 200, 1.0)),
+        (rgba(50, 128, 200, 0.0), rgba(255, 255, 255, 1.0)),
     ],
 )
 def test_reverse_straight_alpha_blending_over(front_color, back_color):
@@ -95,8 +93,8 @@ def test_reverse_straight_alpha_blending_over(front_color, back_color):
     )
     if front_color.a == 0:
         # When the front color has an alpha value of 0, then all information
-        # related to the front color will be lost, and the blended color
-        # cannot be deblended to get the original front color.
+        # related to the front color will be lost when the color is blended,
+        # and the blended color cannot be used to get the original front color.
         with pytest.raises(
             ValueError,
             match=re.escape(
