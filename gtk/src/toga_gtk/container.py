2a36b8787e5c259a8aef681d3c4e8e2406b2ffda
Dan Yeaw <dan@yeaw.me>
2025-01-31 09:04:31 -0500

    Fix drawing container with negative dimensions warning

--- gtk/src/toga_gtk/container.py
+++ gtk/src/toga_gtk/container.py
@@ -96,129 +96,119 @@ if GTK_VERSION >= (4, 0, 0):  # pragma: no-cover-if-gtk3
             container.needs_redraw = False
 
 
-class TogaContainer(Gtk.Box):
-    """A GTK container widget implementing Toga's layout.
+if GTK_VERSION < (4, 0, 0):  # pragma: no-cover-if-gtk3
 
-    This is a GTK widget, with no Toga interface manifestation.
-    """
+    class TogaContainer(Gtk.Fixed):
+        """A GTK container widget implementing Toga's layout.
 
-    def __init__(self):
-        super().__init__()
-
-        if GTK_VERSION >= (4, 0, 0):  # pragma: no-cover-if-gtk3
-            # Because we don’t have access to the existing layout manager, we must
-            # create our custom layout manager class.
-            layout_manager = TogaContainerLayoutManager()
-            self.set_layout_manager(layout_manager)
-
-        self._content = None
-        self.min_width = 100
-        self.min_height = 100
-
-        self.dpi = 96
-        self.baseline_dpi = self.dpi
-
-        # The dirty widgets are the set of widgets that are known to need
-        # re-hinting before any redraw occurs.
-        self._dirty_widgets = set()
-
-        # A flag that can be used to explicitly flag that a redraw is required.
-        self.needs_redraw = True
-
-    def refreshed(self):
-        pass
-
-    def make_dirty(self, widget=None):
-        """Mark the container (or a specific widget in the container) as dirty.
-
-        :param widget: If provided, this widget will be rehinted before the next layout.
+        This is a GTK widget, with no Toga interface manifestation.
         """
-        self.needs_redraw = True
-        if widget is not None:
-            self._dirty_widgets.add(widget)
-        self.queue_resize()
 
-    @property
-    def width(self):
-        """The display width of the container.
+        def __init__(self):
+            super().__init__()
+            self._content = None
+            self.min_width = 100
+            self.min_height = 100
 
-        If the container doesn't have any content yet, the width is 0.
-        """
-        if self._content is None:
-            return 0
-        if GTK_VERSION < (4, 0, 0):  # pragma: no-cover-if-gtk4
+            self.dpi = 96
+            self.baseline_dpi = self.dpi
+
+            # The dirty widgets are the set of widgets that are known to need
+            # re-hinting before any redraw occurs.
+            self._dirty_widgets = set()
+
+            # A flag that can be used to explicitly flag that a redraw is required.
+            self.needs_redraw = True
+
+        def refreshed(self):
+            pass
+
+        def make_dirty(self, widget=None):
+            """Mark the container (or a specific widget in the container) as dirty.
+
+            :param widget: If provided, rehint this widget before the next layout.
+            """
+            self.needs_redraw = True
+            if widget is not None:
+                self._dirty_widgets.add(widget)
+            self.queue_resize()
+
+        @property
+        def width(self):
+            """The display width of the container.
+
+            If the container doesn't have any content yet, the width is 0.
+            """
+            if self._content is None:
+                return 0
             return self.get_allocated_width()
-        else:  # pragma: no-cover-if-gtk3
-            return self.compute_bounds(self)[1].get_width()
 
-    @property
-    def height(self):
-        """The display height of the container.
+        @property
+        def height(self):
+            """The display height of the container.
 
-        If the container doesn't have any content yet, the height is 0.
-        """
-        if self._content is None:
-            return 0
-        if GTK_VERSION < (4, 0, 0):  # pragma: no-cover-if-gtk4
+            If the container doesn't have any content yet, the height is 0.
+            """
+            if self._content is None:
+                return 0
             return self.get_allocated_height()
-        else:  # pragma: no-cover-if-gtk3
-            return self.compute_bounds(self)[1].get_height()
 
-    @property
-    def content(self):
-        """The Toga implementation widget that is the root content of this container.
+        @property
+        def content(self):
+            """The Toga implementation widget that is the root content of this
+            container.
 
-        All children of the root content will also be added to the container as a result
-        of assigning content.
+            All children of the root content will also be added to the container
+            as a result of assigning content.
 
-        If the container already has content, the old content will be replaced. The old
-        root content and all it's children will be removed from the container.
-        """
-        return self._content
+            If the container already has content, the old content will be replaced.
+            The old root content and all it's children will be removed from the
+            container.
+            """
+            return self._content
 
-    @content.setter
-    def content(self, widget):
-        if self._content:
-            self._content.container = None
+        @content.setter
+        def content(self, widget):
+            if self._content:
+                self._content.container = None
 
-        self._content = widget
-        if widget:
-            widget.container = self
-            self.make_dirty(widget)
-        else:
-            self.make_dirty()
+            self._content = widget
+            if widget:
+                widget.container = self
+                self.make_dirty(widget)
+            else:
+                self.make_dirty()
 
-    def recompute(self):
-        """Rehint and re-layout the container's content, if necessary.
+        def recompute(self):
+            """Rehint and re-layout the container's content, if necessary.
 
-        Any widgets known to be dirty will be rehinted. The minimum possible layout size
-        for the container will also be recomputed.
-        """
-        if self._content and self._dirty_widgets:
-            # If any of the widgets have been marked as dirty,
-            # recompute their bounds, and re-evaluate the minimum
-            # allowed size for the layout.
-            while self._dirty_widgets:
-                widget = self._dirty_widgets.pop()
-                widget.rehint()
+            Any widgets known to be dirty will be rehinted. The minimum possible
+            layout size for the container will also be recomputed.
+            """
+            if self._content and self._dirty_widgets:
+                # If any of the widgets have been marked as dirty,
+                # recompute their bounds, and re-evaluate the minimum
+                # allowed size for the layout.
+                while self._dirty_widgets:
+                    widget = self._dirty_widgets.pop()
+                    widget.rehint()
 
-            # Recompute the layout
-            self._content.interface.style.layout(self)
+                # Recompute the layout
+                self._content.interface.style.layout(self)
 
-            self.min_width = self._content.interface.layout.min_width
-            self.min_height = self._content.interface.layout.min_height
+                self.min_width = self._content.interface.layout.min_width
+                self.min_height = self._content.interface.layout.min_height
 
-    def do_get_preferred_width(self):
-        """Return (recomputing if necessary) the preferred width for the container.
+        def do_get_preferred_width(self):
+            """Return (recomputing if necessary) the preferred width for the container.
 
-        The preferred size of the container is its minimum size. This
-        preference will be overridden with the layout size when the layout is
-        applied.
+            The preferred size of the container is its minimum size. This
+            preference will be overridden with the layout size when the layout is
+            applied.
 
-        If the container does not yet have content, the minimum width is set to
-        0.
-        """
-        if GTK_VERSION < (4, 0, 0):  # pragma: no-cover-if-gtk4
+            If the container does not yet have content, the minimum width is set to
+            0.
+            """
             # print("GET PREFERRED WIDTH", self._content)
             if self._content is None:
                 return 0, 0
@@ -229,18 +219,15 @@ class TogaContainer(Gtk.Box):
             # The container will conform to the size of the allocation it is given,
             # so the min and preferred size are the same.
             return self.min_width, self.min_width
-        else:  # pragma: no-cover-if-gtk3
-            pass
 
-    def do_get_preferred_height(self):
-        """Return (recomputing if necessary) the preferred height for the container.
+        def do_get_preferred_height(self):
+            """Return (recomputing if necessary) the preferred height for the container.
 
-        The preferred size of the container is its minimum size. This preference will be
-        overridden with the layout size when the layout is applied.
+            The preferred size of the container is its minimum size. This preference
+            will be overridden with the layout size when the layout is applied.
 
-        If the container does not yet have content, the minimum height is set to 0.
-        """
-        if GTK_VERSION < (4, 0, 0):  # pragma: no-cover-if-gtk4
+            If the container does not yet have content, the minimum height is set to 0.
+            """
             # print("GET PREFERRED HEIGHT", self._content)
             if self._content is None:
                 return 0, 0
@@ -251,18 +238,15 @@ class TogaContainer(Gtk.Box):
             # The container will conform to the size of the allocation it is given,
             # so the min and preferred size are the same.
             return self.min_height, self.min_height
-        else:  # pragma: no-cover-if-gtk3
-            pass
 
-    def do_size_allocate(self, allocation):
-        """Perform the actual layout for the widget, and all it's children.
+        def do_size_allocate(self, allocation):
+            """Perform the actual layout for the widget, and all it's children.
 
-        The container will assume whatever size it has been given by GTK - usually the
-        full space of the window that holds the container. The layout will then be re-
-        computed based on this new available size, and that new geometry will be applied
-        to all child widgets of the container.
-        """
-        if GTK_VERSION < (4, 0, 0):  # pragma: no-cover-if-gtk4
+            The container will assume whatever size it has been given by GTK - usually
+            the full space of the window that holds the container. The layout will then
+            be recomputed based on this new available size, and that new geometry will
+            be applied to all child widgets of the container.
+            """
             # print(
             #     self._content,
             #     f"Container layout {allocation.width}x{allocation.height} "
@@ -313,5 +297,143 @@ class TogaContainer(Gtk.Box):
 
             # The layout has been redrawn
             self.needs_redraw = False
-        else:  # pragma: no-cover-if-gtk3
+
+else:  # pragma: no-cover-if-gtk3
+
+    class TogaContainer(Gtk.Box):
+        """A GTK container widget implementing Toga's layout.
+
+        This is a GTK widget, with no Toga interface manifestation.
+        """
+
+        def __init__(self):
+            super().__init__()
+
+            # Because we don’t have access to the existing layout manager, we must
+            # create our custom layout manager class.
+            layout_manager = TogaContainerLayoutManager()
+            self.set_layout_manager(layout_manager)
+
+            self._content = None
+            self.min_width = 100
+            self.min_height = 100
+
+            self.dpi = 96
+            self.baseline_dpi = self.dpi
+
+            # The dirty widgets are the set of widgets that are known to need
+            # re-hinting before any redraw occurs.
+            self._dirty_widgets = set()
+
+            # A flag that can be used to explicitly flag that a redraw is required.
+            self.needs_redraw = True
+
+        def refreshed(self):
+            pass
+
+        def make_dirty(self, widget=None):
+            """Mark the container (or a specific widget in the container) as dirty.
+
+            :param widget: If provided, rehint this widget before the next layout.
+            """
+            self.needs_redraw = True
+            if widget is not None:
+                self._dirty_widgets.add(widget)
+            self.queue_resize()
+
+        @property
+        def width(self):
+            """The display width of the container.
+
+            If the container doesn't have any content yet, the width is 0.
+            """
+            if self._content is None:
+                return 0
+            return self.compute_bounds(self)[1].get_width()
+
+        @property
+        def height(self):
+            """The display height of the container.
+
+            If the container doesn't have any content yet, the height is 0.
+            """
+            if self._content is None:
+                return 0
+            return self.compute_bounds(self)[1].get_height()
+
+        @property
+        def content(self):
+            """The Toga implementation widget that is the root content of this
+            container.
+
+            All children of the root content will also be added to the container
+            as a result of assigning content.
+
+            If the container already has content, the old content will be replaced. The
+            old root content and all it's children will be removed from the container.
+            """
+            return self._content
+
+        @content.setter
+        def content(self, widget):
+            if self._content:
+                self._content.container = None
+
+            self._content = widget
+            if widget:
+                widget.container = self
+                self.make_dirty(widget)
+            else:
+                self.make_dirty()
+
+        def recompute(self):
+            """Rehint and re-layout the container's content, if necessary.
+
+            Any widgets known to be dirty will be rehinted. The minimum possible
+            layout size for the container will also be recomputed.
+            """
+            if self._content and self._dirty_widgets:
+                # If any of the widgets have been marked as dirty,
+                # recompute their bounds, and re-evaluate the minimum
+                # allowed size for the layout.
+                while self._dirty_widgets:
+                    widget = self._dirty_widgets.pop()
+                    widget.rehint()
+
+                # Recompute the layout
+                self._content.interface.style.layout(self)
+
+                self.min_width = self._content.interface.layout.min_width
+                self.min_height = self._content.interface.layout.min_height
+
+        def do_get_preferred_width(self):
+            """Return (recomputing if necessary) the preferred width for the container.
+
+            The preferred size of the container is its minimum size. This
+            preference will be overridden with the layout size when the layout is
+            applied.
+
+            If the container does not yet have content, the minimum width is set to
+            0.
+            """
+            pass
+
+        def do_get_preferred_height(self):
+            """Return (recomputing if necessary) the preferred height for the container.
+
+            The preferred size of the container is its minimum size. This preference
+            will be overridden with the layout size when the layout is applied.
+
+            If the container does not yet have content, the minimum height is set to 0.
+            """
+            pass
+
+        def do_size_allocate(self, allocation):
+            """Perform the actual layout for the widget, and all it's children.
+
+            The container will assume whatever size it has been given by GTK - usually
+            the full space of the window that holds the container. The layout will then
+            be recomputed based on this new available size, and that new geometry will
+            be applied to all child widgets of the container.
+            """
             pass
