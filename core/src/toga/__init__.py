564c79446ce33383728b8acc0a64bd37b26c33d5
Malcolm Smith <smith@chaquo.com>
2025-02-13 03:00:13 +0000

    Move _package_version to travertino (#3183)
    Moves the definition of _package_version to travertino to eliminate some duplicated code.

--- core/src/toga/__init__.py
+++ core/src/toga/__init__.py
@@ -1,6 +1,8 @@
 import importlib
 import warnings
 
+from travertino import _package_version
+
 toga_core_imports = {
     # toga.app imports
     "App": "toga.app",
@@ -103,28 +105,5 @@ class NotImplementedWarning(RuntimeWarning):
         warnings.warn(NotImplementedWarning(f"[{platform}] Not implemented: {feature}"))
 
 
-def _package_version(file, name):
-    try:
-        # Read version from SCM metadata
-        # This will only exist in a development environment
-        from setuptools_scm import get_version
-
-        # Excluded from coverage because a pure test environment (such as the one
-        # used by tox in CI) won't have setuptools_scm
-        return get_version(root="../../..", relative_to=file)  # pragma: no cover
-    except (
-        ModuleNotFoundError,
-        LookupError,
-    ):  # pragma: no-cover-if-missing-setuptools_scm
-        # If setuptools_scm isn't in the environment, the call to import will fail.
-        # If it *is* in the environment, but the code isn't a git checkout (e.g.,
-        # it's been pip installed non-editable) the call to get_version() will fail.
-        # If either of these occurs, read version from the installer metadata.
-        import importlib.metadata
-
-        # The Toga package names as defined in setup.cfg all use dashes.
-        package = "toga-core" if name == "toga" else name.replace("_", "-")
-        return importlib.metadata.version(package)
-
-
-__version__ = _package_version(__file__, __name__)
+# __name__ is "toga" in this file, but the distribution name is "toga-core".
+__version__ = _package_version(__file__, "toga-core")
