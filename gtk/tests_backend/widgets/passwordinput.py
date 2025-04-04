8c856b107cfaf0fa78584e07078325c7121bc122
Dan Yeaw <dan@yeaw.me>
2025-04-04 15:54:01 -0400

    GTK4: Skip Box and PasswordInput tests

--- gtk/tests_backend/widgets/passwordinput.py
+++ gtk/tests_backend/widgets/passwordinput.py
@@ -1,5 +1,11 @@
+import pytest
+
+from toga_gtk.libs import GTK_VERSION
+
 from .textinput import TextInputProbe
 
 
 class PasswordInputProbe(TextInputProbe):
+    if GTK_VERSION >= (4, 0, 0):
+        pytest.skip("Password Input is not yet supported with GTK4")
     pass
