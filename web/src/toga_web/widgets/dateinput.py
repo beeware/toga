7b24437d3b2e597734e05e725dbc148cf76c7844
mEp3ii2 <mitchell.pontague@hotmail.com>
2025-04-14 22:24:23 +0800

    Fixed handling of min and max bounds

--- web/src/toga_web/widgets/dateinput.py
+++ web/src/toga_web/widgets/dateinput.py
@@ -1,50 +1,71 @@
 import datetime
-from decimal import ROUND_UP
-
-from toga_web.libs import create_proxy
 
 from .base import Widget
 
 
-def py_date(native_date):
-    if not native_date:
-        return datetime.date.today()
-    return datetime.date.fromisoformat(native_date)
+def py_date(native_value):
+    return datetime.date.fromisoformat(native_value)
 
 
-def native_date(py_value):
-    return py_value.isoformat()
+def native_date(py_date_obj):
+    return py_date_obj.isoformat()
 
 
 class DateInput(Widget):
     def create(self):
         self.native = self._create_native_widget("sl-input")
-        self.native.setAttribute("type", "date")
-        self.native.addEventListener("sl-change", create_proxy(self.dom_value_changed))
+        self.native.type = "date"
+        self.native.value = native_date(datetime.date.today())
+        self.native.onblur = self.lost_focus
+
+    def lost_focus(self, event):
+        try:
+            input_date = py_date(self.native.value)
+        except Exception:
+            input_date = datetime.date.today()
+
+        min_date = self.get_min_date()
+        max_date = self.get_max_date()
+
+        if input_date < min_date:
+            input_date = min_date
+        elif input_date > max_date:
+            input_date = max_date
+
+        self.native.value = native_date(input_date)
 
     def get_value(self):
-        return py_date(self.native.value)
+        try:
+            return py_date(self.native.value)
+        except Exception:
+            return datetime.date.today()
 
     def set_value(self, value):
+        if value is None:
+            value = datetime.date.today()
+
+        min_date = self.get_min_date()
+        max_date = self.get_max_date()
+
+        if value < min_date:
+            value = min_date
+        elif value > max_date:
+            value = max_date
+
         self.native.value = native_date(value)
 
-    def get_min_date(self):
-        min_attr = self.native.getAttribute("min")
-        return py_date(min_attr) if min_attr else datetime.date(1800, 1, 1)
-
     def set_min_date(self, value):
-        self.native.setAttribute("min", native_date(value))
+        self.native.min = native_date(value)
 
-    def get_max_date(self):
-        max_attr = self.native.getAttribute("max")
-        return py_date(max_attr) if max_attr else datetime.date(8999, 12, 31)
+    def get_min_date(self):
+        if self.native.min is None:
+            return datetime.date(1800, 1, 1)
+        return datetime.date.fromisoformat(str(self.native.min))
 
     def set_max_date(self, value):
-        self.native.setAttribute("max", native_date(value))
+        self.native.max = native_date(value)
 
-    def rehint(self):
-        height = self.native.offsetHeight or 40
-        self.interface.intrinsic.height = self.scale_out(height, ROUND_UP)
-
-    def dom_value_changed(self, event):
-        self.interface.on_change(None)
+    def get_max_date(self):
+        if self.native.max is None:
+            return datetime.date(8999, 12, 31)
+        return datetime.date.fromisoformat(str(self.native.max))
