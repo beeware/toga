d79fe0f2e59f3d4b894b94753dc8800f3389270e
George Waters <gwatersdev@gmail.com>
2025-04-25 16:47:22 -0400

    Add typing generation script into pre-commit
    Doing it this way also requires not letting isort change the file. This
    is because both hooks will keep making changes, never allowing a commit
    to be made.

--- core/gen_typing.py
+++ core/gen_typing.py
@@ -6,5 +6,6 @@ typing_path = Path(__file__).parent / "src" / "toga" / "toga_typing.py"
 
 with typing_path.open(mode="w") as f:
     f.write("# flake8: noqa\n")  # Ignore unused imports
+    f.write('"""\nisort:skip_file\n"""\n')  # Don't let isort make changes
     for object, module in toga_core_imports.items():
         f.write(f"from {module} import {object}\n")
