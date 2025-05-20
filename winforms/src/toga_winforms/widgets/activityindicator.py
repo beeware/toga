50baf2fd5ff8bdc5761be76c2a3f07e5878cf117
John Zhou <john-xyz@outlook.com>
2025-05-19 20:30:40 -0500

    add activityindicator for winforms

---
+++ winforms/src/toga_winforms/widgets/activityindicator.py
@@ -0,0 +1,31 @@
+from importlib.resources import files
+
+import System.Windows.Forms as WinForms
+
+from .base import Widget
+
+
+class ActivityIndicator(Widget):
+    def create(self):
+        self.native = WinForms.PictureBox()
+        self.native.Image = files("toga_winforms.resources").joinpath("spinner.gif")
+        self.native.SizeMode = WinForms.PictureBoxSizeMode.Zoom
+        self.running = True
+
+    def set_hidden(self, hidden):
+        self.native.Visible = (not self.running) or hidden
+
+    def is_running(self):
+        return self.running
+
+    def start(self):
+        self.native.Visible = True
+        self.running = True
+
+    def stop(self):
+        self.native.Visible = False
+        self.running = False
+
+    def rehint(self):
+        self.interface.intrinsic.width = 32
+        self.interface.intrinsic.height = 32
