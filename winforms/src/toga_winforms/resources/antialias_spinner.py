dc47c7667f4c85b1043de1c68d5d4d502e34ac45
John Zhou <john-xyz@outlook.com>
2025-06-02 11:15:35 -0500

    use a nonfixed size when resizing

--- winforms/src/toga_winforms/resources/antialias_spinner.py
+++ winforms/src/toga_winforms/resources/antialias_spinner.py
@@ -1,3 +1,4 @@
+import ctypes
 import io
 from pathlib import Path
 
@@ -6,7 +7,21 @@ from System.Drawing import Image as NetImage
 from System.IO import MemoryStream
 
 
-def _composite_gif_on_color(path: str, rgb_color: tuple) -> bytes:
+def _get_size(logical_width=32, logical_height=32):
+    user32 = ctypes.windll.user32
+    try:
+        user32.SetProcessDPIAware()
+        dpi = user32.GetDpiForSystem()
+    except AttributeError:
+        dpi = 96
+    physical_width = logical_width * dpi / 96
+    physical_height = logical_height * dpi / 96
+    return int(physical_width * 2.5), int(
+        physical_height * 2.5
+    )  # *2 to make it sharp on monitor
+
+
+def _composite_gif_on_color(path: str, rgb_color: tuple, size: tuple) -> bytes:
     with Image.open(path) as original_gif:
         composited_frames = []
         durations = []
@@ -15,9 +30,7 @@ def _composite_gif_on_color(path: str, rgb_color: tuple) -> bytes:
             frame = frame.convert("RGBA")
             background = Image.new("RGBA", frame.size, rgb_color + (255,))
             composited = Image.alpha_composite(background, frame)
-            composited = composited.resize(
-                (composited.width // 2, composited.height // 2), Image.BILINEAR
-            )
+            composited = composited.resize(size, Image.BILINEAR)
             composited_p = composited.convert("P", palette=Image.ADAPTIVE)
             composited_frames.append(composited_p)
             durations.append(frame.info.get("duration", 20))
@@ -40,12 +53,13 @@ cache = dict()
 
 
 def antialias_spinner(color: tuple):
-    if color not in cache:
-        cache[color] = NetImage.FromStream(
+    size = _get_size()
+    if (color, size) not in cache:
+        cache[(color, size)] = NetImage.FromStream(
             MemoryStream(
                 _composite_gif_on_color(
-                    str(Path(__file__).parent / "spinner.gif"), color
+                    str(Path(__file__).parent / "spinner.gif"), color, size
                 )
             )
         )
-    return cache[color]
+    return cache[(color, size)]
