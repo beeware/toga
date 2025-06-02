0082df34f3bfc5b4cbc5e6ec2b88b23ce3e65a56
John Zhou <john-xyz@outlook.com>
2025-06-01 21:44:48 -0500

    memory leak

--- winforms/src/toga_winforms/resources/antialias_spinner.py
+++ winforms/src/toga_winforms/resources/antialias_spinner.py
@@ -7,33 +7,33 @@ from System.IO import MemoryStream
 
 
 def _composite_gif_on_color(path: str, rgb_color: tuple) -> bytes:
-    original_gif = Image.open(path)
-    composited_frames = []
-    durations = []
+    with Image.open(path) as original_gif:
+        composited_frames = []
+        durations = []
 
-    for frame in ImageSequence.Iterator(original_gif):
-        frame = frame.convert("RGBA")
-        background = Image.new("RGBA", frame.size, rgb_color + (255,))
-        composited = Image.alpha_composite(background, frame)
-        composited = composited.resize(
-            (composited.width // 2, composited.height // 2), Image.BILINEAR
+        for frame in ImageSequence.Iterator(original_gif):
+            frame = frame.convert("RGBA")
+            background = Image.new("RGBA", frame.size, rgb_color + (255,))
+            composited = Image.alpha_composite(background, frame)
+            composited = composited.resize(
+                (composited.width // 2, composited.height // 2), Image.BILINEAR
+            )
+            composited_p = composited.convert("P", palette=Image.ADAPTIVE)
+            composited_frames.append(composited_p)
+            durations.append(frame.info.get("duration", 20))
+
+        output_buffer = io.BytesIO()
+        composited_frames[0].save(
+            output_buffer,
+            format="GIF",
+            save_all=True,
+            append_images=composited_frames[1:],
+            loop=original_gif.info.get("loop", 0),
+            duration=durations,
+            disposal=2,
         )
-        composited_p = composited.convert("P", palette=Image.ADAPTIVE)
-        composited_frames.append(composited_p)
-        durations.append(frame.info.get("duration", 20))
-
-    output_buffer = io.BytesIO()
-    composited_frames[0].save(
-        output_buffer,
-        format="GIF",
-        save_all=True,
-        append_images=composited_frames[1:],
-        loop=original_gif.info.get("loop", 0),
-        duration=durations,
-        disposal=2,
-    )
-    output_buffer.seek(0)
-    return output_buffer.getvalue()
+        output_buffer.seek(0)
+        return output_buffer.getvalue()
 
 
 cache = dict()
