c93e1f43f404db04b0070bc47aee6f0989763b0e
John <john-xyz@outlook.com>
2025-06-03 05:27:53 -0500

    Update app.py

--- gtk/src/toga_gtk/app.py
+++ gtk/src/toga_gtk/app.py
@@ -34,14 +34,16 @@ class App:
         self.loop = self.policy.get_event_loop()
 
         # Stimulate the build of the app
-        # No coverage for either because both GTK3 and
-        # GTK4 could potentially go on the either cases
-        if GLIB_VERSION < (2, 74, 0):  # pragma: no cover
+        # *Note* -- the coverage may be inaccurate if GTK3 is used with
+        # a newer version of glib or if GTK4 is used with an older version
+        # of glib.  On local runs, coverage errors here can be safely
+        # ignored if the version of software is as described above.
+        if GLIB_VERSION < (2, 74, 0):  # pragma: no-cover-if-gtk3
             self.native = Gtk.Application(
                 application_id=self.interface.app_id,
                 flags=Gio.ApplicationFlags.FLAGS_NONE,
             )
-        else:  # pragma: no cover
+        else:  # pragma: no-cover-if-gtk4
             self.native = Gtk.Application(
                 application_id=self.interface.app_id,
                 flags=Gio.ApplicationFlags.DEFAULT_FLAGS,
