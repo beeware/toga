03709f2ae7e821cf0a20716cfac233d301ee70c6
John Zhou <john-xyz@outlook.com>
2025-06-04 09:32:45 -0500

    enable testing ActivityIndiactor

--- gtk/tests_backend/widgets/activityindicator.py
+++ gtk/tests_backend/widgets/activityindicator.py
@@ -6,6 +6,4 @@ from .base import SimpleProbe
 
 
 class ActivityIndicatorProbe(SimpleProbe):
-    if GTK_VERSION >= (4, 0, 0):
-        pytest.skip("ActivityIndicator is not yet supported with GTK4")
     native_class = Gtk.Spinner
