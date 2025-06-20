a8940d95c50df6edf1e6a19fd6424c51372fb881
John Zhou <john-xyz@outlook.com>
2025-06-19 21:03:31 -0500

    Use an assert on iOS and add Winforms support.

--- cocoa/tests_backend/widgets/activityindicator.py
+++ cocoa/tests_backend/widgets/activityindicator.py
@@ -1,5 +1,3 @@
-import pytest
-
 from toga_cocoa.libs import NSProgressIndicator
 
 from .base import SimpleProbe
@@ -9,9 +7,7 @@ class ActivityIndicatorProbe(SimpleProbe):
     native_class = NSProgressIndicator
 
     def assert_is_hidden(self, value):
-        if self.native.isDisplayedWhenStopped():
-            pytest.fail(
-                "ActivityIndicator should not be displayed when stopped on macOS"
-            )
+        # Check that we're not displaying the widget when stopped.
+        assert not self.native.isDisplayedWhenStopped()
         # No other action nessacary otherwise, as there's no way to
         # see if the widget is stopped.
