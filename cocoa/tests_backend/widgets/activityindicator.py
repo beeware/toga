61f41490a94ad4eb5bcb4fd5998ae9f8026f5ac0
Russell Keith-Magee <russell@keith-magee.com>
2025-06-20 11:46:39 +0800

    Clarify some code comments about testing strategy.

--- cocoa/tests_backend/widgets/activityindicator.py
+++ cocoa/tests_backend/widgets/activityindicator.py
@@ -9,5 +9,5 @@ class ActivityIndicatorProbe(SimpleProbe):
     def assert_is_hidden(self, value):
         # Check that we're not displaying the widget when stopped.
         assert not self.native.isDisplayedWhenStopped()
-        # No other action nessacary otherwise, as there's no way to
-        # see if the widget is stopped.
+        # No other assertion is possible, as there's no way to verify
+        # if the underlying widget is visible.
--- gtk/tests_backend/widgets/activityindicator.py
+++ gtk/tests_backend/widgets/activityindicator.py
@@ -7,4 +7,5 @@ class ActivityIndicatorProbe(SimpleProbe):
     native_class = Gtk.Spinner
 
     def assert_is_hidden(self, value):
+        # GTK automatically hides the widget on stop.
         assert (not self.native.get_property("active")) == value
