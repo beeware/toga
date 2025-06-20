aeb6d1bf87d155fcd6deed10cf78b01b25b5067c
John <john-xyz@outlook.com>
2025-06-19 23:04:57 -0500

    Test Visibility of ActivityIndicator (#3572)
    Signed-off-by: Moises <moises.aranas@gmail.com>
    Co-authored-by: Moises <moises.aranas@gmail.com>

--- cocoa/tests_backend/widgets/activityindicator.py
+++ cocoa/tests_backend/widgets/activityindicator.py
@@ -5,3 +5,9 @@ from .base import SimpleProbe
 
 class ActivityIndicatorProbe(SimpleProbe):
     native_class = NSProgressIndicator
+
+    def assert_is_hidden(self, value):
+        # Check that we're not displaying the widget when stopped.
+        assert not self.native.isDisplayedWhenStopped()
+        # No other assertion is possible, as there's no way to verify
+        # if the underlying widget is visible.
