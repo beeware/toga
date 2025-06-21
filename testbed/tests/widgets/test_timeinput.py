966f9540b1b0ad9c72e99e9f1bfc762707a72d8b
John <john-xyz@outlook.com>
2025-06-21 16:44:54 -0500

    Update test_timeinput.py

--- testbed/tests/widgets/test_timeinput.py
+++ testbed/tests/widgets/test_timeinput.py
@@ -81,7 +81,7 @@ async def widget():
 
 test_cleanup = build_cleanup_test(
     toga.TimeInput,
-    skip_platforms=("linux"),
+    skip_platforms=("linux",),
     xfail_platforms=("android",),
 )
 
