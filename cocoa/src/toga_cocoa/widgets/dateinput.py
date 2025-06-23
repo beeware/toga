1d7d1b08f6dc6835cb4b9846248353bc2bac5ad9
John <john-xyz@outlook.com>
2025-06-22 20:01:30 -0500

    Update dateinput.py

--- cocoa/src/toga_cocoa/widgets/dateinput.py
+++ cocoa/src/toga_cocoa/widgets/dateinput.py
@@ -78,7 +78,7 @@ class DateInput(Widget):
         self.interface.on_change()
 
     def rehint(self):
-        self.interface.intrinsic.width = at_least(self.interface._MIN_WIDTH)
+        self.interface.intrinsic.width = at_least(self.native.intrinsicContentSize().width)
         self.interface.intrinsic.height = self.native.intrinsicContentSize().height
 
     def get_min_date(self):
