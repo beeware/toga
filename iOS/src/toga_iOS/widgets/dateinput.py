379244b5554fa209d8208991512a19d3cd50e6ec
John <john-xyz@outlook.com>
2025-06-28 08:32:01 -0500

    Update iOS/src/toga_iOS/widgets/dateinput.py

--- iOS/src/toga_iOS/widgets/dateinput.py
+++ iOS/src/toga_iOS/widgets/dateinput.py
@@ -24,6 +24,8 @@ class TogaDatePicker(UIDatePicker):
     @objc_method
     def dateInputDidChange_(self, dateInput) -> None:
         self.interface.on_change()
+        self.setNeedsLayout()
+        self.layoutIfNeeded()
         self.impl.refresh()
 
 
