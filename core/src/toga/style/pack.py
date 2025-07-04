214932a8968b34f5831a16d10a828f7bbce3a6f6
Charles Whittington <charleswhitt@gmail.com>
2025-07-04 11:04:33 -0400

    Revert "Add no-cover to version branches" to see if it fails
    This reverts commit 9ce19b2490dff5fa7370601be2ee320cd3ea9819.

--- core/src/toga/style/pack.py
+++ core/src/toga/style/pack.py
@@ -178,9 +178,9 @@ class _alignment_property(validated_property):
 # End backwards compatibility
 ######################################################################
 
-if sys.version_info < (3, 10):  # pragma: no-cover-if-gte-py310
+if sys.version_info < (3, 10):
     _DATACLASS_KWARGS = {"init": False, "repr": False}
-else:  # pragma: no-cover-if-lt-py310
+else:
     _DATACLASS_KWARGS = {"kw_only": True, "repr": False}
 
 
