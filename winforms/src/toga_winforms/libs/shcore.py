1b224625660423d7e932ec8b284517688be9dfd8
760ceb3b9c0ba4872cadf3ce35a7a494 <109923290+760ceb3b9c0ba4872cadf3ce35a7a494@users.noreply.github.com>
2025-07-09 19:50:28 -0400

    fix formatting

--- winforms/src/toga_winforms/libs/shcore.py
+++ winforms/src/toga_winforms/libs/shcore.py
@@ -1,14 +1,14 @@
 from ctypes import HRESULT, POINTER, windll, wintypes
 
 try:
-	shcore = windll.shcore
+    shcore = windll.shcore
 except FileNotFoundError:
-	# Windows <8.1
-	shcore = None
+    # Windows <8.1
+    shcore = None
 
 if shcore:
-	GetScaleFactorForMonitor = shcore.GetScaleFactorForMonitor
-	GetScaleFactorForMonitor.restype = HRESULT
-	GetScaleFactorForMonitor.argtypes = [wintypes.HMONITOR, POINTER(wintypes.UINT)]
+    GetScaleFactorForMonitor = shcore.GetScaleFactorForMonitor
+    GetScaleFactorForMonitor.restype = HRESULT
+    GetScaleFactorForMonitor.argtypes = [wintypes.HMONITOR, POINTER(wintypes.UINT)]
 else:
-	GetScaleFactorForMonitor = None
+    GetScaleFactorForMonitor = None
