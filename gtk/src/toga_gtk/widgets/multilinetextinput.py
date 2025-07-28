b8f403e4b1dbc91e23264d4e6cefcbe96dd2b423
HiFiPhile <admin@hifiphile.com>
2025-07-28 11:23:45 +0200

    multilinetextinput: wait until the text change is fully completed on Gtk
    This is necessary to make scroll_to_bottom() after text change to work.
    
    Co-authored-by: Russell Keith-Magee <russell@keith-magee.com>
    Reported-by: Zixun LI <admin@hifiphile.com>

--- gtk/src/toga_gtk/widgets/multilinetextinput.py
+++ gtk/src/toga_gtk/widgets/multilinetextinput.py
@@ -91,6 +91,10 @@ class MultilineTextInput(Widget):
             else:
                 self.native_textview.set_buffer(self.buffer)
 
+        # Wait until the text change is fully completed
+        while Gtk.events_pending():
+            Gtk.main_iteration_do(blocking=False)
+
     def get_readonly(self):
         return not self.native_textview.get_property("editable")
 
