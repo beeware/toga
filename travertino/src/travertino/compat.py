8e5434a5e9b11b88d29fa88ce991b5e55daa691c
Russell Keith-Magee <russell@keith-magee.com>
2025-08-29 11:47:52 +0800

    Correct the comparison.

--- travertino/src/travertino/compat.py
+++ travertino/src/travertino/compat.py
@@ -5,10 +5,10 @@ from importlib.metadata import PackageNotFoundError, metadata
 @cache
 def _toga_lt_5():
     try:
-        toga_version = metadata("toga-core")["version"].split(".")
+        toga_version = metadata("toga-core")["version"]
     except PackageNotFoundError:  # pragma: no cover
         # If Toga isn't installed at all, we shouldn't invoke the backwards
         # compatibility shim; any exceptions must be from other sources.
         return False
     else:
-        return [int(v) for v in toga_version[:2]] < (0, 5)
+        return tuple(int(v) for v in toga_version.split(".")[:2]) < (0, 5)
