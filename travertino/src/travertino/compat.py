fe395576fefdb27949fc860677bfc17c5315714e
Russell Keith-Magee <russell@keith-magee.com>
2025-08-29 13:15:30 +0800

    Remove dependency on packaging. (#3731)
    Remove a dependency on packaging (introduced in backwards compatibility shim for Travertino) that causes web deployments to fail.

--- travertino/src/travertino/compat.py
+++ travertino/src/travertino/compat.py
@@ -1,16 +1,15 @@
 from functools import cache
 from importlib.metadata import PackageNotFoundError, metadata
 
-from packaging.version import Version
-
 
 @cache
 def _toga_lt_5():
     try:
-        toga_version = Version(metadata("toga-core")["version"])
+        toga_version = metadata("toga-core")["version"]
     except PackageNotFoundError:  # pragma: no cover
         # If Toga isn't installed at all, we shouldn't invoke the backwards
         # compatibility shim; any exceptions must be from other sources.
         return False
     else:
-        return toga_version < Version("0.5.0")
+        major, minor, *_ = toga_version.split(".")
+        return (int(major), int(minor)) < (0, 5)
