1a84d9c1974a020a66e47e5aab3d0b3abc97ccad
Russell Keith-Magee <russell@keith-magee.com>
2025-08-29 11:34:55 +0800

    Remove dependency on packaging.

--- travertino/src/travertino/compat.py
+++ travertino/src/travertino/compat.py
@@ -1,16 +1,14 @@
 from functools import cache
 from importlib.metadata import PackageNotFoundError, metadata
 
-from packaging.version import Version
-
 
 @cache
 def _toga_lt_5():
     try:
-        toga_version = Version(metadata("toga-core")["version"])
+        toga_version = metadata("toga-core")["version"].split(".")
     except PackageNotFoundError:  # pragma: no cover
         # If Toga isn't installed at all, we shouldn't invoke the backwards
         # compatibility shim; any exceptions must be from other sources.
         return False
     else:
-        return toga_version < Version("0.5.0")
+        return [int(v) for v in toga_version[:2]] < (0, 5)
