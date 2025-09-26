0f100d9192cd7a0ba6d3fe0cc7b7efe2c740d36d
proneon267 <45512885+proneon267@users.noreply.github.com>
2025-09-26 07:06:02 -0400

    Edit GTK backend parameter

--- gtk/tests_backend/window.py
+++ gtk/tests_backend/window.py
@@ -24,7 +24,7 @@ class WindowProbe(BaseProbe, DialogsMixin):
     supports_minimizable = False
     supports_move_while_hidden = False
     supports_unminimize = False
-    fullscreen_presentation_size_equal = not IS_WAYLAND
+    fullscreen_presentation_size_equal = True
 
     if GTK_VERSION < (4, 0, 0):
         # Wayland mostly prohibits interaction with the larger windowing environment
