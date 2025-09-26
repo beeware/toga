a7dc25d2f1de40b7bbea1d2a3448af9466fb827b
John <john-xyz@outlook.com>
2025-09-26 13:10:53 -0500

    Update fonts.py

--- core/src/toga/fonts.py
+++ core/src/toga/fonts.py
@@ -112,7 +112,21 @@ class Font(BaseFont):
         :param weight: The :ref:`font weight <pack-font-weight>`.
         :param style: The :ref:`font style <pack-font-style>`.
         :param variant: The :ref:`font variant <pack-font-variant>`.
+
+        :raises ValueError: When the registered family has the same name as the standard
+            font families "cursive", "fantasy", "message", "monospace", "sans-serif",
+            "serif", or "system".
         """
+        if family in (
+            CURSIVE,
+            FANTASY,
+            MESSAGE,
+            MONOSPACE,
+            SANS_SERIF,
+            SERIF,
+            SYSTEM,
+        ):
+            raise ValueError("Font family names cannot coincide with standard font types")
         font_key = Font._registered_font_key(family, weight, style, variant)
         _REGISTERED_FONT_CACHE[font_key] = str(toga.App.app.paths.app / path)
 
