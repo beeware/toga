5bd78a7cd0fbac447b6611fdc353856a5137b083
vt37 <110211722+vt37@users.noreply.github.com>
2025-10-03 08:01:57 +0800

    Added new widgets and update base_proxy to serialize callables

--- web-testbed/tests/tests_backend/proxies/object_proxies.py
+++ web-testbed/tests/tests_backend/proxies/object_proxies.py
@@ -30,3 +30,31 @@ class MockProxy(ObjectProxy):
 
 
 MockProxy.__name__ = MockProxy.__qualname__ = "Mock"
+
+
+class LabelProxy(ObjectProxy):
+    _ctor_expr = "toga.Label"
+
+
+LabelProxy.__name__ = LabelProxy.__qualname__ = "Label"
+
+
+class SwitchProxy(ObjectProxy):
+    _ctor_expr = "toga.Switch"
+
+
+SwitchProxy.__name__ = SwitchProxy.__qualname__ = "Switch"
+
+
+class TextInputProxy(ObjectProxy):
+    _ctor_expr = "toga.TextInput"
+
+
+TextInputProxy.__name__ = TextInputProxy.__qualname__ = "TextInput"
+
+
+class PasswordInputProxy(ObjectProxy):
+    _ctor_expr = "toga.PasswordInput"
+
+
+PasswordInputProxy.__name__ = PasswordInputProxy.__qualname__ = "PasswordInput"
