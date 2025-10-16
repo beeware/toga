81c793e0e0671e14887ef955ad27d572d3b051de
John Zhou <john-xyz@outlook.com>
2025-10-15 21:49:26 -0500

    use getattr

--- qt/src/toga_qt/libs/env.py
+++ qt/src/toga_qt/libs/env.py
@@ -2,12 +2,10 @@ from PySide6.QtGui import QGuiApplication
 
 from .utils import create_qapplication
 
-# Without a QApplication instance, determining the platform
-# name does not work correctly.
-create_qapplication()
 
-
-# Must be defined as a function, else the expression
-# gets evaluated too early and returns xcb on Wayland.
-IS_WAYLAND = QGuiApplication.platformName() == "wayland"
-print(IS_WAYLAND)
+def __getattr__(name):
+    if name == "IS_WAYLAND":
+        create_qapplication()
+        return QGuiApplication.platformName() == "wayland"
+    else:
+        raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
