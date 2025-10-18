ba62650f4b85a86ed498e126506702ca9dabef8d
John <john-xyz@outlook.com>
2025-10-18 12:33:45 -0500

    Update visibility check for Gtk.Spinner based on GTK_VERSION

--- gtk/tests_backend/widgets/activityindicator.py
+++ gtk/tests_backend/widgets/activityindicator.py
@@ -1,4 +1,4 @@
-from toga_gtk.libs import Gtk
+from toga_gtk.libs import GTK_VERSION, Gtk
 
 from .base import SimpleProbe
 
@@ -8,5 +8,8 @@ class ActivityIndicatorProbe(SimpleProbe):
 
     def assert_spinner_is_hidden(self, value):
         # GTK automatically hides the widget on stop.
-        is_visible = self.native.get_property("active") and self.native.get_visible()
+        if GTK_VERSION < (4, 0, 0):  # pragma: no-cover-if-gtk4
+            is_visible = self.native.get_property("active") and self.native.get_visible()
+        else:  # pragma: no-cover-if-gtk3
+            is_visible = self.native.get_property("spinning") and self.native.get_visible()
         assert is_visible == (not value)
