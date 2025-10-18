af245e1b795bac9ee7c2cac7a0320e8b0b4c1ebc
John <john-xyz@outlook.com>
2025-10-18 12:35:31 -0500

    Refactor visibility check for spinner widget

--- gtk/tests_backend/widgets/activityindicator.py
+++ gtk/tests_backend/widgets/activityindicator.py
@@ -9,7 +9,11 @@ class ActivityIndicatorProbe(SimpleProbe):
     def assert_spinner_is_hidden(self, value):
         # GTK automatically hides the widget on stop.
         if GTK_VERSION < (4, 0, 0):  # pragma: no-cover-if-gtk4
-            is_visible = self.native.get_property("active") and self.native.get_visible()
+            is_visible = (
+                self.native.get_property("active") and self.native.get_visible()
+            )
         else:  # pragma: no-cover-if-gtk3
-            is_visible = self.native.get_property("spinning") and self.native.get_visible()
+            is_visible = (
+                self.native.get_property("spinning") and self.native.get_visible()
+            )
         assert is_visible == (not value)
