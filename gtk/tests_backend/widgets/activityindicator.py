a26c4044799b19d88254dc11439ce34f59c1f8a7
John Zhou <john-xyz@outlook.com>
2025-11-15 14:50:54 -0600

    [port] ActivityIndicator

--- gtk/tests_backend/widgets/activityindicator.py
+++ gtk/tests_backend/widgets/activityindicator.py
@@ -1,16 +1,19 @@
-import pytest
-
 from toga_gtk.libs import GTK_VERSION, Gtk
 
 from .base import SimpleProbe
 
 
 class ActivityIndicatorProbe(SimpleProbe):
-    if GTK_VERSION >= (4, 0, 0):
-        pytest.skip("ActivityIndicator is not yet supported with GTK4")
     native_class = Gtk.Spinner
 
     def assert_spinner_is_hidden(self, value):
         # GTK automatically hides the widget on stop.
-        is_visible = self.native.get_property("active") and self.native.get_visible()
+        if GTK_VERSION < (4, 0, 0):  # pragma: no-cover-if-gtk4
+            is_visible = (
+                self.native.get_property("active") and self.native.get_visible()
+            )
+        else:  # pragma: no-cover-if-gtk3
+            is_visible = (
+                self.native.get_property("spinning") and self.native.get_visible()
+            )
         assert is_visible == (not value)
