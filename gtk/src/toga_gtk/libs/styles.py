4218eb01f9187325739d9ef1c9713bb615e44869
John <john-xyz@outlook.com>
2025-11-25 19:56:54 -0600

    [GTK4 port] Canvas (#3904)
    Co-authored-by: Russell Keith-Magee <russell@keith-magee.com>

--- gtk/src/toga_gtk/libs/styles.py
+++ gtk/src/toga_gtk/libs/styles.py
@@ -1,4 +1,4 @@
-from toga.colors import TRANSPARENT
+from toga.colors import TRANSPARENT, rgba
 from toga.fonts import SYSTEM_DEFAULT_FONT_SIZE
 
 from ..libs import GTK_VERSION
@@ -65,3 +65,16 @@ def get_font_css(value):
         style["font-size"] = f"{value.size}pt"
 
     return style
+
+
+def parse_css_color(native_string):
+    """Parse a color from a GTK4 native RGB(A) CSS string."""
+    if native_string[:4] == "rgba":
+        native_string = native_string[4:]
+    if native_string[:3] == "rgb":
+        native_string = native_string[3:]
+    r, g, b, *a = map(float, native_string.strip("()").split(","))
+    if a:
+        return rgba(r, g, b, a[0])
+    else:
+        return rgba(r, g, b)
