3a7bd87657443344134ad133f833112779f7ee78
Tom Arn <sw@t-arn.com>
2026-01-05 08:34:33 +0100

    fixed the problem that self.webview_impl can be None in on_page_finished

--- android/src/toga_android/widgets/internal/webview.py
+++ android/src/toga_android/widgets/internal/webview.py
@@ -7,9 +7,8 @@ from java.lang import String as jstring
 
 class TogaWebClient(static_proxy(WebViewClient)):
     def __init__(self, impl):
-        super().__init__()
-        self._webview_impl = impl
         self._webview_impl_ref = weakref.ref(impl)
+        super().__init__()
 
     @property
     def webview_impl(self):
@@ -18,7 +17,10 @@ class TogaWebClient(static_proxy(WebViewClient)):
     @Override(jboolean, [A_WebView, WebResourceRequest])
     def shouldOverrideUrlLoading(self, webview, webresourcerequest):
         allow = True
-        if self.webview_impl.interface.on_navigation_starting._raw:
+        if (
+            self.webview_impl
+            and self.webview_impl.interface.on_navigation_starting._raw
+        ):
             url = webresourcerequest.getUrl().toString()
             result = self.webview_impl.interface.on_navigation_starting(url=url)
             if isinstance(result, bool):
@@ -32,9 +34,9 @@ class TogaWebClient(static_proxy(WebViewClient)):
 
     @Override(jvoid, [A_WebView, jstring])
     def onPageFinished(self, webview, url):
-        if self._webview_impl.interface.on_webview_load:
-            self._webview_impl.interface.on_webview_load()
+        if self.webview_impl and self.webview_impl.interface.on_webview_load:
+            self.webview_impl.interface.on_webview_load()
 
-        if self._webview_impl.loaded_future:
-            self._webview_impl.loaded_future.set_result(None)
-            self._webview_impl.loaded_future = None
+        if self.webview_impl and self.webview_impl.loaded_future:
+            self.webview_impl.loaded_future.set_result(None)
+            self.webview_impl.loaded_future = None
