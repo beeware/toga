23d5bf65bdf93c258d647fe86a5535a5e81a2a21
Corran Webster <cwebster@unital.dev>
2026-01-19 01:04:45 +0000

    [Qt] Add support for all dialogs (#4076)
    Co-authored-by: Russell Keith-Magee <russell@keith-magee.com>

--- qt/tests_backend/dialogs.py
+++ qt/tests_backend/dialogs.py
@@ -1,11 +1,11 @@
 import asyncio
+from unittest.mock import Mock
 
-import pytest
-from PySide6.QtWidgets import QDialog
+from PySide6.QtWidgets import QDialog, QMessageBox
 
 
 class DialogsMixin:
-    supports_multiple_select_folder = True
+    supports_multiple_select_folder = False
 
     def _default_close_handler(self, dialog, qt_result):
         dialog._impl.native.done(qt_result)
@@ -51,26 +51,96 @@ class DialogsMixin:
             pre_close_test_method=pre_close_test_method,
         )
 
-    def setup_question_dialog_result(self, dialog, result):
-        pytest.skip("Qt backend only implements info dialog so far")
+    def setup_question_dialog_result(self, dialog, result, pre_close_test_method=None):
+        self._setup_dialog_result(
+            dialog,
+            QMessageBox.StandardButton.Yes if result else QMessageBox.StandardButton.No,
+            pre_close_test_method=pre_close_test_method,
+        )
 
-    def setup_confirm_dialog_result(self, dialog, result):
-        pytest.skip("Qt backend only implements info dialog so far")
+    def setup_confirm_dialog_result(self, dialog, result, pre_close_test_method=None):
+        self._setup_dialog_result(
+            dialog,
+            QMessageBox.StandardButton.Ok
+            if result
+            else QMessageBox.StandardButton.Cancel,
+            pre_close_test_method=pre_close_test_method,
+        )
 
-    def setup_error_dialog_result(self, dialog):
-        pytest.skip("Qt backend only implements info dialog so far")
+    def setup_error_dialog_result(self, dialog, pre_close_test_method=None):
+        self._setup_dialog_result(
+            dialog,
+            QDialog.DialogCode.Accepted,
+            pre_close_test_method=pre_close_test_method,
+        )
 
-    def setup_stack_trace_dialog_result(self, dialog, result):
-        pytest.skip("Qt backend only implements info dialog so far")
+    def setup_stack_trace_dialog_result(
+        self, dialog, result, pre_close_test_method=None
+    ):
+        if result is None:
+            qt_result = QMessageBox.StandardButton.Ok
+        else:
+            qt_result = (
+                QMessageBox.StandardButton.Retry
+                if result
+                else QMessageBox.StandardButton.Cancel
+            )
+        self._setup_dialog_result(
+            dialog,
+            qt_result,
+            pre_close_test_method=pre_close_test_method,
+        )
 
-    def setup_save_file_dialog_result(self, dialog, result):
-        pytest.skip("Qt backend only implements info dialog so far")
+    def setup_save_file_dialog_result(self, dialog, result, pre_close_test_method=None):
+        self._setup_dialog_result(
+            dialog,
+            QDialog.DialogCode.Accepted
+            if result is not None
+            else QDialog.DialogCode.Rejected,
+            pre_close_test_method=pre_close_test_method,
+        )
 
-    def setup_open_file_dialog_result(self, dialog, result, multiple_select):
-        pytest.skip("Qt backend only implements info dialog so far")
+    def setup_open_file_dialog_result(
+        self, dialog, result, multiple_select, pre_close_test_method=None
+    ):
+        def open_file_pre_close(dialog):
+            # mock results
+            if isinstance(result, list):
+                dialog._impl.native.selectedFiles = Mock(
+                    return_value=[str(path) for path in result]
+                )
+            else:
+                dialog._impl.native.selectedFiles = Mock(return_value=[str(result)])
 
-    def setup_select_folder_dialog_result(self, dialog, result, multiple_select):
-        pytest.skip("Qt backend only implements info dialog so far")
+        self._setup_dialog_result(
+            dialog,
+            QDialog.DialogCode.Accepted
+            if result is not None
+            else QDialog.DialogCode.Rejected,
+            pre_close_test_method=pre_close_test_method
+            if result is None
+            else open_file_pre_close,
+        )
+
+    def setup_select_folder_dialog_result(
+        self, dialog, result, multiple_select, pre_close_test_method=None
+    ):
+        def select_folder_pre_close(dialog):
+            # mock results
+            if isinstance(result, list):
+                dialog._impl.native.selectedFiles = Mock(return_value=[str(result[-1])])
+            else:
+                dialog._impl.native.selectedFiles = Mock(return_value=[str(result)])
+
+        self._setup_dialog_result(
+            dialog,
+            QDialog.DialogCode.Accepted
+            if result is not None
+            else QDialog.DialogCode.Rejected,
+            pre_close_test_method=pre_close_test_method
+            if result is None
+            else select_folder_pre_close,
+        )
 
     def is_modal_dialog(self, dialog):
         if dialog._impl.native is not None:
