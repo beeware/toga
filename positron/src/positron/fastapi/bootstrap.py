c3f0809745f27f56b7be5253609de3ed1ddd4f85
Russell Keith-Magee <russell@keith-magee.com>
2026-02-02 16:34:11 +0800

    Add support for iOS and Android.

--- positron/src/positron/fastapi/bootstrap.py
+++ positron/src/positron/fastapi/bootstrap.py
@@ -1,7 +1,13 @@
 from __future__ import annotations
 
+import sys
 from pathlib import Path
 from typing import Any
+
+if sys.version_info >= (3, 11):  # pragma: no-cover-if-lt-py311
+    import tomllib
+else:  # pragma: no-cover-if-gte-py311
+    import tomli as tomllib
 
 from ..base import BasePositronBootstrap
 
@@ -15,13 +21,18 @@ class FastAPIPositronBootstrap(BasePositronBootstrap):
     def template_path(self):
         return Path(__file__).parent / "templates"
 
+    def app_start_source(self):
+        return self.templated_content("__main__.py", initial_path=self.initial_path)
+
     def app_source(self):
         return self.templated_content("app.py", initial_path=self.initial_path)
 
     def pyproject_table_briefcase_app_extra_content(self):
         return """
 requires = [
-    "fastAPI == 0.128.0",
+    # 0.125.0 is the last version of FastAPI that supports Pydantic < 2.0
+    # This is a blocker on iOS/Android until wheels for pydantic-core are available.
+    "fastAPI == 0.125.0",
     "uvicorn == 0.40.0",
 ]
 test_requires = [
@@ -32,13 +43,29 @@ test_requires = [
 """
 
     def pyproject_table_iOS(self):
-        return """\
-supported = false
+        iOS_table = tomllib.loads(super().pyproject_table_iOS())
+        base_requires = "\n".join(f'    "{req}",' for req in iOS_table["requires"])
+        return f"""\
+requires = [
+{base_requires}
+    "pydantic < 2",
+]
 """
 
     def pyproject_table_android(self):
-        return """\
-supported = false
+        android_table = tomllib.loads(super().pyproject_table_android())
+        base_requires = "\n".join(f'    "{req}",' for req in android_table["requires"])
+        return f"""\
+requires = [
+{base_requires}
+    "pydantic < 2",
+]
+
+base_theme = "Theme.MaterialComponents.Light.DarkActionBar"
+
+build_gradle_dependencies = [
+    "com.google.android.material:material:1.13.0",
+]
 """
 
     def extra_context(self, project_overrides: dict[str, str]) -> dict[str, Any] | None:
@@ -70,7 +97,7 @@ supported = false
         app_path = base_path / "src" / self.context["module_name"]
 
         # App files
-        for template_name in ["site.py"]:
+        for template_name in ["server.py"]:
             self.console.debug(f"Writing {template_name}")
             self.templated_file(
                 template_name,
