45eecae8ccdb47328281613f613d7ebf0c22bd63
Charles Whittington <charleswhitt@gmail.com>
2026-02-02 22:51:34 -0500

    Fix coverage for 3.14

--- core/tests/widgets/canvas/test_deprecations.py
+++ core/tests/widgets/canvas/test_deprecations.py
@@ -1,3 +1,5 @@
+from weakref import ref
+
 import pytest
 
 import toga
@@ -29,6 +31,10 @@ from toga_dummy.utils import (
     assert_action_not_performed,
     assert_action_performed,
 )
+
+# Insert a dead weakref first in the list, just to make sure we cover the case of
+# checking against a nonexistent canvas.
+toga.Canvas._instances.insert(0, ref(toga.Canvas()))
 
 
 @pytest.mark.parametrize(
