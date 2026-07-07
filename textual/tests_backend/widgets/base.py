61a8666158b2e390fdbf996a32a80cf26620bbe9
Kevin Turcios <turcioskevinr@gmail.com>
2026-07-07 00:45:15 -0500

    Use native widget sizes in Textual probes

--- textual/tests_backend/widgets/base.py
+++ textual/tests_backend/widgets/base.py
@@ -1,61 +1,13 @@
-from math import ceil
-
 import pytest
 from textual.widget import Widget as TextualWidget
 
 from ..probe import BaseProbe
 
 
-class _NativeGeometryAdapter:
-    """Convert Textual terminal-cell geometry into Toga-compatible geometry."""
-
-    def __init__(self, widget):
-        self.widget = widget
-        self.impl = widget._impl
-        self.native = widget._impl.native
-
-    @property
-    def width_cells(self):
-        return self.native.styles.width.value
-
-    @property
-    def height_cells(self):
-        return self.native.styles.height.value
-
-    @property
-    def width(self):
-        try:
-            return self.impl.scale_out_horizontal(self.width_cells)
-        except AttributeError:
-            return self.widget.layout.content_width
-
-    @property
-    def height(self):
-        try:
-            return self.impl.scale_out_vertical(self.height_cells)
-        except AttributeError:
-            return self.widget.layout.content_height
-
-    def width_cells_for(self, width):
-        return self.impl.scale_in_horizontal(width)
-
-    def height_cells_for(self, height):
-        return self.impl.scale_in_vertical(height)
-
-    def width_cells_range(self, min_width, max_width):
-        return (
-            max(1 if min_width > 0 else 0, self.impl.scale_in_horizontal(min_width)),
-            ceil(max_width / self.impl.HORIZONTAL_SCALE),
-        )
-
-    def height_cells_range(self, min_height, max_height):
-        return (
-            max(1 if min_height > 0 else 0, self.impl.scale_in_vertical(min_height)),
-            ceil(max_height / self.impl.VERTICAL_SCALE),
-        )
-
-
 class SimpleProbe(BaseProbe):
+    HORIZONTAL_SCALE = 800 // 80
+    VERTICAL_SCALE = 600 // 25
+
     invalid_size_while_hidden = False
     supports_tab_index = False
     supports_text_width_change = True
@@ -66,7 +18,6 @@ class SimpleProbe(BaseProbe):
         self.widget = widget
         self.impl = widget._impl
         self.native = widget._impl.native
-        self._native_geometry = _NativeGeometryAdapter(widget)
         assert isinstance(self.native, self.native_class)
 
     def assert_container(self, container):
@@ -91,43 +42,29 @@ class SimpleProbe(BaseProbe):
 
     @property
     def width(self):
-        return self._native_geometry.width
+        return self.native.styles.width.value * self.HORIZONTAL_SCALE
 
     @property
     def height(self):
-        return self._native_geometry.height
+        return self.native.styles.height.value * self.VERTICAL_SCALE
 
     def assert_layout(self, size, position):
         assert self.widget._impl.container is not None
 
-        assert (
-            self._native_geometry.width_cells
-            == self._native_geometry.width_cells_for(size[0])
-        )
-        assert (
-            self._native_geometry.height_cells
-            == self._native_geometry.height_cells_for(size[1])
-        )
+        assert self.width == self.approx_width(size[0])
+        assert self.height == self.approx_height(size[1])
         assert (
             self.widget.layout.absolute_content_left,
             self.widget.layout.absolute_content_top,
         ) == position
 
     def assert_width(self, min_width, max_width):
-        native_width = self._native_geometry.width_cells
-        min_native_width, max_native_width = self._native_geometry.width_cells_range(
-            min_width, max_width
-        )
-        assert min_native_width <= native_width <= max_native_width, (
+        assert min_width <= self.width <= max_width, (
             f"Width ({self.width}) not in range ({min_width}, {max_width})"
         )
 
     def assert_height(self, min_height, max_height):
-        native_height = self._native_geometry.height_cells
-        min_native_height, max_native_height = self._native_geometry.height_cells_range(
-            min_height, max_height
-        )
-        assert min_native_height <= native_height <= max_native_height, (
+        assert min_height <= self.height <= max_height, (
             f"Height ({self.height}) not in range ({min_height}, {max_height})"
         )
 
