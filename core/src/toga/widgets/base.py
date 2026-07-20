b091a0f5f71103fe10e734271a9bfe009eab331f
Russell Keith-Magee <russell@keith-magee.com>
2026-07-20 11:01:39 +0800

    Apply suggestions from code review

--- core/src/toga/widgets/base.py
+++ core/src/toga/widgets/base.py
@@ -71,13 +71,15 @@ class Widget(Node, PackMixin, ABC):
 
         Properties provided by the widget's style can also be passed as constructor
         keyword arguments, or read and written directly on the widget. For example,
-        ``widget.margin = 10`` is equivalent to ``widget.style.margin = 10``.
+        `widget.margin = 10` is equivalent to `widget.style.margin = 10`.
+        By default, the widget will use [Pack](/reference/api/style/pack.md),
+        style attributes, but Toga allows for other style representations.
 
         :param id: The ID for the widget.
         :param style: A style object. If no style is provided, a default style
             will be applied to the widget.
         :param kwargs: Initial style properties. These override any matching
-            properties on the ``style`` argument.
+            properties on the `style` argument.
         """
         if style is None:
             style = Pack(**kwargs)
