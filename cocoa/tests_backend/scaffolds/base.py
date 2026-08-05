c451112aa1b4cc4f1a758f2f64d81500ad57b50e
John <john-xyz@outlook.com>
2026-08-05 09:10:34 -0500

    cleanup

--- cocoa/tests_backend/scaffolds/base.py
+++ cocoa/tests_backend/scaffolds/base.py
@@ -9,17 +9,6 @@ class ScaffoldProbe(BaseProbe):
         self.impl = scaffold._impl
         self.container = self.impl.container
 
-    async def redraw(self, message=None, delay=0, wait_for=None):
-        """Request a redraw of the scaffold, waiting until that redraw has completed."""
-        # Force a repaint
-        # view = self.impl.root_controller.view
-        # view.setNeedsLayout(True)
-        # view.layoutSubtreeIfNeeded()
-        # view.setNeedsDisplay(True)
-        # view.displayIfNeeded()
-
-        await super().redraw(message=message, delay=delay, wait_for=wait_for)
-
     def assert_container_layout(self):
         pass
 
