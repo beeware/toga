f4736cf16e85e7a08e58ebcc0d30b52f58ae4fc0
760ceb3b9c0ba4872cadf3ce35a7a494 <109923290+760ceb3b9c0ba4872cadf3ce35a7a494@users.noreply.github.com>
2026-08-22 15:50:36 -0400

    test all SwitchRoles of Switch

--- cocoa/tests_backend/widgets/switch.py
+++ cocoa/tests_backend/widgets/switch.py
@@ -1,11 +1,11 @@
 from pytest import xfail
 
-from toga_cocoa.libs import NSButton
+from toga_cocoa.libs import NSButton, NSView
 
 from .base import SimpleProbe
 
 
-class SwitchProbe(SimpleProbe):
+class CheckboxSwitchProbe(SimpleProbe):
     native_class = NSButton
 
     @property
@@ -15,3 +15,29 @@ class SwitchProbe(SimpleProbe):
     @property
     def color(self):
         xfail("Can't get/set the text color of a switch on macOS")
+
+
+class SwitchSwitchProbe(CheckboxSwitchProbe):
+    native_class = NSView
+
+    async def press(self):
+        self.impl.switch_native.performClick(None)
+
+    @property
+    def text(self):
+        return str(self.impl.label_native.stringValue)
+
+    @property
+    def enabled(self):
+        return self.impl.switch_native.isEnabled
+
+    @property
+    def font(self):
+        return self.impl.label_native.font
+
+
+# noinspection PyPep8Naming
+def SwitchProbe(widget):
+    if not isinstance(widget._impl.native, NSButton):
+        return SwitchSwitchProbe(widget)
+    return CheckboxSwitchProbe(widget)
